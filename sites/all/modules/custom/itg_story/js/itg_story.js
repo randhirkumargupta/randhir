3685d3d71c7a71439be2071253f62f95fd23dfab
shravan <shravan.kumar@kelltontech.com>
2016-01-18 13:05:43 +0530

    ITG-35 #comment worked on story

--- sites/all/modules/custom/itg_story/js/itg_story.js
+++ sites/all/modules/custom/itg_story/js/itg_story.js
@@ -1,23 +1,18 @@
-// (function($) {
-//            Drupal.behaviors.itg_story = {
-//                attach: function(context, settings) {
-//                    $('select[name="CI_PState"]').on("change", function() {
-//                        var base_url = settings.itg_story.settings.base_url;
-//                        $.ajax({
-//                            url: base_url + "/personalinfo/get-city-list",
-//                            method: 'post',
-//                            data: {'tid': this.value, 'pid': $('input[name="productid"]').val()},
-//                            success: function(data) {
-//                                var select = $('select[name="CI_PCity"]');
-//                                select.empty().append(data);
-//                                select.data("selectBox-selectBoxIt").refresh();
-//                            },
-//                            error: function(request, status, error) {
-//                                alert(request.responseText);
-//                            }
-//                        });
-//                    });
-//                }
-//
-//    };
-//})(jQuery, Drupal, this, this.document);
+ (function($) {
+        Drupal.behaviors.itg_story = {
+             attach: function(context, settings) {
+                   $('#edit-field-story-reporter-und-0-target-id').blur(function() {
+                     var base_url = settings.itg_story.settings.base_url;                     
+                     $.ajax({
+                         url: base_url + "/reporter-details",
+                         method: 'post',
+                         data: {'reporter_id': $('#edit-field-story-reporter-und-0-target-id').val()},
+                         success: function(data) {
+                               $('#reporter-details').html(data);
+                         }
+                     });
+                 });
+             }
+
+ };
+})(jQuery, Drupal, this, this.document);
