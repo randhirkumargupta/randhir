aa8eb070b99fd118c7e2427df36f8777ae2d8125
Avanish Singh <avanish@electricsage.com>
2016-01-25 20:20:06 +0530

    "#ITG-99 Theme login page and updated css"

--- sites/all/themes/itg/templates/page.tpl.php
+++ sites/all/themes/itg/templates/page.tpl.php
@@ -11,7 +11,7 @@
 <div id="page">
 
   <header class="header" id="header" role="banner">
-
+    <section class="section">
     <?php if ($logo): ?>
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
     <?php endif; ?>
@@ -47,7 +47,7 @@
     <?php endif; ?>
 
     <?php print render($page['header']); ?>
-
+    </section>
   </header>
 
   <div id="main">
@@ -60,7 +60,7 @@
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
-        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+        <h1 class="page__title title" id="page-title"><?php //print $title; ?></h1>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php print $messages; ?>
