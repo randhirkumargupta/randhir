2816e8fbe59ac2b87b4c64c9f042bc2417d3f6af
Avanish Singh <avanish@electricsage.com>
2016-01-28 19:01:05 +0530

    "#ITG-99 Story page, Category management"

--- sites/all/themes/itg/templates/page--front.tpl.php
+++ sites/all/themes/itg/templates/page--front.tpl.php
@@ -15,7 +15,7 @@
       <?php if ($logo): ?>
         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
       <?php endif; ?>
-
+        <div class="fright login"><a href="user">Login</a></div>
       <?php if ($site_name || $site_slogan): ?>
         <div class="header__name-and-slogan" id="name-and-slogan">
           <?php if ($site_name): ?>
