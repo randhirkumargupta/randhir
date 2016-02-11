9e1184d83a1bcf07cd0c7a3322ef9b58b64e966a
shravan <shravan.kumar@kelltontech.com>
2016-02-11 12:31:18 +0530

    ITG-143 #comment worked on story Workflow

--- sites/all/modules/custom/itg_story/js/itg_story.js
+++ sites/all/modules/custom/itg_story/js/itg_story.js
@@ -22,6 +22,7 @@
                      $('.form-item-metatags-und-abstract-value').hide();
                      $('#edit-field-story-facebook-image').hide();
                      $('#edit-field-story-expiry-date').hide();
+                     $('#edit-field-story-archive').hide();
                      
                      // code for Magazine and Supplement field show on edit story form
                      if ($("#edit-field-story-magazine-story-issue-und-magazine-issue-story").is(':checked')) {                         
