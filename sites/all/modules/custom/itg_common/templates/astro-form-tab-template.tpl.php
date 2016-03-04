1ffcbd9cdb76a4b6febcd49da78d7a7891fda4f3
Yogesh Kushwaha <yogesh.kushwaha@kelltontech.com>
2016-03-04 13:13:45 +0530

    Astro node form tab enabled

---
+++ sites/all/modules/custom/itg_common/templates/astro-form-tab-template.tpl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ * Theme implementation for story form in tab display.
+ * 
+ */
+
+?>
+<div class="block-itg-story-list">
+  <span class="target-link" data-target-id="BasicDetails">Basic Details</span> 
+  <span class="target-link" data-target-id="StoryContent">Zodiac Sign</span> 
+  <span class="target-link" data-target-id="Configuration">Collective Content</span> 
+  <span class="target-link" data-target-id="SocialMedia">Numerology</span> 
+</div>
