5ff50f63e3ab26fce3ca0df2eea9c7939da73eaa
Yogesh Kushwaha <ankush.gautam@kelltontech.com>
2016-03-08 11:20:33 +0530

    “ITG-433

--- sites/all/modules/custom/itg_story/js/itg_story.js
+++ sites/all/modules/custom/itg_story/js/itg_story.js
@@ -160,15 +160,17 @@
                  $('#edit-field-story-schedule-date-time-und-0-value-datepicker-popup-0').datepicker({
                     changeYear: true,
                     readonly: true,
+                    changeYear: false,
                     minDate: '0',
                     //maxDate: '+1M',
                  });
                 
                 // code to estrict user to select previous date
                  $('#edit-field-story-expiry-date-und-0-value-datepicker-popup-0').datepicker({
+                    changeMonth: false,
                     changeYear: true,
                     readonly: true,
-                    minDate: '0',
+                    minDate:new Date((currentYear - 1), 12, 1),
                     //maxDate: '+1M',
                 });
                 
