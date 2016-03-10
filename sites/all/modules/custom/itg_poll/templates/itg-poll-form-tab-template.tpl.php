35f4a938e83b4ea0bc9bfd12bcfa3272121190ac
vedprakash.pareek <vedprakash.pareek@kelltontech.com>
2016-03-10 12:42:00 +0530

    ITG-134 ITG-135 #comment Poll preview template

---
+++ sites/all/modules/custom/itg_poll/templates/itg-poll-form-tab-template.tpl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Theme implementation for story form in tab display.
+ * 
+ */
+
+?>
+<div class="block-itg-story-list">
+<span class="target-link" data-target-id="Element">Poll Title</span> 
+<span class="target-link" data-target-id="BrowseMedia">Poll Question</span>
+<span class="target-link" data-target-id="GalleryIndividualImages">Poll configuration</span>
+<span class="target-link" data-target-id="Configuration">Configuration</span>
+<span class="target-link" data-target-id="title-metatags">Meta Details</span>
+</div>
