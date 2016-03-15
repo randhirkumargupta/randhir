e727ee37b5efd2653c6c0de8d4b1eaee12c85801
Yogesh Kushwaha <ankush.gautam@kelltontech.com>
2016-03-15 12:45:48 +0530

    “ITG-167

--- sites/all/themes/rubik/templates/node--breaking-news.tpl.php
+++ sites/all/themes/rubik/templates/node--breaking-news.tpl.php
@@ -45,7 +45,7 @@
                     <?php print render($content['field_type']); ?>
                     <?php print render($content['field_content_type']); ?>
                      <div class="field">
-                        <div class="field-label">Breaking Text:</div>
+                        <div class="field-label"><?php print t('Breaking Text:');?></div>
                         <div class="field-items"><h1><?php print $title; ?></h1></div>
                     </div>
                    
@@ -91,7 +91,7 @@
                     ?>
               <div class="content-node-view">
                   <?php if($cnd != '') { ?>
-                  <h2>Display on</h2>
+                  <h2><?php print t('Display on');?></h2>
                   <?php } ?>
                   <div class="content-details">
                     <?php
@@ -111,7 +111,7 @@
             $browsemedia = render($content['field_story_extra_large_image']);  
             if(!empty($browsemedia)):?>
           <div class="content-node-view">
-            <h2>Browse Media</h2>
+            <h2><?php print t('Browse Media');?></h2>
             <div class="content-details">
             <?php print render($content['field_story_extra_large_image']); ?>
             </div>
@@ -119,7 +119,7 @@
            <?php endif;?>
            
           <div class="content-node-view">
-            <h2>Content Details</h2>
+            <h2><?php print t('Content Details');?></h2>
             <div class="content-details">
             <?php print render($content['field_breaking_content_details']); ?>
                         </div>
