c5f3956fd39673f0093943dc6495859f01ac07d9
Haripal Rao <haripal.rao@kelltontech.com>
2016-03-30 16:06:06 +0530

    ITG-426 #comment Survey start date and end date bug

--- sites/all/modules/custom/itg_survey/js/itg_survey.js
+++ sites/all/modules/custom/itg_survey/js/itg_survey.js
@@ -27,6 +27,7 @@
           minDate: '0',
           readonly: true
         });
+        $('edit-field-survey-start-date-und-0-value-datepicker-popup-0, #edit-field-survey-start-date-und-0-value-datepicker-popup-1, #edit-field-survey-end-date-und-0-value-datepicker-popup-0, edit-field-survey-end-date-und-0-value-datepicker-popup-1').prop("readonly", true);
       }
     }
   };
