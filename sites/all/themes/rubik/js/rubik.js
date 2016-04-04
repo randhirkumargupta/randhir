2b9d5bd0dcceedf48c0fe62141c61478d2de29fc
Sumit Prajapati <sumit.prajapati@kelltontech.com>
2016-04-04 15:22:59 +0530

    ITG-757, 761 bug solve

--- sites/all/themes/rubik/js/rubik.js
+++ sites/all/themes/rubik/js/rubik.js
@@ -149,7 +149,7 @@ Drupal.behaviors.rubik.attach = function(context, settings) {
     
     });
   //Incorrect navigation when user click on home page from view blog page
-  $("a[href='/itgcms/blog']").attr('href', '/itgcms/my-draft');
+  $("a[href='/itgcms/blog']").attr('href', '/itgcms/mydraft-blogs');
     
   // Jquery code to close preview popup
   $(document).on('click', '.close-preview', function(){
