ada493779c9bd8daf5c8695f5139f7d1669f2071
Yogesh Kushwaha <ankush.gautam@kelltontech.com>
2016-04-11 17:30:51 +0530

    “ITG-164

--- sites/all/themes/rubik/templates/user-profile.tpl.php
+++ sites/all/themes/rubik/templates/user-profile.tpl.php
@@ -41,11 +41,17 @@
   <div class="field-name-field-user-picture">
   <div class="field-items">
   <?php
-    if (isset($elements['#account']->field_user_picture[LANGUAGE_NONE])) {
-      $file=$elements['#account']->field_user_picture[LANGUAGE_NONE][0]['uri']; 
+   global $user, $base_url;  
+   
+   if (isset($elements['#account']->field_user_picture[LANGUAGE_NONE])) {
+      $file = $elements['#account']->field_user_picture[LANGUAGE_NONE][0]['uri'];
       print theme('image_style', array('style_name' => 'user_picture', 'path' => $file));
     }
-  ?>
+    else {
+      $file = $base_url . '/sites/all/themes/rubik/images/user-default.png';
+      print "<img src= $file>";
+    }
+    ?>
   </div>
   </div>
   <?php endif; ?>
@@ -81,3 +87,196 @@
     </div>
   </div>
 </div>
+
+<div class="my-stats">
+<h1> My Stats </h1>
+<?php
+ $node_type = itg_get_all_node_type($elements['#account']->uid);
+      
+      foreach ($node_type as $final_type) {
+        $final_type1 = ucfirst(str_replace('_', '', $final_type));
+        $total  =  $final_type1.': Total '.itg_get_all_node($final_type,$elements['#account']->uid);
+        $publish = ': Publish '.itg_get_all_publish_node($final_type,$elements['#account']->uid);
+       print $total.$publish;
+       print '<br/>';
+       
+      }
+      
+      // get last node title and type publish by user
+      $last_record = itg_last_node_user($elements['#account']->uid);
+      $last_record_type = ucfirst(str_replace('_', '', $last_record['type']));
+      if(!empty($last_record)) {
+      $last_record_display = 'Last Content Filed : '.$last_record_type.' - '.$last_record['title'];
+      print $last_record_display;
+      }
+      else
+      {
+      $last_record_display = 'Last Content Filed : N/A';
+      print $last_record_display;
+      }
+      print '<br/>';
+      $last_publish_record = itg_last_node_published_user($elements['#account']->uid,'1');
+      
+      if(!empty($last_publish_record)) {
+        $last_title = node_load($last_publish_record['nid'], $last_publish_record['vid']);
+        $last_record_publish = 'Last Content publish : '.$last_title->title;
+        print $last_record_publish;
+      }
+      else
+      {
+        $last_record_publish = 'Last Content publish : N/A';
+        print $last_record_publish;
+      }
+      
+      
+      $permissions= user_role_permissions($elements['#account']->roles);
+      foreach($permissions as $key => $value) {
+        foreach($value as $key1 => $value1) {
+          //pr($key1);
+          // make create permission array
+         if(strstr($key1,'create')) {
+          $create_arr = explode(' ', $key1);
+          $final_create_arr[] = ucwords(str_replace('_', ' ', $create_arr[1]));
+        }
+        
+        // make edit permission array
+        if(strstr($key1,'edit')) {
+          
+          if(!strstr($key1,'assign')) {
+          $edit_arr = explode(' ', $key1);
+           if(!empty($edit_arr[2]) && $edit_arr[1]!= 'terms') {
+          $final_edit_arr[] = ucwords(str_replace('_', ' ', $edit_arr[2]));
+          $final_edit_arr = array_unique($final_edit_arr);
+         
+           }
+          }
+        }
+        
+         // make delete permission array
+        if(strstr($key1,'delete')) {
+          
+          if(!strstr($key1,'assign')) {
+          $del_arr = explode(' ', $key1);
+           if(!empty($del_arr[2]) && $del_arr[1]!= 'terms') {
+          $final_del_arr[] = ucwords(str_replace('_', ' ', $del_arr[2]));
+          $final_del_arr = array_unique($final_del_arr);
+         
+           }
+          }
+        }
+        
+        // make role permission array
+        if(strstr($key1,'assign')) {
+          $role_arr = explode(' ', $key1);
+          $final_role_arr[] = ucwords(str_replace('_', ' ', $role_arr[1]));
+        }
+      }
+      }
+      
+      // conbert array to comma seprated
+      $comma_create_arr = implode (", ", $final_create_arr);
+      $comma_edit_arr = implode (", ", $final_edit_arr);
+      $comma_role_arr = implode (", ", $final_role_arr);
+      $comma_del_arr = implode (", ", $final_del_arr);
+      
+$output = "<table style='width:100%'>
+        <tr>
+        <th>Operations</th>
+        <th>Types</th>
+              </tr><tr>
+        <td>create</td>
+        <td>$comma_create_arr</td>
+              </tr><tr>
+        <td>Edit</td>
+        <td>$comma_edit_arr</td>
+          
+              </tr><tr>
+        <td>Delete</td>
+        <td>$comma_del_arr</td>
+          
+              </tr><tr>
+        <td>Role</td>
+        <td>$comma_role_arr</td>
+          
+              </tr>";      
+$output .= "</table>";
+
+print $output;
+?>
+
+</div>
+
+<?php
+// query to get count of All node 
+function itg_get_all_node($content_type,$uid) {
+  
+  $query = "SELECT COUNT(*) amount FROM {node} n ".
+              "WHERE n.type = :type AND n.uid = :uid";
+     $result = db_query($query, array(':type' => $content_type, ':uid' => $uid))->fetch();
+     return $result->amount;
+}
+
+// query to get count of publsh node 
+function itg_get_all_publish_node($content_type,$uid) {
+  
+  $query = "SELECT COUNT(*) amount FROM {node} n ".
+              "WHERE n.type = :type AND n.uid = :uid AND n.status = '1'";
+     $result = db_query($query, array(':type' => $content_type, ':uid' => $uid))->fetch();
+     return $result->amount;
+}
+
+// query to get all node type of user
+function itg_get_all_node_type($uid) {
+  
+  $result = db_select('node', 'n')
+          ->fields('n', array('type'))
+          ->condition('uid', $uid, '=')
+          ->groupBy('type')
+          ->execute();
+  
+  while($record = $result->fetchAssoc()) {
+        $type[] = $record['type'];
+    }
+    
+    return $type;
+}
+
+
+// query to get last node created by user
+function itg_last_node_user($uid) {
+  
+  $last_result = db_select('node', 'n')
+          ->fields('n', array('title','type'))
+          ->condition('uid', $uid, '=')
+          ->orderBy('created', 'DESC')//ORDER BY created
+          ->range(0,1)
+          ->execute();
+  
+  while ($last_record = $last_result->fetchAssoc()) {
+    $last_record_info['title'] = $last_record['title'];
+    $last_record_info['type'] = $last_record['type'];
+  }
+  
+    return $last_record_info;
+}
+
+// query to get last node published by user
+function itg_last_node_published_user($uid,$publish_id) {
+  
+  $last_publish_result = db_select('workbench_moderation_node_history', 'w')
+          ->fields('w', array('nid','vid'))
+          ->condition('uid', $uid, '=')
+          ->condition('published', $publish_id, '=')
+          ->orderBy('stamp', 'DESC')//ORDER BY created
+          ->range(0,1)
+          ->execute();
+  
+  while ($last_publish_record = $last_publish_result->fetchAssoc()) {
+    $last_publish_record_info['vid'] = $last_publish_record['vid'];
+    $last_publish_record_info['nid'] = $last_publish_record['nid'];
+  }
+  
+    return $last_publish_record_info;
+}
+
+?>
