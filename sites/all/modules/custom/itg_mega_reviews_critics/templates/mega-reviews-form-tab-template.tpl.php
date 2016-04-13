2cf82d6eede523efc6819c58274bae15a2069518
shravan <shravan.kumar@kelltontech.com>
2016-04-13 15:09:06 +0530

    ITG-798 #comment worked on this issue

---
+++ sites/all/modules/custom/itg_mega_reviews_critics/templates/mega-reviews-form-tab-template.tpl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Theme implementation for story form in tab display.
+ * 
+ */
+
+?>
+<div class="block-itg-story-list">
+  <span class="target-link" data-target-id="basicdetails"><?php print t('Basic Details'); ?></span> 
+  <span class="target-link" data-target-id="browsemedia"><?php print t('Browse Media'); ?></span>  
+  <span class="target-link" data-target-id="moviereview"><?php print t('Movie Review'); ?></span>
+<!--  <span class="target-link" data-target-id="datesettings"><?php print t('Date Settings'); ?></span>-->
+  <span class="target-link" data-target-id="metatags"><?php print t('Meta Details'); ?></span>  
+</div>
