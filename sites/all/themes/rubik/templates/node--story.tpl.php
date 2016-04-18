17bad02ee52b63e6ccde3722dc3df9811324a90a
Yogesh Kushwaha <yogesh.kushwaha@kelltontech.com>
2016-04-18 14:28:44 +0530

    block render code removed from story tpl file.

--- sites/all/themes/rubik/templates/node--story.tpl.php
+++ sites/all/themes/rubik/templates/node--story.tpl.php
@@ -42,11 +42,7 @@
       <?php if (!empty($content)): ?>
         <div class='<?php print $hook ?>-content clearfix <?php if (!empty($is_prose)) print 'prose' ?>'>          
 
-          <?php if ($view_mode == 'full'): ?>
-          <?php
-            //$block = module_invoke('itg_social_media', 'block_view', 'social_media_form');
-            //print render($block['content']);
-          ?>
+          <?php if ($view_mode == 'full'): ?>          
           <div class="basic-details content-box">
             <h2><?php print t('Quick File'); ?></h2>
             <div class="content-details">
