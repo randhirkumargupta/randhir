fa2f3d33bc8533951a55a2daed1e2ac4af324f68
Yogesh Kushwaha <ankush.gautam@kelltontech.com>
2016-04-25 15:15:14 +0530

    “ITG-94

--- sites/all/themes/rubik/templates/node--story.tpl.php
+++ sites/all/themes/rubik/templates/node--story.tpl.php
@@ -77,7 +77,12 @@
                 <?php print render($content['field_story_itg_tags']); ?>
               <?php print render($content['field_story_courtesy']); ?>
               <?php print render($content['field_story_reporter']); ?>
-                <div class="field field-name-field-story-category field-type-taxonomy-term-reference field-label-above"><div class="field-label">Story Body:&nbsp;</div><div class="field-items"><div class="field-item even"><?php print render($content['body']); ?></div></div></div>
+                <div class="field field-name-field-story-body">
+                    <div class="field-label">Story Body:&nbsp;</div>
+                    <div class="field-items">
+                    <div class="field-item even"><?php print render($content['body']); ?></div>
+                    </div>
+                </div>
                 
             </div>
           </div>
