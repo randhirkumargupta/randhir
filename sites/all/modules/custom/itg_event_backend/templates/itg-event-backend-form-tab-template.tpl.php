1f3068e53f1e3b6acd1c2d4ae1ef8e1cf4bb9e96
vedprakash.pareek <vedprakash.pareek@kelltontech.com>
2016-05-11 11:16:12 +0530

    ITG-1136 #comment event backend content type and sponsor content type

---
+++ sites/all/modules/custom/itg_event_backend/templates/itg-event-backend-form-tab-template.tpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file
+ * Theme implementation for poll form in tab display.
+ * 
+ */
+
+?>
+<div class="block-itg-story-list">
+<span class="target-link" data-target-id="GalleryIndividualImages"><?php echo t('Basic Details'); ?></span>
+<span class="target-link" data-target-id="Configuration"><?php echo t('Configuration'); ?></span>
+<span class="target-link" data-target-id="title-metatags"><?php echo t('Meta Details'); ?></span>
+</div>
