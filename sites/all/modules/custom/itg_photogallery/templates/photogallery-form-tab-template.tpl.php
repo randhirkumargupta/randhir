05c1cf86367756624a3da7ab4659d67fcd31f6fa
Yogesh Kushwaha <yogesh.kushwaha@kelltontech.com>
2016-05-25 16:38:28 +0530

    ITG-1236 #time 4h #commit Social media fields are added for photogallery story and recipe

--- sites/all/modules/custom/itg_photogallery/templates/photogallery-form-tab-template.tpl.php
+++ sites/all/modules/custom/itg_photogallery/templates/photogallery-form-tab-template.tpl.php
@@ -10,6 +10,7 @@
 <span class="target-link" data-target-id="Element">Gallery Basic details</span> 
 <span class="target-link" data-target-id="BrowseMedia">Gallery Cover Image</span>
 <span class="target-link" data-target-id="GalleryIndividualImages">Gallery Images Upload</span>
+<span class="target-link" data-target-id="SocialMedia"><?php print t('Social Media'); ?></span>
 <span class="target-link" data-target-id="Configuration">Configuration</span>
 <span class="target-link" data-target-id="Categorization">Categorization</span>
 <span class="target-link" data-target-id="title-metatags">Meta Details</span>
