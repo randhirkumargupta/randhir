9d28340b346f8533aade331c1ad365c2cc35e011
Avanish Singh <avanish@electricsage.com>
2016-05-26 12:53:10 +0530

    Resolve ui issues

--- sites/all/themes/itgadmin/js/script.js
+++ sites/all/themes/itgadmin/js/script.js
@@ -217,7 +217,9 @@ Drupal.behaviors.rubik.attach = function(context, settings) {
   $('.photogallery-list').flexslider({
     animation: "slide",
     slideshowSpeed: 3000,
-    controlNav: false
+    controlNav: false,
+    prevText: "<i class='fa fa-angle-left' aria-hidden='true'></i>",
+    nextText: "<i class='fa fa-angle-right' aria-hidden='true'></i>"
   });
   
   // jQuery code to hide select option whenever user hover on ITGCMS navbar
