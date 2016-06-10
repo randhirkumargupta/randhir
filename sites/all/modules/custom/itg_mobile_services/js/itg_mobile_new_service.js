d4d163b2e1282eee473f71b2e10574eb82ba1448
arun2016 <mishrasoft2009@gmail.com>
2016-06-10 20:26:08 +0530

    ITG-1367 reslove bug for mobile vas

--- sites/all/modules/custom/itg_mobile_services/js/itg_mobile_new_service.js
+++ sites/all/modules/custom/itg_mobile_services/js/itg_mobile_new_service.js
@@ -37,27 +37,38 @@
                     $('#itg-group-service-ftp').show();
                 }
             });
-            
-            
-                //called when key is pressed in textbox
-                $("#edit-field-service-content-count-und-0-value").keypress(function (e) {
-                   //if the letter is not digit then display error and don't type anything
-                   if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
-                      //display error message
-                      return false;
-                  }
-                 });
-                 
-                //called when key is pressed in Field char limit
-                $("#edit-field-service-char-limit-und-0-value").keypress(function (e) {
-                   //if the letter is not digit then display error and don't type anything
-                   if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
-                      //display error message
-                      return false;
-                  }
-                 });
-                 
-                jQuery('#edit-field-story-expiry-date-und-0-value-datepicker-popup-2').datepicker({minDate:0}).attr('readonly','readonly'); 
+
+
+            var selectedVal = "";
+            var selected = $("#edit-field-content-sharing-mode-und input[type='radio']:checked");
+            if (selected.length > 0) {
+                selectedVal = selected.val();
+                if (selectedVal == 2) {
+                    $('#itg-group-service-ftp').show();
+                } else if (selectedVal == 3) {
+                    $('#edit-field-service-email-address').show();
+                }
+            }
+
+            //called when key is pressed in textbox
+            $("#edit-field-service-content-count-und-0-value").keypress(function (e) {
+                //if the letter is not digit then display error and don't type anything
+                if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+                    //display error message
+                    return false;
+                }
+            });
+
+            //called when key is pressed in Field char limit
+            $("#edit-field-service-char-limit-und-0-value").keypress(function (e) {
+                //if the letter is not digit then display error and don't type anything
+                if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+                    //display error message
+                    return false;
+                }
+            });
+
+            jQuery('#edit-field-story-expiry-date-und-0-value-datepicker-popup-2').datepicker({minDate: 0}).attr('readonly', 'readonly');
         }
     }
 })(jQuery);
