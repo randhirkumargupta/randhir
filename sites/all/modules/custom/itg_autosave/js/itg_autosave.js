40c87dcd6e9f7dab1a4c72d181f8b1c7e016525b
Yogesh Kushwaha <yogesh.kushwaha@kelltontech.com>
2016-07-12 17:51:05 +0530

    ITG auto save module js validation uncommented

--- sites/all/modules/custom/itg_autosave/js/itg_autosave.js
+++ sites/all/modules/custom/itg_autosave/js/itg_autosave.js
@@ -59,43 +59,43 @@
       }
       
       // Custom validator function for social media start
-//      $("#itg-autosave-form").validate({
-//        onfocusout: function (element) {
-//          $(element).valid();
-//        },
-//        ignore: '',
-//        errorElement: 'span',
-//        errorPlacement: function (error, element) {
-//          var elementName = element.attr('name');
-//          var errorPlaceHolder = '';
-//          switch (elementName) {
-//            default:
-//              errorPlaceHolder = element.parent();
-//          }
-//          error.appendTo(errorPlaceHolder);
-//        },
-//        rules: {
-//          'itg_form_id': {
-//            required: true,
-//            validate_form_id: true,
-//          },
-//          'time_interval': {
-//            required: true,
-//            number: true
-//          }
-//        },
-//        messages: {
-//          'itg_form_id': {
-//            required: 'This field is required.',
-//          },
-//          'time_interval': {
-//            required: 'This field is required.'
-//          }
-//        }
-//      });
-//      jQuery.validator.addMethod("validate_form_id", function (value, element) {
-//        return this.optional(element) || /^[A-Za-z][A-Za-z0-9]*(?:_[A-Za-z0-9]+)*$/.test(value);
-//      }, 'Please enter valid form id.');
+      $("#itg-autosave-form").validate({
+        onfocusout: function (element) {
+          $(element).valid();
+        },
+        ignore: '',
+        errorElement: 'span',
+        errorPlacement: function (error, element) {
+          var elementName = element.attr('name');
+          var errorPlaceHolder = '';
+          switch (elementName) {
+            default:
+              errorPlaceHolder = element.parent();
+          }
+          error.appendTo(errorPlaceHolder);
+        },
+        rules: {
+          'itg_form_id': {
+            required: true,
+            validate_form_id: true,
+          },
+          'time_interval': {
+            required: true,
+            number: true
+          }
+        },
+        messages: {
+          'itg_form_id': {
+            required: 'This field is required.',
+          },
+          'time_interval': {
+            required: 'This field is required.'
+          }
+        }
+      });
+      jQuery.validator.addMethod("validate_form_id", function (value, element) {
+        return this.optional(element) || /^[A-Za-z][A-Za-z0-9]*(?:_[A-Za-z0-9]+)*$/.test(value);
+      }, 'Please enter valid form id.');
       // Custom validator function end here.
 
       // Custom code ends here
