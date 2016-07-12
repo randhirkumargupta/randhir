6db736f9b93034b9794e224e840bf1d02d031abc
Yogesh Kushwaha <yogesh.kushwaha@kelltontech.com>
2016-07-12 17:44:08 +0530

    added all changes to new branch

--- sites/all/modules/custom/itg_autosave/includes/ItgAutoSave.class.php
+++ sites/all/modules/custom/itg_autosave/includes/ItgAutoSave.class.php
@@ -70,8 +70,11 @@ class ItgAutoSave {
   
   /**
    * Pass form ids to js file
+   * @param int $nid 
+   * @param string $c_type
    */
-  public function itg_save_set_form_ids() {
+  public function itg_save_set_form_ids($nid, $c_type) {
+    global $base_url;
     $this->itg_query = db_select('itg_autosave_forms', 'itg');
     $this->itg_query->fields('itg', array('form_name', 'autosave_time'));
     $this->itg_result = $this->itg_query->execute()->fetchAll();
@@ -83,6 +86,273 @@ class ItgAutoSave {
       );
     }
     // Pass data to js file
-    drupal_add_js(array('itg_autosave' => array('auto_settings' => $this->data)), array('type' => 'setting'));
+    drupal_add_js(array(
+      'itg_autosave' => array(
+        'auto_settings' => $this->data, 
+        'base_url' => $base_url, 
+        'nid' => $nid,
+        'c_type' => $c_type
+      )
+    ), 
+    array(
+      'type' => 'setting'
+    ));
+  }
+  
+  /**
+   * Extract field type available on any content type
+   */  
+  public function itg_save_extract_fieldtype($inputs) {
+    $this->itgResult = array();
+    foreach ($inputs as $key => $value) {
+      $this->itgQuery = field_info_field($key);
+      $this->itgResult[] = array(
+        'fieldName' => $this->itgQuery['field_name'],
+        'fieldType' => $this->itgQuery['type'],
+      );
+    }
+    
+    return $this->itgResult;
+  }
+  
+  /**
+   * Prepare presave array for file field.
+   * 
+   * @param array $field
+   * @param array $value
+   * @return array 
+   */
+  public function itg_save_field_file($op, $value) {
+    if ($op == 'insert') {
+      $this->data = array();
+      $this->data['fid'] = $value['und'][0]['fid'];
+      $this->data['display'] = $value['und'][0]['display'];
+      $this->data['description'] = $value['und'][0]['description'];
+    }    
+    
+    return $this->data;
+  }
+  
+  /**
+   * Prepare presave arrave for text field
+   * 
+   * @param array $field
+   * @param array $value
+   * @return array
+   */
+  public function itg_save_field_text($op, $value) {
+    if ($op == 'insert') {
+      $this->data = '';
+      $this->data = $value['und'][0]['value'];
+    }    
+    
+    return $this->data;
+  }
+  
+  /**
+   * Prepare presave array for term reference field.
+   * 
+   * @param array $field
+   * @param array $value
+   * @return array
+   */
+  public function itg_save_field_taxonomy_term_reference($op, $value) {
+    if ($op == 'insert') {
+      $this->data = '';
+      $this->data = $value['und'];
+    }    
+    
+    return $this->data;
+  }
+  
+  /**
+   * Prepare presave array for datestamp field.
+   * 
+   * @param array $field
+   * @param array $value
+   * @return array
+   */
+  public function itg_save_field_datestamp($op, $value) {
+    if ($op = 'insert') {
+      $this->data = array();
+      $this->data['value'] = strtotime($value['und'][0]['value']['date']);;
+      $this->data['value2'] = strtotime($value['und'][0]['value2']['date']);
+    }
+        
+    return $this->data;
+  }
+  
+  /**
+   * Prepare presave array for datetime field.
+   * 
+   * @param array $field
+   * @param array $value
+   * @return array
+   */
+  public function itg_save_field_datetime($op, $value) {
+    if ($op == 'insert') {
+      $this->data = '';
+      $this->itg_query = $value['und'][0]['value']['date'];
+      $this->itg_result = date("Y-m-d H:i:s", strtotime($this->itg_query));
+      $this->data = $this->itg_result;
+    }    
+    
+    return $this->data;
+  }
+  
+  /**
+   * Prepare presave array for long text field.
+   * 
+   * @param array $field
+   * @param array $value
+   * @return array
+   */
+  public function itg_save_field_text_long($op, $value) {
+    if ($op == 'insert') {
+      $this->data = '';
+      $this->data = $value['und'][0]['value'];
+    }    
+    
+    return $this->data;
+  }
+  
+  /**
+   * Prepare presave array for image field.
+   * 
+   * @param array $field
+   * @param array $value
+   * @return array
+   */
+  public function itg_save_field_image($op, $value) {    
+    if ($op == 'insert') { 
+      $this->data = array();
+      $this->data['fid'] = $value['und'][0]['fid'];
+      $this->data['alt'] = $value['und'][0]['alt'];
+      $this->data['title'] = $value['und'][0]['title'];
+    }    
+    
+    return $this->data;
+  }
+  
+  /**
+   * Prepare presave array for image field.
+   * 
+   * @param array $field
+   * @param array $value
+   * @return array
+   */
+  public function itg_save_field_text_with_summary($op, $value) {   
+    if ($op == 'insert') {
+      $this->data = '';
+      $this->data = $value['und'][0]['value'];
+    }    
+    
+    return $this->data;
+  }
+  
+  /**
+   * Prepare presave array for list text field.
+   * 
+   * @param array $field
+   * @param array $value
+   * @return array
+   */
+  public function itg_save_field_list_text($op, $value) {   
+    if ($op == 'insert') {
+      $this->data = '';
+      $this->data = $value['und'];
+    }    
+    
+    return $this->data;
+  }
+  
+  /**
+   * Save form data into database to fullfill need of autosave.
+   * @param type $pre_save
+   */
+  public function itg_save_form_data($pre_save, $nid, $ctype) {
+    global $user;        
+    $this->itg_query = array(
+      'nid' => $nid,
+      'username' => $user->name,
+      'node_type' => $ctype,
+      'created' => REQUEST_TIME,
+      'form_data' => $pre_save,
+    );
+    drupal_write_record('itg_autosave_node_data', $this->itg_query);
+  }
+  
+  public function itg_save_retrieve_form_data($nid, $ctype) {
+    $this->itg_query = db_select('itg_autosave_node_data', 'itg');
+    $this->itg_query->fields('itg')
+        ->condition('node_type', $ctype);
+    if ($node > 0) {
+      $this->itg_query->condition('nid', $nid);
+    }
+    $this->itg_query->orderBy('id', 'DESC')
+        ->range(0, 1);
+    $this->itg_result = $this->itg_query->execute()->fetchObject();
+    
+    return $this->itg_result;
+  }
+  
+  /**
+   * Prepare retrieve data arrar for node.
+   * 
+   * @param array $form
+   * @param string $field_name
+   * @param string $field_value
+   * @param string $type
+   */
+  public function itg_save_prepare_retrieve_array(&$form, $field_name, $field_value, $type) {
+    switch ($type) {
+      case 'datestamp':
+        $form[$field_name]['und'][0]['#default_value']['value'] = $field_value['value'];
+        $form[$field_name]['und'][0]['#default_value']['value2'] = $field_value['value2'];
+        break;
+      
+      case 'datetime':
+        $form[$field_name]['und'][0]['#default_value']['value'] = $field_value;
+        break;
+      
+      case 'text':
+        $form[$field_name]['und'][0]['value']['#default_value'] = $field_value;
+        break;
+      
+      case 'taxonomy_term_reference':        
+        if (is_array($field_value)) {
+          $form[$field_name]['und']['#default_value'] = $field_value;
+        }
+        else {
+          $form[$field_name]['und']['#default_value'][0] = $field_value;
+        }
+        break;
+      
+      case 'field_collection':
+        //$pre_save[] = $itg_auto_save->itg_save_field_field_collection($field);
+        break;
+      
+      case 'text_long':
+        $form[$field_name]['und'][0]['value']['#default_value'] = strip_tags($field_value);
+        break;
+      
+      case 'image':       
+        $form[$field_name]['und'][0]['#default_value']['fid'] = $field_value['fid'];
+        $form[$field_name]['und'][0]['#default_value']['alt'] = $field_value['alt'];
+        $form[$field_name]['und'][0]['#default_value']['title'] = $field_value['title'];
+        break;
+      
+      case 'file':        
+        $form[$field_name]['und'][0]['#default_value']['fid'] = $field_value['fid'];
+        $form[$field_name]['und'][0]['#default_value']['description'] = $field_value['description'];
+        $form[$field_name]['und'][0]['#default_value']['display'] = $field_value['display'];
+        break;
+      
+      case 'text_with_summary':
+        //$pre_save[$field['fieldName']] = $itg_auto_save->itg_save_field_text_with_summary('insert', $_POST[$field['fieldName']]);
+        break;
+      case 'list_text':
+        $form['field_astro_frequency']['und']['#default_value'] = $field_value;
+    }
   }
 }
--- sites/all/modules/custom/itg_autosave/js/itg_autosave.js
+++ sites/all/modules/custom/itg_autosave/js/itg_autosave.js
@@ -12,50 +12,90 @@
         base_url: function () {
           return Drupal.settings.itg_autosave.base_url;
         },
-        autosave_for_settings: function() {          
-          var form_ids = Drupal.settings.itg_autosave.auto_settings;
-        } 
+        autosave_for_settings: function () {
+          var form_ids = Drupal.settings.itg_autosave.auto_settings;          
+          var formName = $('.page-node input[name="form_id"]').val();
+          if (formName != undefined && formName.length != 0) {
+            for (var property in form_ids) {
+              if (formName === property) {
+                var formId = formName.replace(/_/g, '-');
+                var time_int = form_ids[formName][1];
+                FormValidation.autosave_set_interval(formName, formId, time_int);                
+              }
+            }
+          }
+        },
+        autosave_set_interval: function (formName, formId, time_int) {          
+          // Auto save function                    
+          setInterval(function () {
+            var base_url = Drupal.settings.itg_autosave.base_url;
+            var node_id = Drupal.settings.itg_autosave.nid;
+            var ctype = Drupal.settings.itg_autosave.c_type;
+            if ($('textarea').hasClass('ckeditor-mod')) {
+              for (instance in CKEDITOR.instances) {
+                CKEDITOR.instances[instance].updateElement();
+              }
+            }            
+            $.ajax({
+              url: base_url + '/itg-autosave/nodeform/' + node_id + '/' + ctype,
+              type: "post",
+              data: $("#" + formId).serialize(),
+              success: function (d) {
+                $('#console').find('.autosave').remove();
+                var cus_message1 = '<div class="messages status autosave">Form data have been successfully auto saved</div>';
+                $('#console').append(cus_message1);
+                $('#console').find('.autosave').fadeOut(10000);
+              }
+            });
+
+            
+          }, time_int * 1000);
+        }
       };
       
-      FormValidation.autosave_for_settings();
+      if ($('body').hasClass('page-node-edit') || $('body').hasClass('page-node-add')) {
+        
+        FormValidation.autosave_for_settings();
+      }
+      
       // Custom validator function for social media start
-      $("#itg-autosave-form").validate({
-        onfocusout: function (element) {
-          $(element).valid();
-        },
-        ignore: '',
-        errorElement: 'span',
-        errorPlacement: function (error, element) {
-          var elementName = element.attr('name');
-          var errorPlaceHolder = '';
-          switch (elementName) {
-            default:
-              errorPlaceHolder = element.parent();
-          }
-          error.appendTo(errorPlaceHolder);
-        },
-        rules: {
-          'itg_form_id': {
-            required: true,
-            validate_form_id: true,
-          },
-          'time_interval': {
-            required: true,
-            number: true
-          }
-        },
-        messages: {
-          'itg_form_id': {
-            required: 'This field is required.',            
-          },
-          'time_interval': {
-            required: 'This field is required.'
-          }
-        }
-      });
-      jQuery.validator.addMethod("validate_form_id", function (value, element) {
-        return this.optional(element) || /^[A-Za-z][A-Za-z0-9]*(?:_[A-Za-z0-9]+)*$/.test(value);
-      }, 'Please enter valid form id.');
+//      $("#itg-autosave-form").validate({
+//        onfocusout: function (element) {
+//          $(element).valid();
+//        },
+//        ignore: '',
+//        errorElement: 'span',
+//        errorPlacement: function (error, element) {
+//          var elementName = element.attr('name');
+//          var errorPlaceHolder = '';
+//          switch (elementName) {
+//            default:
+//              errorPlaceHolder = element.parent();
+//          }
+//          error.appendTo(errorPlaceHolder);
+//        },
+//        rules: {
+//          'itg_form_id': {
+//            required: true,
+//            validate_form_id: true,
+//          },
+//          'time_interval': {
+//            required: true,
+//            number: true
+//          }
+//        },
+//        messages: {
+//          'itg_form_id': {
+//            required: 'This field is required.',
+//          },
+//          'time_interval': {
+//            required: 'This field is required.'
+//          }
+//        }
+//      });
+//      jQuery.validator.addMethod("validate_form_id", function (value, element) {
+//        return this.optional(element) || /^[A-Za-z][A-Za-z0-9]*(?:_[A-Za-z0-9]+)*$/.test(value);
+//      }, 'Please enter valid form id.');
       // Custom validator function end here.
 
       // Custom code ends here
