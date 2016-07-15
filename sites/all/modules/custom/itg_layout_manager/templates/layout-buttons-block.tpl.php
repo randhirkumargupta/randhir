2b5424218e963ddcb11242e5345dcabc10cc4a02
shravan <shravan.kumar@kelltontech.com>
2016-07-15 13:02:49 +0530

    ITG-1287 #comment Worked on this task

--- sites/all/modules/custom/itg_layout_manager/templates/layout-buttons-block.tpl.php
+++ sites/all/modules/custom/itg_layout_manager/templates/layout-buttons-block.tpl.php
@@ -1,5 +1,5 @@
-<div class="layout-button" id="<?php print $_GET['template_name']?>">    
-    <a class="btn btn-save" href="javascript:void(0)" id="layout-button-save">Save</a>
+<div class="layout-button" id="<?php print $_GET['template_name']?>-button">    
+    <a class="btn btn-save" href="javascript:void(0)" id="layout-button-save">Publish</a>
     <a class="btn btn-preview" href="javascript:void(0)" id="layout-button-preview">Preview</a>
     <a class="btn btn-cancel mr-0" href="javascript:void(0)" id="layout-button-cancel">Cancel</a>
 </div>
