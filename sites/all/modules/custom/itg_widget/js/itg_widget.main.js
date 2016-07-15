668e903b1f213786cf51dfe08524be9e98fb1c9a
rajveergangwar <rajveer.gang@gmail.com>
2016-07-15 17:26:12 +0530

    ITG-1593 #comment works on bugs

--- sites/all/modules/custom/itg_widget/js/itg_widget.main.js
+++ sites/all/modules/custom/itg_widget/js/itg_widget.main.js
@@ -30,15 +30,45 @@ Drupal.behaviors.itg_widget = {
             var qid = jQuery(this).attr("data-queueid");
             var view_name = jQuery(this).attr("data-view");
             var view_page = jQuery(this).attr("data-page");
-            if (confirm('Are you sure ?')) {
-                jQuery.get("remove_from_widgets/" + nid + "/" + qid + "/" + view_name + "/" + view_page, function(data, status) {
-                    if (data == 'deleted') {
-                        window.location.reload();
-                    }
-                });
+            // get query parameter
+            var c_tid = get_url_parameter('field_story_category_tid');
+            var type = get_url_parameter('type');
+            if (typeof (c_tid) != "undefined" && c_tid !== null && typeof (type) != "undefined" && type !== null) {
+                if (confirm('Are you sure ?')) {
+                    jQuery.get("remove_from_widgets_section/" + nid + "/" + qid + "/" + view_name + "/" + view_page + "/" + c_tid + "/" + type, function(data, status) {
+                        if (data == 'deleted') {
+                            window.location.reload();
+                        }
+                    });
+                }
+                console.log("section and type found");
+            } else {
+                if (confirm('Are you sure ?')) {
+                    jQuery.get("remove_from_widgets/" + nid + "/" + qid + "/" + view_name + "/" + view_page, function(data, status) {
+                        if (data == 'deleted') {
+                            window.location.reload();
+                        }
+                        console.log("section and type not found");
+                    });
+                }
             }
 
-        })
+        });
     }
 
+};
+
+
+var get_url_parameter = function get_url_parameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
 };
