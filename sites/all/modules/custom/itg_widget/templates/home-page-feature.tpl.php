187c832a9e86bbbabe8889e2a7d0f1f1d78db451
Sunil Yadav <sunil.yadav@kelltontech.com>
2016-07-21 14:50:18 +0530

    UI Changes for featured story

--- sites/all/modules/custom/itg_widget/templates/home-page-feature.tpl.php
+++ sites/all/modules/custom/itg_widget/templates/home-page-feature.tpl.php
@@ -1,43 +1,43 @@
 <?php
-if(!empty($data)) {
-$entity_data = array();
-foreach ($data as $entity_data) {
-    $entity_info = get_required_data_from_entity_id($entity_data['entity_id']);
-    $entity_data[] = $entity_info[$entity_data['entity_id']];
-    ?>
-<?php } ?>
-<div class="featured-news">
-    <div class="featured-post featured-post-first">
-        <?php if (isset($entity_data[0]->field_story_extra_large_image) && $entity_data[0]->field_story_extra_large_image['und'][0]['uri']) : ?>
-            <a href="#" title="">
-                <img src="<?php print image_style_url("home_page_feature_large", $entity_data[0]->field_story_extra_large_image['und'][0]['uri']); ?>" />
-            </a>
-        <?php endif; ?>
-        <?php if (!empty($entity_data[0]->title)) : ?>
-            <h2><a href="#" title=""><?php echo $entity_data[0]->title ?></a></h2>   
-        <?php endif; ?>
+if (!empty($data)) {
+    $entity_data = array();
+    foreach ($data as $entity_data_node) {
+        $entity_info = get_required_data_from_entity_id($entity_data_node['entity_id']);
+        $entity_data[] = $entity_info[$entity_data_node['entity_id']];
+        ?>
+    <?php } ?>
+    <div class="featured-news">
+        <div class="featured-post featured-post-first">
+            <?php if (isset($entity_data[0]->field_story_extra_large_image) && $entity_data[0]->field_story_extra_large_image['und'][0]['uri']) : ?>
+                <a href="#" title="">
+                    <img src="<?php print image_style_url("home_page_feature_large", $entity_data[0]->field_story_extra_large_image['und'][0]['uri']); ?>" />
+                </a>
+            <?php endif; ?>
+            <?php if (!empty($entity_data[0]->title)) : ?>
+                <h2><a href="#" title=""><?php echo $entity_data[0]->title ?></a></h2>   
+            <?php endif; ?>
+        </div>
+        <div class="featured-post">
+            <?php if (isset($entity_data[1]->field_story_extra_large_image) && $entity_data[1]->field_story_extra_large_image['und'][0]['uri']) : ?>
+                <a href="#" title="">
+                    <img src="<?php print image_style_url("home_page_feature_small", $entity_data[1]->field_story_extra_large_image['und'][0]['uri']); ?>" />
+                </a>
+            <?php endif; ?>
+            <?php if (!empty($entity_data[1]->title)) : ?>
+                <h3><a href="#" title=""><?php echo $entity_data[1]->title ?></a></h3>   
+            <?php endif; ?>
+        </div>
+        <div class="featured-post">
+            <?php if (isset($entity_data[2]->field_story_extra_large_image) && $entity_data[2]->field_story_extra_large_image['und'][0]['uri']) : ?>
+                <a href="#" title="">
+                    <img src="<?php print image_style_url("home_page_feature_small", $entity_data[2]->field_story_extra_large_image['und'][0]['uri']); ?>" />
+                </a>
+            <?php endif; ?>
+            <?php if (!empty($entity_data[2]->title)) : ?>
+                <h3><a href="#" title=""><?php echo $entity_data[2]->title ?></a></h3>   
+            <?php endif; ?>
+        </div>
     </div>
-    <div class="featured-post">
-        <?php if (isset($entity_data[1]->field_story_extra_large_image) && $entity_data[1]->field_story_extra_large_image['und'][0]['uri']) : ?>
-            <a href="#" title="">
-                <img src="<?php print image_style_url("home_page_feature_small", $entity_data[1]->field_story_extra_large_image['und'][0]['uri']); ?>" />
-            </a>
-        <?php endif; ?>
-        <?php if (!empty($entity_data[1]->title)) : ?>
-            <h3><a href="#" title=""><?php echo $entity_data[1]->title ?></a></h3>   
-        <?php endif; ?>
-    </div>
-    <div class="featured-post">
-        <?php if (isset($entity_data[2]->field_story_extra_large_image) && $entity_data[2]->field_story_extra_large_image['und'][0]['uri']) : ?>
-            <a href="#" title="">
-                <img src="<?php print image_style_url("home_page_feature_small", $entity_data[2]->field_story_extra_large_image['und'][0]['uri']); ?>" />
-            </a>
-        <?php endif; ?>
-        <?php if (!empty($entity_data[2]->title)) : ?>
-            <h3><a href="#" title=""><?php echo $entity_data[2]->title ?></a></h3>   
-        <?php endif; ?>
-    </div>
-</div>
 <?php } else { ?>
- No Feature content found
+    No Feature content found
 <?php } ?>
