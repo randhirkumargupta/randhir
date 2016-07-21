3eefd2902d24eb0786a7f5d1a2b8bcbd657c9dff
Sunil Yadav <sunil.yadav@kelltontech.com>
2016-07-21 13:13:44 +0530

    ui chnages

--- sites/all/modules/custom/itg_widget/templates/home-page-feature.tpl.php
+++ sites/all/modules/custom/itg_widget/templates/home-page-feature.tpl.php
@@ -1,14 +1,43 @@
+<?php
+if(!empty($data)) {
+$entity_data = array();
+foreach ($data as $entity_data) {
+    $entity_info = get_required_data_from_entity_id($entity_data['entity_id']);
+    $entity_data[] = $entity_info[$entity_data['entity_id']];
+    ?>
+<?php } ?>
 <div class="featured-news">
     <div class="featured-post featured-post-first">
-        <a href="#" title=""><img src="<?php print base_path() ?>sites/all/themes/itg/images/f-news1.jpg" alt=""></a>
-       <h2><a href="#" title="">Attempts to a portray govt a and of anti-Dalit, says world a PM Modi at Ambedkar event</a></h2>   
+        <?php if (isset($entity_data[0]->field_story_extra_large_image) && $entity_data[0]->field_story_extra_large_image['und'][0]['uri']) : ?>
+            <a href="#" title="">
+                <img src="<?php print image_style_url("home_page_feature_large", $entity_data[0]->field_story_extra_large_image['und'][0]['uri']); ?>" />
+            </a>
+        <?php endif; ?>
+        <?php if (!empty($entity_data[0]->title)) : ?>
+            <h2><a href="#" title=""><?php echo $entity_data[0]->title ?></a></h2>   
+        <?php endif; ?>
     </div>
     <div class="featured-post">
-        <a href="#" title=""><img src="<?php print base_path() ?>sites/all/themes/itg/images/f-news2.jpg" alt=""></a>
-        <h3><a href="#" title="">NDMC plans make CP vehicle-free, shopkeepers say move could hit business</a></h3>   
+        <?php if (isset($entity_data[1]->field_story_extra_large_image) && $entity_data[1]->field_story_extra_large_image['und'][0]['uri']) : ?>
+            <a href="#" title="">
+                <img src="<?php print image_style_url("home_page_feature_small", $entity_data[1]->field_story_extra_large_image['und'][0]['uri']); ?>" />
+            </a>
+        <?php endif; ?>
+        <?php if (!empty($entity_data[1]->title)) : ?>
+            <h3><a href="#" title=""><?php echo $entity_data[1]->title ?></a></h3>   
+        <?php endif; ?>
     </div>
     <div class="featured-post">
-        <a href="#" title=""><img src="<?php print base_path() ?>sites/all/themes/itg/images/f-news3.jpg" alt=""></a>
-        <h3><a href="#" title="">Malaika Arora-Arbaaz Khan divorce: So this is the reason behind the couple's split</a></h3>
+        <?php if (isset($entity_data[2]->field_story_extra_large_image) && $entity_data[2]->field_story_extra_large_image['und'][0]['uri']) : ?>
+            <a href="#" title="">
+                <img src="<?php print image_style_url("home_page_feature_small", $entity_data[2]->field_story_extra_large_image['und'][0]['uri']); ?>" />
+            </a>
+        <?php endif; ?>
+        <?php if (!empty($entity_data[2]->title)) : ?>
+            <h3><a href="#" title=""><?php echo $entity_data[2]->title ?></a></h3>   
+        <?php endif; ?>
     </div>
 </div>
+<?php } else { ?>
+ No Feature content found
+<?php } ?>
