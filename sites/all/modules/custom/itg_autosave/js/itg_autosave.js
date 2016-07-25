97561533fc71a98c4fe2a19344429725296c1cc1
Yogesh Kushwaha <yogesh.kushwaha@kelltontech.com>
2016-07-25 20:59:34 +0530

    ITG-152 #time 10h #comment Auto save for astro is done. Autosave will work only for save as draft state. After publishing a node auto save won't work.

--- sites/all/modules/custom/itg_autosave/js/itg_autosave.js
+++ sites/all/modules/custom/itg_autosave/js/itg_autosave.js
@@ -12,11 +12,13 @@ jQuery(document).ready(function () {
     autosave_for_settings: function () {
       var form_ids = Drupal.settings.itg_autosave.auto_settings;
       var form_name = jQuery('.page-node input[name="form_id"]').val();
+      var form_id;
+      var time_int;
       if (form_name != undefined && form_name.length != 0) {
         for (var property in form_ids) {
           if (form_name === property) {
-            var form_id = form_name.replace(/_/g, '-');
-            var time_int = form_ids[form_name][1];
+            form_id = form_name.replace(/_/g, '-');
+            time_int = form_ids[form_name][1];
             FormValidation.autosave_set_interval(form_name, form_id, time_int);
           }
         }
@@ -38,16 +40,26 @@ jQuery(document).ready(function () {
           type: "post",
           data: jQuery("#" + form_id).serialize(),
           success: function (d) {
-            jQuery('#console').find('.autosave').remove();
-            var cus_message1 = '<div class="messages status autosave">Form data have been successfully auto saved</div>';
-            jQuery('#console').append(cus_message1);
-            jQuery('#console').find('.autosave').fadeOut(10000);
+            if (d == 0 || d == 1) {
+              jQuery('#content').find('.autosave').remove();
+              var cus_message = '<div class="status autosave">Form data have been successfully auto saved</div>';
+              jQuery('#content').prepend(cus_message);
+              jQuery('#content').find('.autosave').fadeOut(10000);
+            }
+            else {
+              jQuery('#content').find('.autosave').remove();
+              var cus_message = '<div class="error autosave">Getting error on autosave.</div>';
+              jQuery('#content').prepend(cus_message);
+              jQuery('#content').find('.autosave').fadeOut(10000);
+            }
           }
         });
       }, time_int * 1000);
     }
+    
   };
   if (jQuery('body').hasClass('page-node-edit') || jQuery('body').hasClass('page-node-add')) {
     FormValidation.autosave_for_settings();
   }
+
 });
