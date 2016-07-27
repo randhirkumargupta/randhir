630bbc87413ccf6e06636ae2a518820fa0fb9148
rajveergangwar <rajveer.gang@gmail.com>
2016-07-27 16:04:31 +0530

    ITG-1582 #comment dont work widget is worked

--- sites/all/modules/custom/itg_widget/templates/section-ordering-data.tpl.php
+++ sites/all/modules/custom/itg_widget/templates/section-ordering-data.tpl.php
@@ -2,8 +2,8 @@
     <?php
     $extra_large_image_url = "";
     foreach ($data as $count => $entity_data) {
-        $entity_info = get_required_data_from_entity_id($entity_data['entity_id']);
-        $entity = $entity_info[$entity_data['entity_id']];
+        $entity_info = get_required_data_from_entity_id($entity_data['nid']);
+        $entity = $entity_info[$entity_data['nid']];
         if ($count == 0 && (!empty($entity->field_story_extra_large_image['und'][0]['uri']) && isset($entity->field_story_extra_large_image['und'][0]['uri']))) {
             $extra_large_image_url = file_create_url($entity->field_story_extra_large_image['und'][0]['uri']);
         }
