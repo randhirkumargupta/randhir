74393a5668923a1b1ce04b9ab4019d78bdc6117b
user <pavnish.kumar@kelltontech.com>
2016-07-27 15:58:43 +0530

    ITG-1673 #comment worked on popup

--- sites/all/modules/custom/itg_image_croping/js/imagecroping.js
+++ sites/all/modules/custom/itg_image_croping/js/imagecroping.js
@@ -9,28 +9,32 @@
 
 
 (function($) {
-   jQuery('.image-preview img').live('click',function(){
-     var imageid=jQuery(this).parent().next().find('input[type=hidden]').val();
-    var fieldname=jQuery(this).parent().next().find('input[type=hidden]').attr('name');
-    
-     var fieldname=fieldname.split('[');
-   
-     
-         jQuery.ajax({
-                    url: Drupal.settings.basePath + 'imagetagedit',
-                    type: 'post',
-                    data: {'fid': imageid, 'field_name': fieldname},
-                    success: function(data) {
-                        $.colorbox({html:""+data+""});
-               
-                    
-                    },
-                    error: function(xhr, desc, err) {
-                    console.log(xhr);
-                    console.log("Details: " + desc + "\nError:" + err);
-                    }
-                    }); 
-   })
-})(jQuery);
+    jQuery('.image-preview img').live('click', function() {
+        var imageid = jQuery(this).parent().next().find('input[type=hidden]').val();
+        var fieldname = jQuery(this).parent().next().find('input[type=hidden]').attr('name');
 
+        var fieldname = fieldname.split('[');
+
+
+        jQuery.ajax({
+            url: Drupal.settings.basePath + 'imagetagedit',
+            type: 'post',
+            data: {'fid': imageid, 'field_name': fieldname},
+            success: function(data) {
+                $.colorbox({html: "" + data + ""});
+
+
+            },
+            error: function(xhr, desc, err) {
+                console.log(xhr);
+                console.log("Details: " + desc + "\nError:" + err);
+            }
+        });
+    })
+})(jQuery);
+function showimagerepopopu(iframeurl)
+{
+    jQuery.colorbox({href: iframeurl, iframe: true, width: "80%", height: "80%"});
+
+}
 
