80aae365a84a4a16a61cc4d8d256aad427197ae3
ishwar <ishwar.tiwari@kelltontech.com>
2016-08-03 12:36:32 +0530

    Worked for

---
+++ sites/all/modules/custom/itg_widget/templates/featured-video-carousel.tpl.php
@@ -0,0 +1,21 @@
+<?php 
+if (!empty($data)) { 
+    ?>
+    <div class="flexslider-video">
+        <ul class="slides"> 
+            <?php
+            foreach ($data as $entity_data_node) {
+                ?>
+                <li>
+                    <a href="<?php echo  $entity_data_node['node_url']; ?>"><?php print $entity_data_node['file_url']; ?></a>
+                    <p class="flex-count"><?php echo  $entity_data_node['count']; ?> Images</p>
+                    <p class="flex-caption"><?php print $entity_data_node['caption']; ?></p>
+                </li>
+    <?php } ?>
+        </ul>
+    </div>
+
+
+<?php } else { ?>
+    No Feature content found
+<?php } ?>
