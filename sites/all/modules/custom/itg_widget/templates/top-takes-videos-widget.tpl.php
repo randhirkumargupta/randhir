0c9b45a09bbfdcdb7df2122b3ae630f075e08a04
Sunil Yadav <sunil.yadav@kelltontech.com>
2016-08-04 20:13:44 +0530

    UI development

--- sites/all/modules/custom/itg_widget/templates/top-takes-videos-widget.tpl.php
+++ sites/all/modules/custom/itg_widget/templates/top-takes-videos-widget.tpl.php
@@ -1,10 +1,10 @@
 <div class="top-takes-video-container">
-<ul>
-  <h3><span><?php print t("Top Takes") ?></span></h3>
+<h3><span><?php print t("Top Takes") ?></span></h3>
+<ul>  
   <?php foreach ($data as $video_key => $video_data) { ?>
   <li id="top-takes-<?php echo $video_data->nid ?>" class="top-takes-video top-takes-list top-takes-<?php echo $video_key ?>"">
       <?php if (!empty($video_data->field_story_extra_large_image['und'][0]['uri'])) { ?>
-        <a href="<?php print drupal_get_path_alias("node/$video_data->nid"); ?>" class="pic">
+        <a href="<?php print drupal_get_path_alias("node/$video_data->nid"); ?>" class="pic">         
           <img width="88" height="66" src="<?php print image_style_url("thumbnail", $video_data->field_story_extra_large_image['und'][0]['uri']); ?>" />
         </a>
         <?php
@@ -16,7 +16,7 @@
         </a>
       <?php } ?>
       <?php if (!empty($video_data->title)) : ?>
-        <p class="title">
+        <p class="title">           
           <a href="<?php print drupal_get_path_alias("node/$video_data->nid"); ?>"><?php print $video_data->title; ?></a>
         </p>
       <?php endif; ?>
