8fcb31816267de3c537f4fa87564e5b75b6d42ce
Sunil Yadav <sunil.yadav@kelltontech.com>
2016-08-04 20:01:58 +0530

    UI development top takes

--- sites/all/modules/custom/itg_widget/templates/top-takes-videos-widget.tpl.php
+++ sites/all/modules/custom/itg_widget/templates/top-takes-videos-widget.tpl.php
@@ -1,25 +1,26 @@
-<ul class="top-takes-video">
+<div class="top-takes-video-container">
+<ul>
   <h3><span><?php print t("Top Takes") ?></span></h3>
   <?php foreach ($data as $video_key => $video_data) { ?>
-  <li id="top-takes-<?php echo $video_data->nid ?>" class="top-takes-list top-takes-<?php echo $video_key ?>"">
-      <?php if (!empty($video_data->title)) : ?>
-        <p class="title">
-          <a href="<?php print drupal_get_path_alias("node/$video_data->nid"); ?>"><?php print $video_data->title; ?></a>
-        </p>
-      <?php endif; ?>
-
+  <li id="top-takes-<?php echo $video_data->nid ?>" class="top-takes-video top-takes-list top-takes-<?php echo $video_key ?>"">
       <?php if (!empty($video_data->field_story_extra_large_image['und'][0]['uri'])) { ?>
-        <a href="#" class="pic">
+        <a href="<?php print drupal_get_path_alias("node/$video_data->nid"); ?>" class="pic">
           <img width="88" height="66" src="<?php print image_style_url("thumbnail", $video_data->field_story_extra_large_image['und'][0]['uri']); ?>" />
         </a>
         <?php
       }
       else {
         ?>
-        <a href="#" class="pic">
+        <a href="<?php print drupal_get_path_alias("node/$video_data->nid"); ?>" class="pic">
           <img width="88" height="66" src="<?php print base_path() . "/" . drupal_get_path('theme', 'itg'); ?>/images/default_for_all.png" />
         </a>
       <?php } ?>
+      <?php if (!empty($video_data->title)) : ?>
+        <p class="title">
+          <a href="<?php print drupal_get_path_alias("node/$video_data->nid"); ?>"><?php print $video_data->title; ?></a>
+        </p>
+      <?php endif; ?>
     </li>
   <?php } ?>
 </ul>
+</div>
