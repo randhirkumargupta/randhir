a1a32a7b5e765dde9cf3cbe3fa9f811fa2dcacd2
user <pavnish.kumar@kelltontech.com>
2016-08-10 15:52:04 +0530

    ITG-1528 #comment search

--- sites/all/modules/custom/itg_image_croping/templates/image_tagger.tpl.php
+++ sites/all/modules/custom/itg_image_croping/templates/image_tagger.tpl.php
@@ -359,7 +359,6 @@
             parent.jQuery(document).ajaxComplete(function(event, request, settings) {
 
                 if (settings.url.indexOf(field_name) >= 0) {
-
                     parent.jQuery('[name="' + getbame + '[alt]"]').val(jQuery('#img_alttext').val());
                     parent.jQuery('[name="' + getbame + '[title]"]').val(jQuery('#img_title').val());
                     parent.jQuery('[name="' + getbame + '[field_image_caption][und][0][value]"]').val(jQuery('#img_title').val());
