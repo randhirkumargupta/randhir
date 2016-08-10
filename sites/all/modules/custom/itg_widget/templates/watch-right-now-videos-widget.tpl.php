1892f453cc74589fb714ec94a4bcd14f0c45e074
rajveer singh <rajveer.gang@gmail.com>
2016-08-10 10:35:01 +0530

    #ITG-1770 #comment bug fixes , trim and image size is applied

--- sites/all/modules/custom/itg_widget/templates/watch-right-now-videos-widget.tpl.php
+++ sites/all/modules/custom/itg_widget/templates/watch-right-now-videos-widget.tpl.php
@@ -1,25 +1,31 @@
-<ul class="watch-right-now-video">
-  <h3><span><?php print t("Top Takes") ?></span></h3>
-  <?php foreach ($data as $video_key => $video_data) { ?>
-  <li id="watch-right-now-<?php echo $video_data->nid ?>" class="watch-right-now-list watch-right-now-<?php echo $video_key ?>"">
-      <?php if (!empty($video_data->title)) : ?>
-        <p class="title">
-          <a href="<?php print drupal_get_path_alias("node/$video_data->nid"); ?>"><?php print $video_data->title; ?></a>
-        </p>
-      <?php endif; ?>
+<?php if (!empty($data)) : ?>
+  <ul class="watch-right-now-video">
+    <h3><span><?php print t("Watch Right Now") ?></span></h3>
+    <?php foreach ($data as $video_key => $video_data) { ?>
+      <li id="watch-right-now-<?php echo $video_data->nid ?>" class="watch-right-now-list watch-right-now-<?php echo $video_key ?>"">
+        <?php if (!empty($video_data->title)) : ?>
+          <p class="title">
+            <a title="<?php print $video_data->title; ?>" href="<?php print drupal_get_path_alias("node/$video_data->nid"); ?>">
+              <?php echo mb_strimwidth($video_data->title, 0, 140, ".."); ?>
+            </a>
+          </p>
+        <?php endif; ?>
 
-      <?php if (!empty($video_data->field_story_extra_large_image['und'][0]['uri'])) { ?>
-        <a href="#" class="pic">
-          <img width="88" height="66" src="<?php print image_style_url("thumbnail", $video_data->field_story_extra_large_image['und'][0]['uri']); ?>" />
-        </a>
-        <?php
-      }
-      else {
-        ?>
-        <a href="#" class="pic">
-          <img width="88" height="66" src="<?php print base_path() . "/" . drupal_get_path('theme', 'itg'); ?>/images/default_for_all.png" />
-        </a>
-      <?php } ?>
-    </li>
-  <?php } ?>
-</ul>
+        <?php if (!empty($video_data->field_story_extra_large_image['und'][0]['uri'])) { ?>
+          <a href="#" class="pic">
+            <img  src="<?php print image_style_url("widget_small", $video_data->field_story_extra_large_image['und'][0]['uri']); ?>" />
+          </a>
+          <?php
+        }
+        else {
+          ?>
+          <a href="#" class="pic">
+            <img  src="<?php print base_path() . "/" . drupal_get_path('theme', 'itg'); ?>/images/default_for_all.png" />
+          </a>
+        <?php } ?>
+      </li>
+    <?php } ?>
+  </ul>
+<?php else : ?>
+  <span class="no-result-found"><?php print t("Content Not Found") ?></span>
+<?php endif; ?>
--- sites/all/modules/custom/itg_widget/templates/we-may-suggest.tpl.php
+++ sites/all/modules/custom/itg_widget/templates/we-may-suggest.tpl.php
@@ -1,37 +1,34 @@
-<?php
-$entity_data = array();
-if (!empty($data)) {
-  foreach ($data as $entity_data_node) {
-    $entity_info = get_required_data_from_entity_id($entity_data_node['nid']);
-    $entity_data[] = $entity_info[$entity_data_node['nid']];
-  }
-}
-?>
-<?php if (!empty($entity_data)) { ?>
+<?php if (!empty($data)) : ?>
   <div class="may-be-suggest-container">
     <h3><span><?php print t("May We Suggest") ?></span></h3>
     <ul>
-      <?php foreach ($entity_data as $key => $entity_info) { ?>
+      <?php foreach ($data as $key => $entity_info) { ?>
         <li class="may-we-suggest" id="may-be-suggest-<?php print $key ?>">
           <?php if (!empty($entity_info->field_story_extra_large_image['und'][0]['uri'])) { ?>
             <a href="#" class="pic">
-              <img width="88" height="66" src="<?php print image_style_url("thumbnail", $entity_info->field_story_extra_large_image['und'][0]['uri']); ?>" />
+              <img  src="<?php print image_style_url("widget_very_small", $entity_info->field_story_extra_large_image['und'][0]['uri']); ?>" />
             </a>
             <?php
           }
           else {
             ?>
             <a href="#" class="pic">
-              <img width="88" height="66" src="<?php print base_path() . "/" . drupal_get_path('theme', 'itg'); ?>/images/default_for_all.png" />
+              <img  src="<?php print base_path() . "/" . drupal_get_path('theme', 'itg'); ?>/images/default_for_all.png" />
             </a>
           <?php } ?>
           <?php if (!empty($entity_info->title)) : ?>
-            <p class="title"><?php print $entity_info->title; ?></p>
+            <p class="title may-be-suggest-<?php echo $entity_info->nid ?>">
+              <a title="<?php echo $entity_info->title; ?>" href="<?php print drupal_get_path_alias("node/$entity_info->nid"); ?>">
+                <?php echo mb_strimwidth($entity_info->title, 0, 90, ".."); ?>
+              </a>  
+            </p>
           <?php endif; ?>
 
         </li>        
       <?php } ?>
     </ul>
   </div>
-<?php } ?>
+<?php else : ?>
+  <span class="no-result-found"><?php print t("Content Not Found") ?></span>
+<?php endif; ?>
 
