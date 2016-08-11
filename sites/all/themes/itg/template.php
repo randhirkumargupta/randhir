1eeac485a092317bf72b8ea5994868374f7df2df
rajveer singh <rajveer.gang@gmail.com>
2016-08-11 15:17:57 +0530

    foratting fo tempalte.php file is done ,  extra file is removed and code is optimize

--- sites/all/themes/itg/template.php
+++ sites/all/themes/itg/template.php
@@ -2,153 +2,17 @@
 
 /**
  * @file
+ * template.php
  * Contains the theme's functions to manipulate Drupal's default markup.
  *
  * Complete documentation for this file is available online.
  * @see https://drupal.org/node/1728096
  */
-/**
- * Override or insert variables into the maintenance page template.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("maintenance_page" in this case.)
- */
-/* -- Delete this line if you want to use this function
-  function itg_preprocess_maintenance_page(&$variables, $hook) {
-  // When a variable is manipulated or added in preprocess_html or
-  // preprocess_page, that same work is probably needed for the maintenance page
-  // as well, so we can just re-use those functions to do that work here.
-  itg_preprocess_html($variables, $hook);
-  itg_preprocess_page($variables, $hook);
-  }
-  // */
 
 /**
- * Override or insert variables into the html templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("html" in this case.)
+ * Implementation of hook_theme()
+ * {@inheritdoc}
  */
-/* -- Delete this line if you want to use this function
-  function itg_preprocess_html(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
-
-  // The body tag's classes are controlled by the $classes_array variable. To
-  // remove a class from $classes_array, use array_diff().
-  //$variables['classes_array'] = array_diff($variables['classes_array'], array('class-to-remove'));
-  }
-  // */
-
-/**
- * Override or insert variables into the page templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("page" in this case.)
- */
-/* -- Delete this line if you want to use this function
-  function itg_preprocess_page(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
-  }
-  // */
-
-/**
- * Override or insert variables into the node templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("node" in this case.)
- */
-/* -- Delete this line if you want to use this function
-  function itg_preprocess_node(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
-
-  // Optionally, run node-type-specific preprocess functions, like
-  // itg_preprocess_node_page() or itg_preprocess_node_story().
-  $function = __FUNCTION__ . '_' . $variables['node']->type;
-  if (function_exists($function)) {
-  $function($variables, $hook);
-  }
-  }
-  // */
-
-/**
- * Override or insert variables into the comment templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("comment" in this case.)
- */
-/* -- Delete this line if you want to use this function
-  function itg_preprocess_comment(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
-  }
-  // */
-
-/**
- * Override or insert variables into the region templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("region" in this case.)
- */
-/* -- Delete this line if you want to use this function
-  function itg_preprocess_region(&$variables, $hook) {
-  // Don't use Zen's region--sidebar.tpl.php template for sidebars.
-  //if (strpos($variables['region'], 'sidebar_') === 0) {
-  //  $variables['theme_hook_suggestions'] = array_diff($variables['theme_hook_suggestions'], array('region__sidebar'));
-  //}
-  }
-  // */
-
-/**
- * Override or insert variables into the block templates.
- *
- * @param $variables
- *   An array of variables to pass to the theme template.
- * @param $hook
- *   The name of the template being rendered ("block" in this case.)
- */
-/* -- Delete this line if you want to use this function
-  function itg_preprocess_block(&$variables, $hook) {
-  // Add a count to all the blocks in the region.
-  // $variables['classes_array'][] = 'count-' . $variables['block_id'];
-
-  // By default, Zen will use the block--no-wrapper.tpl.php for the main
-  // content. This optional bit of code undoes that:
-  //if ($variables['block_html_id'] == 'block-system-main') {
-  //  $variables['theme_hook_suggestions'] = array_diff($variables['theme_hook_suggestions'], array('block__no_wrapper'));
-  //}
-  }
-  // */
-
-/**
- * Output customized node preview on node edit and add forms.
- *
- * @return
- * This just outputs the Full node ignoring the teaser mode.
- */
-//  function YOURTHEME_Node_preview($variables)
-//  {
-//      $node = $variables['node'];
-//      $elements = node_view($node, 'full');
-//      $full = drupal_render($elements);
-//      $output = '<div class="preview">';
-//      $output .= '<h3 class="post-preview" >' . t('Preview of your posting') . '</h3>';
-//      $output .= $full;
-//      $output .= "</div>\n";
-//      return $output;
-//  }
-
-
 function itg_theme() {
   $items = array();
   $items['user_login'] = array(
@@ -164,12 +28,18 @@ function itg_theme() {
   return $items;
 }
 
-// hide read more link from front page 
+/**
+ * Hide read more link from front page
+ * {@inheritdoc}
+ */
 function itg_preprocess_node(&$variables) {
   unset($variables['content']['links']['node']['#links']['node-readmore']);
 }
 
-// change comment date format
+/**
+ * Change comment date format
+ * {@inheritdoc}
+ */
 function itg_preprocess_comment(&$variables) {
   $comment = $variables['elements']['#comment'];
   $node = $variables['elements']['#node'];
@@ -183,6 +53,7 @@ function itg_preprocess_comment(&$variables) {
 
 /**
  * Override the video field
+ * {@inheritdoc}
  */
 function itg_preprocess_field(&$vars) {
   if ($vars['element']['#field_name'] == 'field_upload_video') {
@@ -195,24 +66,15 @@ function itg_preprocess_field(&$vars) {
 }
 
 /**
- * Preprocessor for theme('page').
- */
-//function itg_preprocess_page(&$vars) {
-// //page__section-photo.tpl
-// $vars['theme_hook_suggestions'][] = 'page__section_photo';
-//}
-
-/**
- * Override category_management vocuavolry page teamplate.
+ * {@inheritdoc}
  */
 function itg_preprocess_page(&$variables) {
   $arg = arg();
-
   if ($arg[0] == 'taxonomy' && $arg[1] == 'term') {
     $term = taxonomy_term_load($arg[2]);
-    if($term->vocabulary_machine_name  = 'category_management') {
+    if ($term->vocabulary_machine_name = 'category_management') {
+      // remove the extra vocavolary information on page buttom.
       unset($variables['page']['content']['system_main']);
-      $variables['theme_hook_suggestions'][] = 'page__category_management';
     }
   }
 }
