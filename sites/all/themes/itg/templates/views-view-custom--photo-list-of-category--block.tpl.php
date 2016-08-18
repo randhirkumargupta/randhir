fbf7bb0ecb5e33a078a3480dfef916dad1c768db
shravan <shravan.kumar@kelltontech.com>
2016-08-18 14:55:26 +0530

    worked on layout issue

---
+++ sites/all/themes/itg/templates/views-view-custom--photo-list-of-category--block.tpl.php
@@ -0,0 +1,19 @@
+<?php
+ if (isset($_GET['category'])) {
+   $section_cat_id = $_GET['category'];
+ }
+ else {
+   $section_cat_id = get_first_category_of_media_widget(arg(2), 'page--section_photo', 'photo_list_of_category');    
+ }
+?>
+
+<?php foreach($rows as $index => $row): ?>
+<figure>
+  <?php $img = $row['field_story_extra_large_image']; ?>
+  <?php print l($img, 'node/'.$row['nid'], array('query' => array('category' => $section_cat_id, 'sid' => arg(2)), 'html' => TRUE)); ?>
+  <figcaption><i class="fa fa-camera" aria-hidden="true"></i><?php print $row['delta']; ?></figcaption>
+</figure>
+<span class="posted-on"><?php print $row['created']; ?></span>
+<?php $title = $row['title'];?>
+<?php print l($title, 'node/'.$row['nid'], array('query' => array('category' => $section_cat_id, 'sid' => arg(2)), 'html' => TRUE)); ?>
+<?php endforeach; ?>
