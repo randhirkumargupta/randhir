3ecb757cc5ea5c45b2744c771f84bd67ce417a3f
Sunil Yadav <sunil.yadav@kelltontech.com>
2016-08-18 17:50:48 +0530

    UI development

--- sites/all/themes/itg/templates/views-view-custom--video-list-of-category--block.tpl.php
+++ sites/all/themes/itg/templates/views-view-custom--video-list-of-category--block.tpl.php
@@ -1,7 +1,9 @@
+<ul class="photo-list">
 <?php foreach($rows as $index => $row): ?>
 <?php $img = $row['field_story_extra_large_image']; 
   $section_cat_id = $row['field_story_category']; 
 ?>
+    <li class="col-md-3">
  <figure>
 <figcaption><i class="fa fa-camera" ></i> <?php print l($img, 'node/'.$row['nid'], array('query' => array('category' => $section_cat_id, 'sid' => $_GET['sid']), 'html' => TRUE)); ?>
 <?php print $row['field_video_duration']; ?></figure>
@@ -10,7 +12,9 @@
 
       <span class="posted-on"><?php print $row['created']; ?></span>
 <?php print $row['field_story_expert_name'];?>
+      </li>
 <?php endforeach; ?>
+      </ul>
 <?php
 
 $term = end($row->_field_data['nid']['entity']->field_story_category[LANGUAGE_NONE]);
