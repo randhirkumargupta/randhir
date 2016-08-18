c5089d0f8b64212e653d3a8128ef8028311646a0
vedprakash.pareek <vedprakash.pareek@kelltontech.com>
2016-08-18 11:12:49 +0530

    ITG-1534  #Comment video landing page theme

--- sites/all/modules/custom/itg_videogallery/templates/videogallery-menu-landing-template.tpl.php
+++ sites/all/modules/custom/itg_videogallery/templates/videogallery-menu-landing-template.tpl.php
@@ -17,21 +17,21 @@ print '<ul class="video_landing_menu">' . $output . '</ul>';
 
 
 <?php
-//
-//drupal_add_js("jQuery('#block-itg-videogallery-videogallery-menu-video-block ul li').click(function(){
-//               var section_id = jQuery(this).val();
-//               jQuery('#block-itg-videogallery-videogallery-menu-video-block ul li a').removeClass('active');
-//               jQuery('#edit-field-story-category-tid').val(section_id); 
-//               jQuery('#edit-field-story-category-tid').trigger('change');
-//               jQuery(this).find('a').addClass('active');
-//           });", array('type' => 'inline', 'scope' => 'footer'));
-//drupal_add_js("jQuery(document).ready(function(){
-//               jQuery('#edit-field-story-category-tid').val(" . $_GET['category'] . "); 
-//               jQuery('#edit-field-story-category-tid').trigger('change');
-//               jQuery('#block-itg-videogallery-videogallery-menu-video-block ul li').each(function() {
-//                  if(jQuery(this).val() == " . $_GET['category'] . "){
-//                      jQuery(this).find('a').addClass('active');
-//                    }
-//              });
-//           });", array('type' => 'inline', 'scope' => 'footer'));
+
+drupal_add_js("jQuery('#block-itg-videogallery-videogallery-menu-video-block ul li').click(function(){
+               var section_id = jQuery(this).val();
+               jQuery('#block-itg-videogallery-videogallery-menu-video-block ul li a').removeClass('active');
+               jQuery('#edit-field-story-category-tid').val(section_id); 
+               jQuery('#edit-field-story-category-tid').trigger('change');
+               jQuery(this).find('a').addClass('active');
+           });", array('type' => 'inline', 'scope' => 'footer'));
+drupal_add_js("jQuery(document).ready(function(){
+               jQuery('#edit-field-story-category-tid').val(" . $_GET['category'] . "); 
+               jQuery('#edit-field-story-category-tid').trigger('change');
+               jQuery('#block-itg-videogallery-videogallery-menu-video-block ul li').each(function() {
+                  if(jQuery(this).val() == " . $_GET['category'] . "){
+                      jQuery(this).find('a').addClass('active');
+                    }
+              });
+           });", array('type' => 'inline', 'scope' => 'footer'));
 ?>
