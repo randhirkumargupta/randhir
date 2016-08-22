4ab21db3ee9874d87f1f30b60b6db1dc3163a567
Sunil Yadav <sunil.yadav@kelltontech.com>
2016-08-22 19:12:57 +0530

    ui Development video header

--- sites/all/themes/itg/templates/views-view-custom--video-list-of-category--block.tpl.php
+++ sites/all/themes/itg/templates/views-view-custom--video-list-of-category--block.tpl.php
@@ -5,6 +5,7 @@
     $section_cat_id = $row['field_story_category'];
     ?>
     <li class="col-md-3">
+        <div class="tile">
       <figure>
   <?php print l($img, 'node/' . $row['nid'], array('query' => array('category' => $section_cat_id, 'sid' => $_GET['sid']), 'html' => TRUE)); ?>
         <figcaption><i class="fa fa-camera" ></i> <?php print $row['field_video_duration']; ?></figcaption>
@@ -13,6 +14,7 @@
 
       <span class="posted-on"><?php print $row['created']; ?></span>
     <?php print l($row['title'], 'node/' . $row['nid'], array('query' => array('category' => $section_cat_id, 'sid' => $_GET['sid']), 'html' => TRUE)); ?>
+      </div>
     </li>
 <?php endforeach; ?>
 </ul>
