735514c257aeb3460ff7e27f99e184bb64188862
rajveer singh <rajveer.gang@gmail.com>
2016-08-26 16:07:13 +0530

    ITG-1619 #comment remove-extra-content for photo and video crosel has been added and not feature content found message has been added

--- sites/all/modules/custom/itg_widget/templates/featured-video-carousel-rollingslider.tpl.php
+++ sites/all/modules/custom/itg_widget/templates/featured-video-carousel-rollingslider.tpl.php
@@ -1,35 +1,37 @@
 <?php
 //p($entity_data_node);
 global $base_url;
-if (!empty($data)) {  ?>
+if (!empty($data)) {
+  ?>
 
   <div class="container">
     <div class="carousel">
       <div class="slides">                     
-        <?php $i = 1;
+        <?php
+        $i = 1;
         $countd = 1;
-        foreach ($data as $entity_data_node) { ?>
+        foreach ($data as $entity_data_node) {
+          ?>
 
           <div class="slideItem"> 
-              <a href="<?php echo $base_url?>/node/<?php print $entity_data_node['nid']; ?>">
-              <?php print $entity_data_node['file_url'];  ?>
-              <!--<img src="/itgcms/sites/all/themes/itg/images/demo-photo.jpg">-->
+            <a href="<?php echo $base_url ?>/node/<?php print $entity_data_node['nid']; ?>">
+    <?php print $entity_data_node['file_url']; ?>
+            <!--<img src="/itgcms/sites/all/themes/itg/images/demo-photo.jpg">-->
               <span class="flex-count" style="text-align: right"><i class="fa fa-play-circle"></i></span>
               <span class="pic-tit"><?php print $entity_data_node['title']; ?></span>
             </a>
-            
+
           </div>  
 
-        <?php $i++;
-} ?>
+    <?php $i++;
+  }
+  ?>
 
       </div>
     </div>
   </div>
 
-<?php } else { ?>
-  No Feature content found
 <?php } ?>
 
-  
-  
+
+
