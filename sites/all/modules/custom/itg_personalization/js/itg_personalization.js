62c76424fc8c999b443ef74ce08f9ae68a3c0c87
Yogesh Kushwaha <yogesh.kushwaha@kelltontech.com>
2016-08-29 16:24:33 +0530

    ITG-1814 #time 5h #comment saparate pages created for my saved content.

--- sites/all/modules/custom/itg_personalization/js/itg_personalization.js
+++ sites/all/modules/custom/itg_personalization/js/itg_personalization.js
@@ -4,6 +4,6 @@
  */
 jQuery(document).ready(function () {
     // Hide my saved content menu tab by default.
-    jQuery('#block-itg-personalization-personalized-saved-content').hide();
-    jQuery('#block-itg-personalization-user-my-content').hide();    
+    //jQuery('#block-itg-personalization-personalized-saved-content').hide();
+    //jQuery('#block-itg-personalization-user-my-content').hide();    
 });
