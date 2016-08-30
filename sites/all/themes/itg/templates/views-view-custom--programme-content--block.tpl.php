de60b15fe3cc6ce74d0bce4321928d951cab84ae
avanish.singh <avanish.singh@192.168.14.82>
2016-08-30 17:26:50 +0530

    Theme program page, personalize page, and resolve image repo issues

--- sites/all/themes/itg/templates/views-view-custom--programme-content--block.tpl.php
+++ sites/all/themes/itg/templates/views-view-custom--programme-content--block.tpl.php
@@ -1,43 +1,42 @@
-<div class="programe-video-view-container">
+<div class="programe-list">
   <?php
   $url = "#";
   foreach ($rows as $key => $row) :
     $section_cat_id = $row['field_story_category'];
     if ($key == 0) {
-      $url = l("More>>", 'node/' . $row['nid'], array('query' => array('category' => $section_cat_id), 'html' => TRUE));
+      $url = l("More »", 'node/' . $row['nid'], array('query' => array('category' => $section_cat_id), 'html' => TRUE));
     }
     if ($key > 3) {
       continue;
     }
     ?>
-    <div class="video-container">
+    <div class="col-md-3 tile-wrapper">
+      <div class="tile">
+        <figure>
       <?php if (isset($row['field_story_extra_large_image'])) : ?>
         <?php
         $img = $row['field_story_extra_large_image'];
         ?>
-        <div class="large-image">
           <?php print l($img, 'node/' . $row['nid'], array('query' => array('category' => $section_cat_id), 'html' => TRUE)); ?>
-        </div>
       <?php else : ?>
-        <div class="large-image">
           <?php
           global $base_url;
           $img = "<img width='170' height='127'  src='" . $base_url . '/' . drupal_get_path('theme', 'itg') . "/images/default_for_all.png' />";
           ?>
           <?php print l($img, 'node/' . $row['nid'], array('query' => array('category' => $section_cat_id), 'html' => TRUE)); ?>
-        </div>
+        
       <?php endif; ?>
 
       <?php if (isset($row['field_video_duration'])) : ?>
-        <div class="duration">
-          <?php print $row['field_video_duration']; ?>
-        </div>
+          <figcaption>
+          <i class="fa fa-play-circle"></i> <?php print $row['field_video_duration']; ?>
+          </figcaption>
       <?php endif; ?>
-
+        </figure>
       <?php if (isset($row['created'])) : ?>
-        <div class="date">
+        <span class="posted-on">
           <?php print $row['created']; ?>
-        </div>
+        </span>
       <?php endif; ?>
 
       <?php if (isset($row['title'])) : ?>
@@ -46,9 +45,10 @@
         </div>
       <?php endif; ?>
     </div>
+    </div>
     <?php
     if ($key == 3 && count($rows) > 3) {
-      print $url;
+      print '<div class="col-md-12"><div class="more">' . $url . '</div></div>';
     }
     ?>
   <?php endforeach; ?>
