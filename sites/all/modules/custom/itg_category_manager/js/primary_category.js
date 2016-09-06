9b47b15defb4d5a6d83f763b6dd674506bd1e77d
user <pavnish.kumar@kelltontech.com>
2016-09-06 18:41:55 +0530

    ITG-1920 #comment impliment for photogallery

--- sites/all/modules/custom/itg_category_manager/js/primary_category.js
+++ sites/all/modules/custom/itg_category_manager/js/primary_category.js
@@ -10,6 +10,7 @@
                 var selectvalue = jQuery('.selects > .form-select:last option:selected').val();
                 var comptext = "";
                 var makeradio = "";
+                var datahtml=jQuery('.dropbox-title').html();
                 var flag = 0;
                 if (selectvalue != "")
                 {
@@ -25,7 +26,7 @@
 
                     });
                     comptext = comptext.slice(0, -1);
-                    jQuery('#primary-category-data option').each(function()
+                    jQuery('.select-prim').each(function()
                     {
                         var existvalue = jQuery(this).val();
                         if (selectvalue == existvalue)
@@ -34,47 +35,80 @@
                         }
 
                     });
-                    if (comptext != "" && flag == 0)
-                    {
-                        makeradio = '<option value="' + selectvalue + '">' + comptext + '</option>';
-                        jQuery('#primary-category-data').append(makeradio);
+                    
 
-                        var gethtml = jQuery('#primary-category-data').html();
-                        jQuery('#edit-field-primary-category-html-und-0-value').val(gethtml);
+                    jQuery( document ).on('ajaxComplete',function( event, xhr, settings ) {
+
+  if (settings.url.indexOf('hierarchical_select_ajax') >= 0) {
+
+if (comptext != "" && flag == 0)
+                    {
+                       
+                        makeradio = '<span class="hiddenradio"><input type="radio" class="select-prim" value="' + selectvalue + '"><span class="primary-text">' + comptext + '</span></span>';
+                       datahtml = datahtml.replace(makeradio, ""); 
+                       datahtml=datahtml+makeradio;
+                                                jQuery('.dropbox-title').html(datahtml).hide();
+                     jQuery(".hiddenradio").each(function(){
+          var gettoptext=jQuery(this).find(".primary-text").text();
+          var selectvalue=jQuery(this).find(".select-prim").val();
+          
+        makeradio = '<input class="primary-radio" name="primary_radio" type=radio  value="'+selectvalue+'">';
+
+                        
+                         jQuery(".dropbox-entry").each(function(){
+                            var gettrtext=jQuery(this).children("td:first").text();
+                            if(gettoptext==gettrtext)
+                            {
+                                
+                               if(! jQuery(this).children("td:first").find('input').hasClass('primary-radio'))
+                               {
+                                jQuery(this).children("td:first").find(".dropbox-item:first").before(makeradio);
+                            }
+                            }
+                          });
+
+           });
+                        var gethtml = jQuery('.dropbox-title').html();
+                        jQuery('#edit-field-primary-category-html-text-und-0-value').val(gethtml);
                     }
+
+  }
+  
+});
+
                 }
 
             });
-            jQuery('.dropbox-remove a').click(function() {
-                var getdattext = jQuery(this).parent().siblings('td').text();
-               
-                $('#primary-category-data option').each(function() {
-                   
+            jQuery('.dropbox-remove a').live('mousedown',function() {
+              var getdattext = jQuery(this).parent().siblings('td').text();
+                jQuery( document ).on('ajaxComplete',function( event, xhr, settings ) {
+  if (settings.url.indexOf('hierarchical_select_ajax') >= 0) {
+
+       var getdoptiontext=jQuery(this).text();
+ jQuery('.dropbox-title .primary-text').each(function() {
+                
                     var getdoptiontext=jQuery(this).text();
+
                     if (getdoptiontext== getdattext) {
-                        jQuery(this).remove();
+                       
+                        jQuery(this).parent('.hiddenradio').remove();
                     }
                 });
-
+             
+}
+});
+                
             });
 
-            jQuery('#primary-category-data').change(function() {
+            jQuery('.primary-radio').live('mousedown',function() {
                 var getval = jQuery(this).val();
+
                 jQuery('#edit-field-primary-category-und-0-value').val(getval);
-                jQuery('#primary-category-data option').attr('selected', false);
-                jQuery('#primary-category-data option[value=' + getval + ']').attr('selected', 'selected');
-                var gethtml = jQuery('#primary-category-data').html();
-                jQuery('#edit-field-primary-category-html-und-0-value').val(gethtml);
+
+                var gethtml = jQuery('.dropbox-title').html();
+                
+                jQuery('#edit-field-primary-category-html-text-und-0-value').val(gethtml);
             });
 
 })(jQuery, Drupal, this, this.document);
 
-//   code load the selectd option and add to select box
-jQuery(window).load(function() {
-    // executes when complete page is fully loaded, including all frames, objects and images
-    var getvaluehtml = jQuery('#edit-field-primary-category-html-und-0-value').val();
-    if (getvaluehtml != "")
-    {
-        jQuery('#primary-category-data').html(getvaluehtml);
-    }
-});
