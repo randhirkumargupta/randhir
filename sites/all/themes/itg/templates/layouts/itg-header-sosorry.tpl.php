e5eb78f868ee7379f740427f73e379e837763bfb
sunil <sunil.kumar@kelltontech.com>
2016-09-08 17:13:44 +0530

    UI development so sory page

--- sites/all/themes/itg/templates/layouts/itg-header-sosorry.tpl.php
+++ sites/all/themes/itg/templates/layouts/itg-header-sosorry.tpl.php
@@ -1,17 +1,21 @@
 <div class="sosory-header">
     <div class="container">
-        <div class="sosory-left"><img class="sosorryitg" src="<?php print base_path() ?>sites/all/themes/itg/images/sosorryitg.jpg" alt=""><img src="<?php print base_path() ?>sites/all/themes/itg/images/sosorry.jpg" alt=""><img class="soindiatoday" src="<?php print base_path() ?>sites/all/themes/itg/images/india-today-logo.jpg" alt=""></div>
-        <div class="sosory-right">        
-            
+        <div class="sosory-left">
+            <a href="#"><img class="sosorryitg" src="<?php print base_path() ?>sites/all/themes/itg/images/sosorryitg.jpg" alt=""></a>
+            <a href="#"><img src="<?php print base_path() ?>sites/all/themes/itg/images/sosorry.jpg" alt=""></a>
+            <a href="#"><img class="soindiatoday" src="<?php print base_path() ?>sites/all/themes/itg/images/india-today-logo.jpg" alt=""></a>
         </div>
+<!--        <div class="sosory-right">        
+            
+        </div>-->
     </div>
 </div>
 <div class="sosory-footer">
     <div class="container">
         <ul>
-            <li><i class="fa fa-trophy"></i> BCS Ratna Award for Broadcast Excellence as Best Politoon</li>
-            <li><i class="fa fa-trophy"></i> FICCI-BAF Best Animation in the Open Category</li>
-            <li><i class="fa fa-trophy"></i> FWinner FICCI BAF Show of the Year</li>
+            <li><a href="#"><i class="fa fa-trophy"></i> BCS Ratna Award for Broadcast Excellence as Best Politoon</a></li>
+            <li><a href="#"><i class="fa fa-trophy"></i> FICCI-BAF Best Animation in the Open Category</a></li>
+            <li><a href="#"><i class="fa fa-trophy"></i> FWinner FICCI BAF Show of the Year</a></li>
         </ul>
     </div>
 </div>
