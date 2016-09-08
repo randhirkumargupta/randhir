ea0a1e8bf4c3947135b927dfad9c4b8f8096eb3f
Haripal Rao <haripal.rao@kelltontech.com>
2016-09-08 16:26:12 +0530

    ITG-1804 #comment Survey front-end page

--- sites/all/modules/custom/itg_survey/templates/itg-survey-form.tpl.php
+++ sites/all/modules/custom/itg_survey/templates/itg-survey-form.tpl.php
@@ -11,15 +11,24 @@ $byline_twitter_handler = $byline_node->field_reporter_twitter_handle[LANGUAGE_N
 $byline_image = $base_url . str_replace('public://', '/sites/default/files/', $byline_node->field_story_extra_large_image[LANGUAGE_NONE][0]['uri']);
 $created_date = date('M d, Y', $byline_node->created);
 $updated_date = date('h:i', $byline_node->created);
+
+if($form['question_format']['#value'] == 'All questions at a time') {
+  $form_class = 'survey-form-wrapper-all';
+}
+else {
+  $form_class = 'survey-form-wrapper-one';
+}
 ?>
 <div class="survey-form-main-container">
   <div class="survey-title"><?php echo $node->title; ?></div>
   <div class="survey-description"><?php echo $node->body[LANGUAGE_NONE][0]['value']; ?></div>
   <div class="survey-byline-name"><?php echo $byline_name; ?></div>
-  <div class="survey-byline-twitter-handler"><?php echo $byline_twitter_handler;?></div>
-  <div class="survey-byline-image"><img src="<?php echo $byline_image;?>" alt="" title=""/></div>
-  <div class="survey-byline-date"><?php echo $created_date .' | UPDATED '.$updated_date .' IST | New Delhi';?></div>
-  <div class="survey-form">
- <?php print drupal_render_children($form);?>
-    </div>
+  <div class="survey-byline-twitter-handler"><?php echo $byline_twitter_handler; ?></div>
+  <div class="survey-byline-image"><img src="<?php echo $byline_image; ?>" alt="" title=""/></div>
+  <div class="survey-byline-date"><?php echo $created_date . ' | UPDATED ' . $updated_date . ' IST | New Delhi'; ?></div>
+  
+  <!-- Render survey form -->
+  <div class="<?php echo $form_class; ?>">
+    <?php print drupal_render_children($form); ?>
+  </div>
 </div>
