c70eb91312f3f02f7aced94aac884fd097df890a
sunil <sunil.kumar@kelltontech.com>
2016-09-09 16:32:06 +0530

    UI Development so sorry

--- sites/all/themes/itg/templates/views-view-custom--so-sorry--most-watched.tpl.php
+++ sites/all/themes/itg/templates/views-view-custom--so-sorry--most-watched.tpl.php
@@ -1,8 +1,12 @@
 <?php
 global $base_url;
 ?>
+<h2>Most <span>Watched</span></h2>
+<ul class="photo-list">
 <?php foreach ($rows as $key => $value) : ?>
-  <div class="large-image">
+    <li class="col-md-3">
+        <div class="tile">
+  <figure>
     <?php if (!empty($value['field_story_extra_large_image'])) : ?>
       <a href="<?php print $base_url . '/' . "sosorry/" . $value['nid'] . "" ?>" class="pic">
         <?php print $value['field_story_extra_large_image'] ?>
@@ -12,17 +16,13 @@ global $base_url;
         <img  src="<?php print $base_url . "/" . drupal_get_path('theme', 'itg'); ?>/images/default_for_all.png" />
       </a>
     <?php endif; ?>
-  </div>
-<div>
-  <?php print $value['field_video_duration']; ?>
-</div>
+      <figcaption><i class="fa fa-camera"></i> <?php print $value['field_video_duration']; ?></figcaption>
+</figure>
+            <span class="posted-on"><?php print $value['created']; ?></span>
+             <?php print l($value['title'], $base_url . '/' . "sosorry/" . $value['nid'] . "") ?>
 
-<div>
-  <?php print $value['created']; ?>
-</div>
-
-  <div class="title">
-    <?php print l($value['title'], $base_url . '/' . "sosorry/" . $value['nid'] . "") ?>
-  </div>
+        </div>
+        </li>
 <?php endforeach; ?>
 
+</ul>
