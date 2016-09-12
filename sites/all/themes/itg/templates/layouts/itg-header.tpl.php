327085690686119830b89c11b6e8b0991c36400c
sunil <sunil.kumar@kelltontech.com>
2016-09-12 18:05:46 +0530

    UI Development chnage in header

--- sites/all/themes/itg/templates/layouts/itg-header.tpl.php
+++ sites/all/themes/itg/templates/layouts/itg-header.tpl.php
@@ -17,17 +17,17 @@ foreach($data['itg_main_manu_header'] as $key => $val) {
     </ul>
     <div class="globle-search">
      <input class="search-text" placeholder="Type here" type="text" value=""></div>
-      <div class="menu-login desktop-hide">
+<!--      <div class="menu-login desktop-hide">
         <div class="container ">   
             <div class="user-menu">
                 <a href="#" class="user-icon"><i class="fa fa-user"></i></a>
                 <?php
-                $block = module_invoke('system', 'block_view', 'user-menu');
-                print render($block['content']);
+                //$block = module_invoke('system', 'block_view', 'user-menu');
+               // print render($block['content']);
                 ?>
             </div>
         </div>
-    </div>
+    </div>-->
 </div>
 <div class="itg-logo-container">
     <div class="container top-nav">                  
