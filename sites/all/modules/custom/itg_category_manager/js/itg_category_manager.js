8f7b7149e71776cf8858b2bee79ddb8be95ebb0f
user <pavnish.kumar@kelltontech.com>
2016-09-13 13:41:48 +0530

    ITG-1621 remove Sponsored Checkbox

--- sites/all/modules/custom/itg_category_manager/js/itg_category_manager.js
+++ sites/all/modules/custom/itg_category_manager/js/itg_category_manager.js
@@ -1,92 +1,92 @@
-(function ($, Drupal, window, document, undefined) {
-  Drupal.behaviors.itg_category_manager = {
-    attach: function (context, settings) {
-      // Place your code here.
-      var setting = Drupal.settings.baseUrl;
-      // Show loader image
-      $('.term-spinner').hide();
-      function showLoadingImage() {
-        $('.term-spinner').show();
-      }
-      
-      // Hide loader image
-      function hideLodingImage() {
-        $('.term-spinner').hide();
-      }
-      
-        if(jQuery('#edit-parent-hierarchical-select-selects-0').val()!='0')
-        {
-            jQuery('#edit-field-set-as-featured-cate').hide();
-        }
-        
-      
-      // Disable manual or forcefully
-      $('#edit-state-mode-1, #edit-state-mode-0').click(function () {
-        var state = $(this).is(':checked');
-        var tid = $('input[name="term_tid"]').val();
-        switch ($(this).val()) {
-          case '0':
-            showLoadingImage();
-            $.ajax({
-              url: setting.baseUrl + "/category-management/disable-forcefully",
-              type: 'post',
-              data: {'tid': tid},
-              dataType: "JSON",
-              success: function (data) {                
-                window.location = setting.baseUrl + "/category-manager-listing";                
-              },
-              complete: function () {
-                hideLoadingImage();
-              }
+(function($, Drupal, window, document, undefined) {
+    Drupal.behaviors.itg_category_manager = {
+        attach: function(context, settings) {
+            // Place your code here.
+            var setting = Drupal.settings.baseUrl;
+            // Show loader image
+            $('.term-spinner').hide();
+            function showLoadingImage() {
+                $('.term-spinner').show();
+            }
+
+            // Hide loader image
+            function hideLodingImage() {
+                $('.term-spinner').hide();
+            }
+
+            if (jQuery('#edit-parent-hierarchical-select-selects-0').val() != '0')
+            {
+                jQuery('#edit-field-set-as-featured-cate').hide();
+            }
+
+
+            // Disable manual or forcefully
+            $('#edit-state-mode-1, #edit-state-mode-0').click(function() {
+                var state = $(this).is(':checked');
+                var tid = $('input[name="term_tid"]').val();
+                switch ($(this).val()) {
+                    case '0':
+                        showLoadingImage();
+                        $.ajax({
+                            url: setting.baseUrl + "/category-management/disable-forcefully",
+                            type: 'post',
+                            data: {'tid': tid},
+                            dataType: "JSON",
+                            success: function(data) {
+                                window.location = setting.baseUrl + "/category-manager-listing";
+                            },
+                            complete: function() {
+                                hideLoadingImage();
+                            }
+                        });
+                        break;
+                    case '1':
+                        showLoadingImage();
+                        window.location = setting.baseUrl + "/category-management/disable-manually/" + tid;
+                }
             });
-            break;
-          case '1': 
-            showLoadingImage();
-            window.location = setting.baseUrl + "/category-management/disable-manually/"+tid;
+
+            // Ajax callback to check number of node associated with term.
+            $('#show-content-count').on('click', function() {
+                var tid = $('.tid').text();
+                //Call Ajax
+                $.ajax({
+                    url: setting.baseUrl + "/content-associated",
+                    type: 'post',
+                    data: {'tid': tid},
+                    dataType: "JSON",
+                    success: function(data) {
+                        $('span.count').html(' : ' + data);
+                    }
+                });
+            });
+            $('.hierarchical-select select').on('change', function() {
+                if ($(this).val() != 0)
+                {
+                    $('#edit-field-set-as-featured-cate-und-yes').prop('checked', false);
+                    $('#edit-field-set-as-featured-cate').hide();
+                    $('#edit-field-cm-select-type-und').attr('disabled', true);
+                    $('#edit-field-cm-select-type-und').removeAttr('required');
+                } else {
+                    jQuery('#edit-field-set-as-featured-cate').show();
+                    $('#edit-field-cm-select-type-und').attr('disabled', false);
+                    $('#edit-field-cm-select-type-und').attr('required', 'required');
+
+                }
+            })
+
+            // Pager settings
+            var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            var d = new Date();
+            var currentMonth = monthNames[d.getMonth()] + ' ' + d.getFullYear();
+            var currentMonthPager = $(".date-heading h3").text();
+            if (currentMonth === currentMonthPager) {
+                $(".date-next").css("display", "none");
+            }
+
+            // Copy to clipboard code.
+            new Clipboard('.itg-clipboard');
+            // End of Copy to clipboard code.
         }
-      });
-
-      // Ajax callback to check number of node associated with term.
-      $('#show-content-count').on('click', function () {
-        var tid = $('.tid').text();
-        //Call Ajax
-        $.ajax({
-          url: setting.baseUrl + "/content-associated",
-          type: 'post',
-          data: {'tid': tid},
-          dataType: "JSON",
-          success: function (data) {
-            $('span.count').html(' : ' + data);
-          }
-        });
-      });
-      $('.hierarchical-select select').on('change',function(){
-          if($(this).val()!=0)
-          {   
-              $('#edit-field-set-as-featured-cate-und-yes').prop('checked',false);
-              $('#edit-field-set-as-featured-cate').hide();
-              $('#edit-field-cm-select-type-und').attr('disabled',true);
-               $('#edit-field-cm-select-type-und').removeAttr('required');
-          }else{
-              jQuery('#edit-field-set-as-featured-cate').show();
-            $('#edit-field-cm-select-type-und').attr('disabled',false);
-             $('#edit-field-cm-select-type-und').attr('required','required');
-
-          }
-      })
-      
-      // Pager settings
-      var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-      var d = new Date();      
-      var currentMonth = monthNames[d.getMonth()]+' '+d.getFullYear();
-      var currentMonthPager = $(".date-heading h3").text();
-      if (currentMonth === currentMonthPager) {        
-        $(".date-next").css("display", "none");
-      }
-      
-      // Copy to clipboard code.
-      new Clipboard('.itg-clipboard');
-      // End of Copy to clipboard code.
-    }
-  };
+    };
 })(jQuery, Drupal, this, this.document);
