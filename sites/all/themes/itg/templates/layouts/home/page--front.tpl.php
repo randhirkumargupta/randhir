2eebbd2231598c605f9f0b76f573d00334e2e8fa
user <pavnish.kumar@kelltontech.com>
2016-09-14 14:06:57 +0530

    ITG-1853 #comment load more and less in home page

--- sites/all/themes/itg/templates/layouts/home/page--front.tpl.php
+++ sites/all/themes/itg/templates/layouts/home/page--front.tpl.php
@@ -809,6 +809,9 @@ if($theme != 'itgadmin')
                 jQuery(this).parent('.itg-common-section').next('.show-on-add').show(750);
                 jQuery(this).parent('.itg-common-section').next('.show-on-add').find('.removes-more-block').show();
                 jQuery(this).parent('.itg-common-section').next('.show-on-add').find('.add-more-block').show();
+                 if (jQuery(this).parent('.itg-common-section').next('.itg-common-section').next('.itg-common-section').is(':visible')) {
+                  jQuery(this).parent('.itg-common-section').next('.itg-common-section').find('.add-more-block').hide();
+                }
             });
             jQuery('.add-more-block').each(function() {
 
