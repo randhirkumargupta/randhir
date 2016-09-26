a7d2d66379a5bfa08a6ec28321bb211ed8903cd4
user <pavnish.kumar@kelltontech.com>
2016-09-26 17:43:34 +0530

    ITG-1983 #comment worked on tech block and widget most read

---
+++ sites/all/themes/itg/templates/views-view-custom--tech--block-7.tpl.php
@@ -0,0 +1,24 @@
+<?php foreach($rows as $index => $row){
+    $video_class="";
+    if(strtolower($row['type'])=='videogallery')
+    {
+       $video_class='content-video'; 
+    }
+    $desc=$row['title'];
+    if($row['field_story_kicker_text']!="")
+    {
+        $desc = $row['field_story_kicker_text'];
+    }else if($row['field_story_kicker_text']=="" && $row['body']!=""){
+         $desc = $row['body'];
+    }
+   if($index==0){?>
+       <div class="first-buying-guid-block <?php echo $video_class;?>"><?php print $row['field_story_extra_large_image'];?></div>
+<div><?php echo l(mb_strimwidth(strip_tags($desc), 0, 150, ".."), $base_url . '/' . drupal_get_path_alias("node/{$row['nid']}")) ?></div>
+   <?php }else{
+?>
+<div class="<?php echo $video_class;?>"><?php print $row['field_story_extra_large_image'];?></div>
+<div><?php echo l(mb_strimwidth(strip_tags($desc), 0, 150, ".."), $base_url . '/' . drupal_get_path_alias("node/{$row['nid']}")) ?></div>
+   <?php } ?>
+
+<?php }; ?>
+
