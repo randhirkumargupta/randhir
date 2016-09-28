5d87ecd0f17174edf0ab8483510ac6899f18dd65
sunil <sunil.kumar@kelltontech.com>
2016-09-28 13:06:26 +0530

    ITG-1982 #time 2h #comment UI Development special auto page

--- sites/all/themes/itg/templates/layouts/special/page--special_auto.tpl.php
+++ sites/all/themes/itg/templates/layouts/special/page--special_auto.tpl.php
@@ -91,7 +91,7 @@ if ($theme != 'itgadmin') {
 <!--------------------------------Code for Front tpl and admin tpl---------------------------------------->
  
 
-<div class="itg-layout-container <?php echo $itg_class; ?>">
+<div class="itg-layout-container <?php echo $itg_class; ?> auto-layout-page">
     <?php if(isset($widget_data['big_story'])) : ?>
     <div class="row">
         <div class="col-md-12">
