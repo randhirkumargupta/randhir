270158aea46819741aab580c99f86ba2fa190a90
avanish.singh <avanish.singh@192.168.14.82>
2016-09-28 15:51:49 +0530

    ITG-2026 #time 1d #comment theme redeem-points page

--- sites/all/modules/custom/itg_loyalty_reward/js/itg_loyalty_reward_cart.js
+++ sites/all/modules/custom/itg_loyalty_reward/js/itg_loyalty_reward_cart.js
@@ -15,6 +15,27 @@
                     $('.' + value).parent().parent().find('.redeem-point a').attr('href', 'cart');
                 });
             }
+            
+            // Code to show detail popup
+            $('.view-redeem-points').on('click', '.product-pic', function(){
+              var pic = $(this).html();
+              var added_on = "April 25, 216";
+              var title = $(this).siblings('.product-title').html();
+              var desc = "Cras ex mauris; euismod vitae hendrerit ac, molestie nec mi. Vestibulum felis nisl; volutpat eget nisl nec, accumsan ultricies turpis. Donec et lectus ut ex maximus aliquam vel non nulla.";
+              var redeem_points = $(this).siblings('.redeem-points').html();
+              var actions = $(this).siblings('.product-actions').html();
+              var popup_html = '<div class="cart-popup-wrapper">\n\
+                                <div class="cart-popup"><a class="cart-close" href="javascript:;"><i class="fa fa-times" aria-hidden="true"></i></a>\n\
+                                <div class="col-md-3">' + pic + '</div>\n\
+                                <div class="col-md-9"><h2>' + title + '</h2><p>' + desc + '</p>\n\
+                                <div class="redeem-points"><strong>You can claim this product for:</strong><span>'+ redeem_points +'</span></div>\n\
+                                <div class="cart-actions">'+ actions +'</div></div></div></div>';
+              $('body').addClass('has-cart-popup').remove('.cart-popup-wrapper');
+              $('body').append(popup_html);
+            });
+            $('.view-redeem-points').on('click', '.cart-close', function(){
+              $(this).closest('.cart-popup-wrapper').fadeOut();
+            });
             // Checkout page script.
 
             // Module code ends.
