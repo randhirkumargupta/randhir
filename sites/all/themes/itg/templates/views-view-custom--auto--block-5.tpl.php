6160d3f100b4e3c4776fb8bfc2467bdd7ab6894e
sunil <sunil.kumar@kelltontech.com>
2016-09-28 14:44:09 +0530

    ITG-1982 #time 2h #comment UI development auto page

--- sites/all/themes/itg/templates/views-view-custom--auto--block-5.tpl.php
+++ sites/all/themes/itg/templates/views-view-custom--auto--block-5.tpl.php
@@ -2,7 +2,7 @@
 
    ?>
 
-<div class="here"><?php print $row['field_story_extra_large_image'];?></div>
+<div><?php print $row['field_story_extra_large_image'];?></div>
         <div><?php print $row['delta'];?></div>
        <div><?php print ucfirst($row['title']);?></div>
   
