f0fdd194c3bee8edfd3c32e4cc319c184a7af8f9
user <pavnish.kumar@kelltontech.com>
2016-09-29 11:26:23 +0530

    ITG-1983 #comment add flex slider #time 2h

--- sites/all/themes/itg/templates/views-view-custom--auto--block-5.tpl.php
+++ sites/all/themes/itg/templates/views-view-custom--auto--block-5.tpl.php
@@ -1,11 +1,41 @@
+<?php drupal_add_library('flexslider', 'flexslider');
+      libraries_load('flexslider');
+      module_load_include('inc', 'itg_widget', 'includes/featured_photo_carousel');
+
+      drupal_add_js('jQuery(window).load(function() {
+            jQuery(".flexslider").flexslider({
+            animation: "slide",
+            prevText: "",
+            nextText: "",
+            });
+            });', array('type' => 'inline', 'scope' => 'footer', 'weight' => 5)
+      );
+
+      drupal_add_css('.flex-caption {
+              width: 96%;
+              padding: 2%;
+              left: 0;
+              bottom: 0;
+              background: rgba(0,0,0,.5);
+              color: #fff;
+              text-shadow: 0 -1px 0 rgba(0,0,0,.3);
+              font-size: 14px;
+              line-height: 18px;
+            }', 'inline');?>
+
+<div class="flexslider">
+      <ul class="slides"> 
 <?php foreach($rows as $index => $row){
 
    ?>
 
-<div class="here"><?php print $row['field_story_extra_large_image'];?></div>
-        <div><?php print $row['delta'];?></div>
-       <div><?php print ucfirst($row['title']);?></div>
+ <li><?php print $row['field_story_extra_large_image'];?> 
+      <div class="detail">
+        <p><?php print $row['delta'];?></p>
+        <p><?php print ucfirst($row['title']);?></p>
+      </div>
   
-
+ </li>
 <?php }; ?>
-
+</ul>
+ </div>
