e32dc14cccdbd7d927c9e381484a8f4c6540f432
Yogesh Kushwaha <ankush.gautam@kelltontech.com>
2016-09-29 13:27:56 +0530

    “ITG-1542

--- sites/all/modules/custom/itg_reporter/templates/reporter-node-form.tpl.php
+++ sites/all/modules/custom/itg_reporter/templates/reporter-node-form.tpl.php
@@ -4,6 +4,7 @@
     <?php print drupal_render($form['field_reporter_email_id']); ?>
     <?php print drupal_render($form['field_reporter_twitter_handle']); ?>
     <?php print drupal_render($form['field_story_expert_name']); ?>
+    <?php print drupal_render($form['field_story_new_title']); ?>
     <?php print drupal_render($form['field_celebrity_pro_occupation']); ?>
     <?php print drupal_render($form['field_story_category']); ?>
     <?php print drupal_render($form['field_story_extra_large_image']); ?>
