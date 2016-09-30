090662bc55ce9c16a9663b2fda7c95f2cb5c2f16
avanish.singh <avanish.singh@192.168.14.82>
2016-09-30 17:30:57 +0530

    ITG-2025 #time 2h #comment theme checkout page

--- sites/all/modules/custom/itg_loyalty_reward/templates/itg-loyalty-reward-cart.tpl.php
+++ sites/all/modules/custom/itg_loyalty_reward/templates/itg-loyalty-reward-cart.tpl.php
@@ -8,8 +8,9 @@
     <?php if (count($data) > 0): ?>
       <?php $cart_total = 0; ?>
       <?php foreach ($data as $key => $cart_detail): ?>    
-        <div class="row">
-            <div class="cart-image">
+        <div class="cart-row">
+        <div class="cart-checkout-list">
+            <div class="cart-image col-md-2 col-sm-2">
                 <?php
                 $product_pic = theme(
                     'image_style', array(
@@ -20,18 +21,19 @@
                 print $product_pic;
                 ?>
             </div>
-            <div class="product-details">
-                <span>Title: <?php print $cart_detail['product']['title']; ?></span>
-                <span>Magzine ID: <?php print $cart_detail['product']['nid']; ?></span>                
+            <div class="product-details col-md-6 col-sm-6">
+              <div><span>Title:</span><strong><?php print $cart_detail['product']['title']; ?></strong></div>
+              <div><span>Magzine ID:</span><strong><?php print $cart_detail['product']['nid']; ?></strong></div>              
             </div>
-            <div class="cart-action">
+            <div class="cart-action col-md-4 col-sm-4">
                 <div class="cart-action-links">
                     <span>Item (<?php print $cart_detail['quantity']; ?>)</span>
-                    <span><?php print l(t('Delete (X)'), 'cart/delete/' . $cart_detail['product']['nid'] . '/' . $cart_detail['product']['title'], array('query' => array('destination' => arg(0)))); ?></span>
+                    <?php print l(t('Delete (X)'), 'cart/delete/' . $cart_detail['product']['nid'] . '/' . $cart_detail['product']['title'], array('query' => array('destination' => arg(0)))); ?>
                 </div>
                 <?php $item_total = $cart_detail['product']['points'] * $cart_detail['quantity']; ?>
                 <div class="points"><?php print $item_total . ' ' . t('Points'); ?></div>
             </div>
+          </div>
         </div>
         <?php $cart_total += $item_total; ?> 
       <?php endforeach; ?>
