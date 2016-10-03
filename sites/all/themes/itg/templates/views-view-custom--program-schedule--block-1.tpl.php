08a677dadcc7f5151764acd9f38b19f16aaa0af9
vedprakash.pareek <vedprakash.pareek@kelltontech.com>
2016-10-03 20:05:46 +0530

    ITG-1979 #Comment Event front end sponsor work

---
+++ sites/all/themes/itg/templates/views-view-custom--program-schedule--block-1.tpl.php
@@ -0,0 +1,9 @@
+<?php foreach ($rows as $index => $row): ?>
+  <?php print $row['field_start_time']; ?>
+  <?php print $row['field_story_expert_name']; ?>
+  <?php //print $row['field_daywise_event']; ?>
+  <?php print $row['field_story_extra_large_image']; ?>
+  <?php print $row['title']; ?>
+  <?php print $row['field_story_new_title']; ?>
+  <?php print $row['view']; ?>
+<?php endforeach; ?>
