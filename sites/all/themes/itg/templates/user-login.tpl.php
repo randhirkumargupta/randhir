f8930c4c02fe3a9163cca69069ddb86a4f3ee115
Yogesh Kushwaha <ankush.gautam@kelltontech.com>
2016-10-05 13:27:07 +0530

    “ITG-1542

--- sites/all/themes/itg/templates/user-login.tpl.php
+++ sites/all/themes/itg/templates/user-login.tpl.php
@@ -13,18 +13,18 @@ if (isset($_GET['ReturnTo']) && !empty($_GET['ReturnTo'])) {
 <a href="saml_login/twitter/<?php echo $shr; ?>" class="sso-twitter">Twitter</a>
 <a href="#" class="sso-facebook">Facebook</a>
 </div>
-<?php print '<div class="sign-border">or sign in using your email address</div>'; ?>
-<?php } else { ?>
+<?php print '<div class="sign-border">or sign in using your email address</div>';
+ } else { ?>
   <h2>Log in</h2>
 <?php } ?>
   <div class="login-wrapper">
-    <?php print drupal_render($form['name']); ?>
-    <?php print drupal_render($form['pass']); ?>
-      <?php print drupal_render($form['remember_me']); ?>
+    <?php print drupal_render($form['name']); 
+          print drupal_render($form['pass']);
+          print drupal_render($form['remember_me']); ?>
     <div class="form-actions">
-      <?php print drupal_render($form['actions']['submit']); ?>
+      <?php print drupal_render($form['actions']['submit']);
         
-        <?php if (isset($_GET['ReturnTo']) && !empty($_GET['ReturnTo'])) {
+        if (isset($_GET['ReturnTo']) && !empty($_GET['ReturnTo'])) {
           if (strpos($_GET['ReturnTo'], 'domain_info') !== false) {
                    $forgot = 'forgot-password/domain_info';
                    $signup = 'signup/domain_info';
@@ -37,10 +37,13 @@ if (isset($_GET['ReturnTo']) && !empty($_GET['ReturnTo'])) {
           print '<div class="bottom-link">'. l('Forgot Your Password?',$forgot, array('attributes' => array('class' => 'sso-forgot-link')));
           print l('New Here ? Signup',$signup, array('attributes' => array('class' => 'sso-register-link')));
           print '</div>';
-          ?>
+          
+          } 
+          else { 
+          print l('Forgot Password?','user/password', array('attributes' => array('class' => 'forgot-link'))); 
+        } 
         
-        <?php } else { ?>
-        <?php print l('Forgot Password?','user/password', array('attributes' => array('class' => 'forgot-link'))); }?>
+        ?>
     </div>
     <?php print drupal_render_children($form) ?>
     
