88651e889e97aa57f7d6b2391eebce7011c596f9
Yogesh Kushwaha <ankush.gautam@kelltontech.com>
2016-10-06 16:40:37 +0530

    work on sso

--- sites/all/themes/itg/templates/layouts/itg-header.tpl.php
+++ sites/all/themes/itg/templates/layouts/itg-header.tpl.php
@@ -62,7 +62,7 @@ foreach($data['itg_main_manu_header'] as $key => $val) {
 
                 if ($user->uid == 0 || $_GET['q'] != 'user') {
                   ?>
-                  <?php if (!empty('PARENT_SSO') && PARENT_SSO == 'YES') { ?>
+                  <?php if (!empty(PARENT_SSO) && PARENT_SSO == 'YES') { ?>
                 <a onclick="window.open('http://dev.indiatodayonline.in/saml_login/other/domain_info', '_blank', 'location=yes,height=490,width=550,scrollbars=yes,status=yes');" class="user-icon"><i class="fa fa-user"></i></a>    
                 
                   <?php }
