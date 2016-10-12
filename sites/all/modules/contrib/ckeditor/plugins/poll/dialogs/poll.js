e6866c4c507906256f99c3f264df68603f1b845f
Haripal Rao <haripal.rao@kelltontech.com>
2016-10-12 19:25:08 +0530

    ITG-1957 #comment Survey, Quiz and Poll Plugin

--- sites/all/modules/contrib/ckeditor/plugins/poll/dialogs/poll.js
+++ sites/all/modules/contrib/ckeditor/plugins/poll/dialogs/poll.js
@@ -36,7 +36,7 @@ CKEDITOR.dialog.add('pollDialog', function(editor) {
             label: 'Get Polls',
             title: 'Poll',
             onClick: function() {
-              editor.popup(Drupal.settings.basePath + 'pqs/associate-with-story/poll', 400, 600);
+              editor.popup(window.location.host + '/pqs/associate-with-story/poll', 400, 600);
               
             }
           },
