2069f7ccf46a861c4be06952ce39c987a643760f
Yogesh Kushwaha <ankush.gautam@kelltontech.com>
2016-10-12 16:29:44 +0530

    ITG-1988 work on global comment configuration

--- sites/all/themes/itg/templates/node--story.tpl.php
+++ sites/all/themes/itg/templates/node--story.tpl.php
@@ -247,6 +247,13 @@
               }
            ?>
       
+      <?php 
+           if (function_exists(global_comment_last_record)) {
+                 $last_record = global_comment_last_record();
+                 $config_name = trim($last_record[0]->config_name);
+               }
+               if($config_name == 'vukkul') {
+               ?>
       <div class="vukkul-comment">
 
             <div id="vuukle_div"></div>
@@ -258,7 +265,14 @@
               ?>
      
         </div>
-
+               <?php } 
+               if($config_name == 'other') {
+               ?>
+      <div id="other-comment">
+        <?php print render($content['comment_form']); ?>
+        <?php print render($content['comments']); ?>
+        </div>
+               <?php } ?>
   </div>
 </div>
 <?php endif; ?>
