01016861d3ef87cd503b6fc6f1fd8c156c46712a
Yogesh Kushwaha <ankush.gautam@kelltontech.com>
2016-10-13 12:56:43 +0530

    ITG-1988 work on global comment configuration

--- sites/all/themes/itg/templates/node--story.tpl.php
+++ sites/all/themes/itg/templates/node--story.tpl.php
@@ -216,22 +216,11 @@
                         var js, fjs = d.getElementsByTagName(s)[0];
                         if (d.getElementById(id)) return;
                         js = d.createElement(s); js.id = id;
-                        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1050682001621663";
+                        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=265688930492076";
                         fjs.parentNode.insertBefore(js, fjs);
                       }(document, 'script', 'facebook-jssdk'));</script>
                       <div class="fb-share-button" data-href="<?php print $short_url; ?>" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">Share</a></div></li>
-                
-<script>
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=265688930492076";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-</script>
-
+ 
 <button onclick="gogogo('assdfs', 'alfl', 'alfmnl')">Share me, please</button>
 <script>
 function gogogo(linkurl, title, desc) {
