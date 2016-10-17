7afeae909409e172079d52a2cd5e5dca1032270a
Yogesh Kushwaha <ankush.gautam@kelltontech.com>
2016-10-17 17:23:44 +0530

    ITG-1988 work on global comment

--- sites/all/themes/itg/templates/node--photogallery.tpl.php
+++ sites/all/themes/itg/templates/node--photogallery.tpl.php
@@ -8,14 +8,29 @@ if (function_exists('taboola_view')) {
 }
 ?>
 
-<div class="vukkul-comment">
+      <?php 
+           if (function_exists(global_comment_last_record)) {
+                 $last_record = global_comment_last_record();
+                 $config_name = trim($last_record[0]->config_name);
+               }
+               if($config_name == 'vukkul') {
+               ?>
+      <div class="vukkul-comment">
 
-  <div id="vuukle_div"></div>
-
-  <?php
-  if (function_exists('vukkul_view')) {
-    vukkul_view();
-  }
-  ?>
-
-</div>
+            <div id="vuukle_div"></div>
+            
+             <?php 
+              if(function_exists('vukkul_view')) {
+                vukkul_view();
+              }
+              ?>
+     
+        </div>
+               <?php } 
+               if($config_name == 'other') {
+               ?>
+      <div id="other-comment">
+        <?php print render($content['comment_form']); ?>
+        <?php print render($content['comments']); ?>
+        </div>
+               <?php } ?>
