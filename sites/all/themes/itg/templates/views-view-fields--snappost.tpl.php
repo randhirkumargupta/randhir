7bbc6c65b1e94edd1a81ae88e8f84a629b4dac42
Yogesh Kushwaha <ankush.gautam@kelltontech.com>
2016-10-18 12:12:54 +0530

    ITG-1988 work on social sharing

--- sites/all/themes/itg/templates/views-view-fields--snappost.tpl.php
+++ sites/all/themes/itg/templates/views-view-fields--snappost.tpl.php
@@ -26,6 +26,7 @@ $snap= "snap".$row->nid;
 $snapurl = urlencode('http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'/#'.$snap);
 $fb_title = addslashes($row->field_field_story_snap_post[0]['raw']['value']);
 $fb_url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+$share_desc = '';
 $node = node_load($row->nid);
 $image = file_create_url($node->field_story_extra_large_image[LANGUAGE_NONE][0]['uri']);
 ?>
@@ -61,7 +62,7 @@ $image = file_create_url($node->field_story_extra_large_image[LANGUAGE_NONE][0][
         </div>
         <div class="snap-social">
             <ul>
-                <li><a onclick="gogogo('<?php print $fb_url;?>', '<?php print $fb_title; ?>', '<?php print $image;?>')"><i class="fa fa-facebook"></i><span>Share</span></a></li>
+                <li><a onclick="gogogo('<?php print $fb_url;?>', '<?php print $fb_title; ?>','<?php print $share_desc; ?>', '<?php print $image;?>')"><i class="fa fa-facebook"></i><span>Share</span></a></li>
                 <li><a href="javascript:" onclick="twitter_popup('<?php print urlencode($row->field_field_story_snap_post[0]['raw']['value']);?>', '<?php print $snapurl; ?>')"><i class="fa fa-twitter"></i><span>Twitter</span></a><span class="twt-count">0</span></li>
                 <li><a href="#"><i class="fa fa-comment-o"></i><span>Comment</span></a></li>
             </ul>
