d26c8eb17ffd7cc1dfe895276f1af00778a40f8d
sunil <sunil.kumar@kelltontech.com>
2016-10-19 17:20:42 +0530

    ITG-1836 #time 6h #comment UI Development Sport page

--- sites/all/modules/custom/itg_widget/templates/live-score-data.tpl.php
+++ sites/all/modules/custom/itg_widget/templates/live-score-data.tpl.php
@@ -1,23 +1,18 @@
 <?php
 if (!empty($data)) : global $base_url; ?>
-    <div>
-        <div> <h2><?php echo $data['team1sabbr'] . '<span>vs</span>' . $data['team2sabbr']; ?></h2>
-            <span><?php echo $data['desc']; ?></span>
+<div class="live-scorecard">
+    <div class="team-detail"> <h4><?php echo $data['team1sabbr'] . ' <span>vs</span> ' . $data['team2sabbr']; ?> <?php echo $data['desc']; ?></h4>
+            
             <span class="match-location"><?php echo $data['venue']; ?></span>
         </div>
-        <div> 
+    <div class="score"> 
             <span class="batteam"><?php echo $data['currentscore']->batteamname; ?></span>
             <span class="batteamscore"> <?php echo $data['currentscore']->batteamruns; ?>/<?php echo $data['currentscore']->batteamwkts; ?></span>
-            <span class="crrrunrate">Crr <?php echo $data['currentscore']->crr; ?></span>
-            <span class="crrrunover">(<?php echo $data['currentscore']->batteamovers; ?> Over)</span>
+            <span class="crrrunrate">Crr <?php echo $data['currentscore']->crr; ?>  <span class="crrrunover">(<?php echo $data['currentscore']->batteamovers; ?> Over)</span></span>           
         </div>
-        <span>Live:Commentary</span> |
-            <span>Full Scoredcard</span>
-            <span>Graphs</span>
-        <div>
-            
-            
-        </div>
+        <span>Live: Commentary</span> |
+            <span>Full Scoredcard |</span>
+            <span>Graphs</span>       
     </div>
 <?php else : ?>
     <span class="no-result-found"><?php print t("Content Not Found") ?></span>
