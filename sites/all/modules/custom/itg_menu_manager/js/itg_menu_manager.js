3f7ef0f82add018c98b88a5fcff791633e1ea5ca
rajveer singh <rajveer.gang@gmail.com>
2016-10-21 19:11:47 +0530

    ITG-2145 #comment disabled field is removed

--- sites/all/modules/custom/itg_menu_manager/js/itg_menu_manager.js
+++ sites/all/modules/custom/itg_menu_manager/js/itg_menu_manager.js
@@ -11,13 +11,13 @@ jQuery(".itg_menu_manager_form").ajaxSuccess(function(event, xhr, settings) {
     var url_type = jQuery("input[name=url_type]").val();
     if (menu_level_val == 'third' && section_value == 0) {
         jQuery("input[type=text]").val("");
-        jQuery("input[name=title]").attr("disabled", "disabled");
+        //jQuery("input[name=title]").attr("disabled", "disabled");
         jQuery("input[name=url]").attr("disabled", "disabled");
         jQuery("div#decided-section").css("display", "inline-block !important");
     }
 
     if (menu_level_val == 'second') {
-        jQuery("input[name=title]").removeAttr("disabled");
+        //jQuery("input[name=title]").removeAttr("disabled");
         jQuery("input[name=url]").removeAttr("disabled");
     }
 
@@ -28,7 +28,7 @@ jQuery(".itg_menu_manager_form").ajaxSuccess(function(event, xhr, settings) {
 
 
 jQuery(".itg_menu_manager_form").ready(function() {
-    jQuery("#itg-menu-manager-form input[type=text]").attr("disabled", "disabled");
+    //jQuery("#itg-menu-manager-form input[type=text]").attr("disabled", "disabled");
 });
 
 
