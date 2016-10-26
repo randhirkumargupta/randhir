42d2531bda70c992be059a133866c8ff237d9d8d
arun2016 <arunmishra.coder@gmail.com>
2016-10-26 11:35:21 +0530

    ITG-1882 #comment hide settings menu from all mobile services page.

--- sites/all/modules/custom/itg_budget_predictor/templates/itg-budget-predictor.tpl.php
+++ sites/all/modules/custom/itg_budget_predictor/templates/itg-budget-predictor.tpl.php
@@ -40,7 +40,7 @@ if (!empty($data)) {
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; utf-8" />
-        <title>Pilotmade Sortables Demo</title>
+        <title>Budget Predictor</title>
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js"></script>
 
