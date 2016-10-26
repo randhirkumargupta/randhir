bacfea2cfe15c12057bcdae997c6f055f8bfb30e
rajveer singh <rajveer.gang@gmail.com>
2016-10-26 15:45:22 +0530

    ITG-2187 #comment worked on live tv

---
+++ sites/all/themes/itg/templates/views-view-custom--live-tv--live-tv-latest-block.tpl.php
@@ -0,0 +1,27 @@
+<div class="live-tv-latest-video-container container">
+  <h2><?php print t("Latest video"); ?></h2>
+  <?php foreach ($rows as $key => $output) : ?>
+    <div class="latest-video-rows latest-video-rows-<?php print $key ?>">
+      <div class="cateogry cateogry-<?php print $keys; ?>">
+        <?php
+        if ($output['field_primary_category'] && isset($output['field_primary_category'])) {
+          $term_data = taxonomy_term_load($output['field_primary_category']);
+        }
+        print $term_data->field_cm_display_title['und'][0]['value'];
+        ?>
+      </div>
+      <div class="extra-large-image">
+        <?php print $output['field_story_extra_large_image'] ?>
+      </div>
+      <div class="title">
+        <?php print $output['title'] ?>
+      </div>
+      <div class="video-anchor">
+        <?php print $output['field_video_anchor'] ?>
+      </div>
+      <div class="created">
+        <?php print $output['created'] ?>
+      </div>
+    </div>
+  <?php endforeach; ?>
+</div>
