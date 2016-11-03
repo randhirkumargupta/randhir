194d5c9e35dbb8d54e8e2826a114dbf17d0e3e2e
sunil <sunil.kumar@kelltontech.com>
2016-11-03 17:26:46 +0530

    ITG-1838 #time 8h #comment UI development

--- sites/all/themes/itg/templates/views-view-custom--most-popular--elections-most-popular.tpl.php
+++ sites/all/themes/itg/templates/views-view-custom--most-popular--elections-most-popular.tpl.php
@@ -1,15 +1,15 @@
-<div class="row">
-    <div class="col-md-3">
-        <?php
-        foreach ($rows as $index => $row) {
-            $desc = $row['title'];
-            $video_class = "";
-            if (strtolower($row['type']) == 'videogallery') {
-                $video_class = 'video-icon';
-            }
+<div class="row">    
+    <?php
+    foreach ($rows as $index => $row) {
+        $desc = $row['title'];
+        $video_class = "";
+        if (strtolower($row['type']) == 'videogallery') {
+            $video_class = 'video-icon';
+        }
         ?>
-        <a class="<?php echo $video_class; ?>" href="<?php echo $base_url . '/' . drupal_get_path_alias("node/{$row['nid']}") ?>"><?php print $row['field_story_extra_large_image']; ?>    </a>
-        <p><?php echo l(mb_strimwidth(strip_tags($desc), 0, 70, ".."), $base_url . '/' . drupal_get_path_alias("node/{$row['nid']}")) ?></p>            
-        <?php } ?>
-    </div>  
+        <div class="col-md-3">
+            <a class="<?php echo $video_class; ?>" href="<?php echo $base_url . '/' . drupal_get_path_alias("node/{$row['nid']}") ?>"><?php print $row['field_story_extra_large_image']; ?>    </a>
+            <p><?php echo l(mb_strimwidth(strip_tags($desc), 0, 70, ".."), $base_url . '/' . drupal_get_path_alias("node/{$row['nid']}")) ?></p>            
+        </div>         
+    <?php } ?>    
 </div>
