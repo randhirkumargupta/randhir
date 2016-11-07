a49d4f14558eba214fc8aa9081010ee6735574ee
sunil <sunil.kumar@kelltontech.com>
2016-11-07 15:26:13 +0530

    responsive changes

--- sites/all/themes/itg/js/script.js
+++ sites/all/themes/itg/js/script.js
@@ -297,18 +297,15 @@ jQuery(document).ready(function () {
     var eventMenu = function(){    
          winWidth = jQuery(window).width();  
          if(winWidth < 1024){
-            jQuery('#block-menu-menu-event-menu').prepend('<div><a class="mobile-nav" href="javascript:void(0)"><i class="fa fa-bars"></i></a></div>'); 
-        }
-        jQuery('#block-menu-menu-event-menu a.mobile-nav').click(function(){            
-            jQuery('#block-menu-menu-event-menu ul.menu').slideToggle();
-        });
+            jQuery('#block-menu-menu-event-menu').prepend('<div><a class="mobile-nav" href="javascript:void(0)"><i class="fa fa-bars"></i></a></div>');        
+            jQuery('#block-menu-menu-event-menu a.mobile-nav').click(function(){            
+                jQuery('#block-menu-menu-event-menu ul.menu').slideToggle();
+            });
+         }
     };
    
     eventMenu();
     jQuery(window).resize(eventMenu);
-    
-    
-    
       
 });
 
