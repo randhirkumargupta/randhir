c43002ec9b8fb222149efdbcbf4b323391e40fb0
user <pavnish.kumar@kelltontech.com>
2016-11-08 12:22:56 +0530

    ITG-2223 #comment done

--- sites/all/modules/custom/itg_menu_manager/templates/third-level-menu.tpl.php
+++ sites/all/modules/custom/itg_menu_manager/templates/third-level-menu.tpl.php
@@ -92,12 +92,15 @@ $field_cm_category_color = ($section_banner_data->field_cm_category_color['und']
               $target = "_blank";
             }
             $active='';
-            if(end(explode('/',$link_url)) ==  arg(1)){
+            $arg1 = arg(1);
+            $urlalise= drupal_get_path_alias("node/$arg1");
+            $real_path_alise = ($urlalise) ? $urlalise : "node/$arg1";
+            if(end(explode('/',$link_url)) ==  $real_path_alise){
                 $active='active';
             }
             
             ?>
-            <li class="<?php echo $active;?>"><?php print l($link_text, $link_url, array('attributes' => array('target' => $target, 'class' => array("third-level-child", "third-level-child-$key")))); ?></li>
+            <li><?php print l($link_text, $link_url, array('attributes' => array('target' => $target, 'class' => array("third-level-child", "third-level-child-$key" , $active)))); ?></li>
           <?php endforeach; ?>
         </ul>
       </div>
