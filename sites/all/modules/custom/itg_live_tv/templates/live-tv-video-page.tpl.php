e9d3d5889a4aeb75c33ba24fbed4d26c10a4ea24
vedprakash.pareek <vedprakash.pareek@kelltontech.com>
2016-11-09 09:40:08 +0530

    ITG-2181 live tv front page

---
+++ sites/all/modules/custom/itg_live_tv/templates/live-tv-video-page.tpl.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @file
+ * Theme implementation for Live TV Video page.
+ * 
+ */
+?>
+
+<div class="live_tv_video">
+  <?php print $data; ?>
+</div>
+  <div class="social-icon mhide">
+            <ul>
+              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="javascript:" onclick="twitter_popup('<?php print urlencode($share_title);?>', '<?php print urlencode($short_url); ?>')"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-link"></i></a></li>
+            </ul>
+        </div>
+     <?php 
+     itg_live_tv_page_video_category();
+     ?>
