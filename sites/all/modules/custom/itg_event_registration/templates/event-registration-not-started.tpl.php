81c6eab9c4a066bf80ab72238bbc9e111e1d267f
vedprakash.pareek <vedprakash.pareek@kelltontech.com>
2016-11-23 10:28:56 +0530

    ITG-2193 #comment event bugs 2248, 2319, 2229 #time 13h

--- sites/all/modules/custom/itg_event_registration/templates/event-registration-not-started.tpl.php
+++ sites/all/modules/custom/itg_event_registration/templates/event-registration-not-started.tpl.php
@@ -4,7 +4,6 @@
  * Event Registration Page
  * 
  */
-
 ?>
 
 <h2>Registration</h2>
@@ -14,5 +13,8 @@
  <div class="event-registration-link"><i class="fa fa-frown-o" aria-hidden="true"></i> Online Registration are not started yet. <i class="fa fa-frown-o" aria-hidden="true"></i></div>
  <div class="buy-ticket-txt">START DATE: <?php print date('d/m/Y', strtotime($node->field_story_expiry_date[LANGUAGE_NONE][0]['value']));?></div>
 <!-- <div class="buy-ticket-txt">YOU CAN BY TICKETS FROM VENUE: <a href="#" class="siri-audi-link">SIRI FORT AUDITORIUM</a> </br> PLEASE CARRY A VALID PHOTO AND ID PROOF.</div>-->
- <div class="tc-txt"><span>*T&C:</span> Living Media India Ltd. reserves the right to terminate, extend or change this offer or any part thereof at any times. It also reserves the right to accept or reject any or all forms received at their absolute discretion without assigning any reason.  </span></div>
+<?php
+if(!empty($node->field_story_expert_description)): ?>
+<div class="tc-txt"><span>*T&C:</span> <?php print $node->field_story_expert_description[LANGUAGE_NONE][0]['value'] ?> </span></div>
+<?php endif; ?>
 </div>
