8bcaa71439c19be3aeac7b0dc2a63cbe3021fb47
Yogesh Kushwaha <yogesh.kushwaha@kelltontech.com>
2016-11-24 17:05:29 +0530

    social Icon added to refer a friend page.

--- sites/all/modules/custom/itg_personalization/templates/personalization-edit-profile.tpl.php
+++ sites/all/modules/custom/itg_personalization/templates/personalization-edit-profile.tpl.php
@@ -7,18 +7,5 @@
 ?>
 <?php global $base_url; ?>
 <?php print $data['tab']; ?>
-<div class="social-edit">
-    <ul>
-        <li>Via Social</li>
-        <li><img alt="" src="<?php echo $base_url . '/' . drupal_get_path('theme', 'itg') . '/images/facebook-edit.png'; ?>"></li>
-        <li><img alt="" src="<?php echo $base_url . '/' . drupal_get_path('theme', 'itg') . '/images/edit-twitter.png'; ?>"></li>
-        <li><img alt="" src="<?php echo $base_url . '/' . drupal_get_path('theme', 'itg') . '/images/google-plus-edit.png'; ?>"></li>
-        <li><img alt="" src="<?php echo $base_url . '/' . drupal_get_path('theme', 'itg') . '/images/linkedin-edit.png'; ?>"></li>
-    </ul>
-    
-    <div class="more-option"> OR </div>
-    
-</div>
-
 <?php print render($data['form']); ?>
 
