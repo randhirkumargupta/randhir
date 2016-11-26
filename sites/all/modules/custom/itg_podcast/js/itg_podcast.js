00aa0c9c216add1909d7450acec3325678df7da1
ishwar <ishwar.tiwari@kelltontech.com>
2016-11-26 15:26:28 +0530

    Worked on PODCAST - ITG-1877

--- sites/all/modules/custom/itg_podcast/js/itg_podcast.js
+++ sites/all/modules/custom/itg_podcast/js/itg_podcast.js
@@ -3,12 +3,22 @@
  * Contains all functionality related to podcast
  */
 
-(function ($) {
+(function($) {
     Drupal.behaviors.itg_podcast = {
-        attach: function (context, settings) {
+        attach: function(context, settings) {
             //var uid = settings.itg_podcast.settings.uid;
-            
+
         }
 
     };
 })(jQuery, Drupal, this, this.document);
+
+
+jQuery(document).ready(function() {
+    try {
+        jQuery('#podcast-node-form #edit-field-story-category').find('label').append('<span class="form-required" title="This field is required.">*</span>');
+        jQuery('#podcast-node-form #field-podcast-audio-upload-values').find('label').append('<span class="form-required" title="This field is required.">*</span>');
+    }
+    catch (e) {
+    }
+});
