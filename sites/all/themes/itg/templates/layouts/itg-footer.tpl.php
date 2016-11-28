8d01713373aa2b2cb05a2975fee4b98a4cf13639
avanish.singh@kelltontech.com <avanish.singh@kelltontech.com>
2016-11-28 12:20:32 +0530

    #comment theme footer #time 2h

--- sites/all/themes/itg/templates/layouts/itg-footer.tpl.php
+++ sites/all/themes/itg/templates/layouts/itg-footer.tpl.php
@@ -1,73 +1,84 @@
 <section class="footer-top">
     <div class="container">
-        <div class="footer-top-link">
-            <?php print drupal_render($data['footer-top-menu']); ?>
-        </div>
-        
-        <div class="footer-social-link mhide">
+      <div class="row">
+        <div class="footer-top-link clearfix">
+  
+                <div class="footer-logo">
+                    <a href="#" title="" rel="home" class="header__logo"><img src="http://localhost/aajtak/sites/all/themes/itg/logo.png" alt="" class="header__logo-image" /></a>
+                </div>
+                 
+          <div class="menu-first">
             <ul>
-                <li><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#" title=""><i class="fa fa-twitter"></i></a></li>
-                <li><a href="#" title=""><i class="fa fa-google-plus"></i></a></li>
-                <li><a href="#" title=""><i class="fa fa-rss"></i></a></li>                
-                <li><a href="javascript:void(0)" class="search-icon" title=""><i class="fa fa-search"></i></a></li>
-            </ul>
-            <div class="globle-search">
-                <input class="search-text" placeholder="Type here" type="text" value="">
+              <li><a href="#">Lorem Ipsum</a></li>
+              <li><a href="#">Lorem Ipsum</a></li>
+              <li><a href="#">Lorem Ipsum</a></li>
+              <li><a href="#">Lorem Ipsum</a></li>
+              <li><a href="#">Lorem Ipsum</a></li>
+            </ul>   
             </div>
+           <div class="menu-second">
+             <h4> Explorer  </h4>
+            <ul>
+              <li><a href="#">Lorem </a></li>
+              <li><a href="#">Lorem </a></li>
+              <li><a href="#">Lorem </a></li>
+              <li><a href="#">Lorem </a></li>
+              <li><a href="#">Lorem </a></li>
+              <li><a href="#">Lorem </a></li>
+               <li><a href="#">Lorem </a></li>
+              <li><a href="#">Lorem </a></li>
+              <li><a href="#">Lorem </a></li>
+              <li><a href="#">Lorem </a></li>
+              <li><a href="#">Lorem </a></li>
+              <li><a href="#">Lorem </a></li>
+               <li><a href="#">Lorem </a></li>
+              <li><a href="#">Lorem </a></li>
+              <li><a href="#">Lorem </a></li>
+              <li><a href="#">Lorem </a></li>
+              <li><a href="#">Lorem </a></li>
+              <li><a href="#">Lorem </a></li>
+            </ul>   
+            </div>
+           <div class="menu-third">
+             <h4> Assistance </h4>
+            <ul>
+              <li><a href="#">Lorem Ipsum</a></li>
+              <li><a href="#">Lorem Ipsum</a></li>
+              <li><a href="#">Lorem Ipsum</a></li>
+              <li><a href="#">Lorem Ipsum</a></li>
+              <li><a href="#">Lorem Ipsum</a></li>
+            </ul>   
+            </div>
+           <div class="menu-fourth">
+             <h4> Connect With Us </h4>
+             <div class="news-letter">
+               <a href="#"> Subscribe to our newsletter </a>
+             </div>
+            <ul> 
+              <li><a href="#" title="" class="fb-icon"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#" title="" class="google-plus-icon"><i class="fa fa-google-plus"></i></a></li>
+              <li><a href="#" title="" class="twiiter-icon"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#" title="" class="youtube-icon"><i class="fa fa-youtube"></i></a></li>                
+              <li><a href="#" class="linkedin-icon" title=""><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+            </div>
+            <?php //print drupal_render($data['footer-top-menu']); ?>
         </div>
-        <div class="footer-expand-icon"></div>
+     </div>
     </div>
 </section>
-<section class="footer-toggle">
- <section class="footer-mid mhide">
-        <div class="container">
-<!--         <img src="<?php print base_path() ?>sites/all/themes/itg/images/header-ads.png" alt="ads">-->
-        <?php  print ($data['itg_top']['150*150_footer']); ?>
-        </div>
- </section>
-   <section class="footer-bottom">
-       <div class="container">
-           <div class="row">
-               <div class="cell">
-                   <h4>Publications:</h4>
-                   <?php print drupal_render($data['publications_footer']); ?>
-               </div>
-               <div class="cell">
-                   <h4>Television:</h4>
-                   <?php print drupal_render($data['television_footer']); ?>
-                   <h4>Radio:</h4>
-                   <?php print drupal_render($data['radio_footer']); ?>
-               </div>
-               <div class="cell">
-                   <h4>Education:</h4>
-                   <?php print drupal_render($data['education_footer']); ?>
-                   <h4>Online Shopping:</h4>
-                   <?php print drupal_render($data['shopping_footer']); ?>
-               </div>
-               <div class="cell">
-                   <h4>Events:</h4>
-                   <?php print drupal_render($data['events_footer']); ?>
-               </div>
-               <div class="cell">
-                   <h4>Printing:</h4>
-                   <?php print drupal_render($data['printing_footer']); ?>
-                   <h4>Welfare</h4>
-                   <?php print drupal_render($data['welfare_footer']); ?>
-                   <h4>Music:</h4>
-                   <?php print drupal_render($data['music_footer']); ?>
-               </div>              
-                <div class="cell">
-                   <h4>Syndications:</h4>
-                   <?php print drupal_render($data['syndication_footer']); ?>
-                   <h4>Distribution:</h4>
-                   <?php print drupal_render($data['distribution_footer']); ?>
-                   <h4>Useful Links :</h4>
-                   <?php print drupal_render($data['useful_footer']); ?>
-               </div>
-           </div>
-       </div>
-    </section>
+ <section class="footer-bottom">
+   <div class="container">
+     <div class="row">
+       <ul class="footer-bottom-menu clearfix">
+         <li><a href="#"> India Today Group </a></li>
+         <li><a href="#"> Terms and Conditions </a></li>
+         <li><a href="#"> Privacy Policy Security </a></li>
+         <li><a href="#"> Terms of Service </a></li>
+         <li><a href="#"> Sitemap </a></li>
+       </ul>
+     </div>
+   </div>
     <section class="footer-copyright">
         <div class="container"><p>Copyright &copy; <?php echo date("Y") ?> Living Media India Limited. For reprint rights: Syndications Today</p></div>
     </section>
