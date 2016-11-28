03d2f5cf31ee49000c2e5b9c55f851bdb680b9e2
avanish.singh@kelltontech.com <avanish.singh@kelltontech.com>
2016-11-28 20:25:57 +0530

    ITG-2185 #time 1d #comment theme story page

--- sites/all/themes/itg/templates/node--story.tpl.php
+++ sites/all/themes/itg/templates/node--story.tpl.php
@@ -11,36 +11,35 @@
                       print theme('image_style', array('style_name' => 'story_image', 'path' => $story_image)); ?>
           
                 <?php } if (!empty($node->field_story_extra_large_image[LANGUAGE_NONE])) { ?>
-      <div class="photoby"><?php print $node->field_story_extra_large_image[LANGUAGE_NONE][0]['title']; ?></div>
+      <!--<div class="photoby"><?php //print $node->field_story_extra_large_image[LANGUAGE_NONE][0]['title']; ?></div>-->
               <?php } ?>
       </div>
       
-      <div class="image-alt"><?php if (!empty($node->field_story_extra_large_image[LANGUAGE_NONE][0]['alt'])) { print $node->field_story_extra_large_image[LANGUAGE_NONE][0]['alt']; } ?></div>
+      <!--<div class="image-alt"><?php //if (!empty($node->field_story_extra_large_image[LANGUAGE_NONE][0]['alt'])) { print $node->field_story_extra_large_image[LANGUAGE_NONE][0]['alt']; } ?></div>-->
   
-      <div class="below-image-sidebar">
-        
-          <div class="profile-detail">
-                  <ul>
-                      <li class="title"><?php 
-                      
-                      $byline_id = $node->field_story_reporter[LANGUAGE_NONE][0]['target_id'];
-                      $reporter_node = node_load($byline_id);
-                      print $reporter_node->title; ?></li>
-                      <?php  $twitter_handle = $reporter_node->field_reporter_twitter_handle[LANGUAGE_NONE][0]['value'];
-                      $twitter_handle = str_replace('@', '', $twitter_handle);
-                      if(!empty($twitter_handle)) {
-                      ?>
-                      <li class="twitter"><a href="https://twitter.com/<?php print $twitter_handle;?>" class="twitter-follow-button" data-show-count="false">Follow @TwitterDev</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script><?php //print $reporter_node->field_reporter_twitter_handle[LANGUAGE_NONE][0]['value']; ?></li>                
-                      <?php } ?>
-                  </ul>
-                  <ul class="date-update">
-                     
-                      <li><?php print $node->field_stroy_city[LANGUAGE_NONE][0]['taxonomy_term']->name;  ?></li>
-                     <li><?php print  make_hin_str(date('j F, Y', $node->created)); ?>   </li>
-                      <li>अपडेटेड <?php print  make_hin_str(date('H:i', $node->changed)); ?> IST</li>
-                      
-                  </ul>
-                  </div>
+      <div class="byline">
+        <div class="byline-pic"></div>
+        <div class="byline-detail">
+            <ul>
+              <li class="title"><?php
+                $byline_id = $node->field_story_reporter[LANGUAGE_NONE][0]['target_id'];
+                $reporter_node = node_load($byline_id);
+                print $reporter_node->title;
+                ?></li>
+              <?php
+              $twitter_handle = $reporter_node->field_reporter_twitter_handle[LANGUAGE_NONE][0]['value'];
+              $twitter_handle = str_replace('@', '', $twitter_handle);
+              if (!empty($twitter_handle)) {
+                ?>
+                <li class="twitter"><a href="https://twitter.com/<?php print $twitter_handle; ?>" class="twitter-follow-button" data-show-count="false">Follow @TwitterDev</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script><?php //print $reporter_node->field_reporter_twitter_handle[LANGUAGE_NONE][0]['value']; ?></li>                
+              <?php } ?>
+            </ul>
+            <ul class="date-update">
+              <li><?php print $node->field_stroy_city[LANGUAGE_NONE][0]['taxonomy_term']->name; ?></li>
+              <li><?php print make_hin_str(date('j F, Y', $node->created)); ?>   </li>
+              <li>अपडेटेड <?php print make_hin_str(date('H:i', $node->changed)); ?> IST</li>
+            </ul>
+          </div>
           
           
       </div>
@@ -54,54 +53,61 @@
         $view_display = 'page_1';
         $vvid = itg_front_end_node_weight(1683, $view_name, $view_display);
         ?>
-      <div id="previous">
-          <h2>Previous</h2>
-          <?php
-           $previous = node_load(itg_get_previousnext_weight($vvid-1, $view_name, $view_display));
-           ?>
-          <div id="a-title"><?php 
-          $pre_url = url('node/' . $previous->nid, array('absolute' => TRUE));
-          print l($previous->title, $pre_url); ?></div>
-          <div id="a-image"><?php  $pre_image = $previous->field_story_extra_large_image[LANGUAGE_NONE][0]['uri']; 
-          
-          if (!empty($pre_image)) {
-            print theme('image_style', array('style_name' => 'magazine_rhs_100x140', 'path' => $pre_image));
-          }
-          ?></div>
-          
-      </div>
-          <div id="next">
-               <h2>Next</h2>
-                 <?php
-                 $next = node_load(itg_get_previousnext_weight($vvid + 1, $view_name, $view_display));
-                 ?>
-                 <div id="a-title">
-                     <?php
-                     $next_url = url('node/' . $next->nid, array('absolute' => TRUE));
-                     print l($next->title, $next_url);
-                     ?></div>
-                 <div id="a-image"><?php
-                     $nxt_image = $next->field_story_extra_large_image[LANGUAGE_NONE][0]['uri'];
+        <div id="previous">
+            <a href="#">Previous</a>
+            <div class="prev-details">
+              <?php
+              $previous = node_load(itg_get_previousnext_weight($vvid - 1, $view_name, $view_display));
+              ?>
+              <h4>
+                <?php
+                $pre_url = url('node/' . $previous->nid, array('absolute' => TRUE));
+                print l($previous->title, $pre_url);
+                ?>
+              </h4>
+              <?php
+              $pre_image = $previous->field_story_extra_large_image[LANGUAGE_NONE][0]['uri'];
 
-                     if (!empty($nxt_image)) {
-                       print theme('image_style', array('style_name' => 'magazine_rhs_100x140', 'path' => $nxt_image));
-                     }
-                     ?></div>
-      </div>
+              if (!empty($pre_image)) {
+                print theme('image_style', array('style_name' => 'magazine_rhs_100x140', 'path' => $pre_image));
+              }
+              ?>
+            </div>
+          </div>
+        <div id="next">
+            <a href="#">Next</a>
+            <div class="prev-details">
+              <?php
+              $next = node_load(itg_get_previousnext_weight($vvid + 1, $view_name, $view_display));
+              ?>
+              <h4>
+              <?php
+              $next_url = url('node/' . $next->nid, array('absolute' => TRUE));
+              print l($next->title, $next_url);
+              ?>
+              </h4>
+              <?php
+              $nxt_image = $next->field_story_extra_large_image[LANGUAGE_NONE][0]['uri'];
+              if (!empty($nxt_image)) {
+                print theme('image_style', array('style_name' => 'magazine_rhs_100x140', 'path' => $nxt_image));
+              }
+              ?>
+            </div>
+          </div>
           </div>
       <div class="tags">
-                  <ul>
-                      <li><i class="fa fa-tags"></i> Tags :</li>        
-                      <?php
-                      foreach ($node->field_story_itg_tags['und'] as $tags) {
-                          $term = taxonomy_term_load($tags['tid']);
-                          $t_name = $term->name;
-                          $comma_sep_tag[] = $t_name;
-                          print '<li>#' . $t_name . '</li>';
-                      }
-                      ?>
-                  </ul>
-              </div>
+        <h3>Tags</h3> 
+        <ul>      
+          <?php
+          foreach ($node->field_story_itg_tags['und'] as $tags) {
+            $term = taxonomy_term_load($tags['tid']);
+            $t_name = $term->name;
+            $comma_sep_tag[] = $t_name;
+            print '<li>#' . $t_name . '</li>';
+          }
+          ?>
+        </ul>
+      </div>
       
       <div id="ads-place">Ads goes here</div>
       <div id="api-place">Third party api goes here</div>
@@ -131,8 +137,12 @@
         <?php print render($content['comments']); ?>
         </div>
                <?php } ?>
-      <h2>photos</h2>
-      <div id="photo-place"><?php print views_embed_view('related_photogallery','page', $node->field_primary_category[LANGUAGE_NONE][0]['value']); ?></div>
+      <div class="story-photos">
+        <div class="story-photos-inner">
+        <h3>photos</h3>
+        <?php print views_embed_view('related_photogallery','page', $node->field_primary_category[LANGUAGE_NONE][0]['value']); ?>
+        </div>
+      </div>
         <h2>popular videos</h2>
       <div id="video-place"><?php print views_embed_view('related_photogallery','page_1', $node->field_primary_category[LANGUAGE_NONE][0]['value']); ?></div>
             <h2>Sambandhit Khabre</h2>
--- sites/all/themes/itg/templates/page.tpl.php
+++ sites/all/themes/itg/templates/page.tpl.php
@@ -20,33 +20,16 @@ window.addEventListener("message", function(ev) {
 </script>
 <?php } ?>
 <div id="page">
-    <header class="header" id="header" role="banner">
-            <section class="header-top">
-                <div class="container header-logo">
-              <?php if ($logo): ?>
-                <div class="logo">
-                    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
-                </div>
-                    <?php endif; ?>                    
-                    </div>
-                
-                <?php if ($site_name || $site_slogan): ?>
-                    <div class="header__name-and-slogan" id="name-and-slogan">
-                        <?php if ($site_name): ?>
-                            <h1 class="header__site-name" id="site-name">
-                                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
-                            </h1>
-                        <?php endif; ?>
-                        <?php if ($site_slogan): ?>
-                            <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
-                        <?php endif; ?>
-                    </div>
-                <?php endif; ?>
-                
-                <?php print render($page['header']); ?>
-                    
-            </section>
-        </header>
+  <header class="header" id="header" role="banner">
+      <div class="logo-n-three-bar">
+        <div class="three-bar"><span></span></div>
+        <?php if ($logo): ?>
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
+        <?php endif; ?>
+      </div>
+        <?php print render($page['header']); ?>
+    </header>
+    
     <?php
       // Render the sidebars to see if there's anything in them.
       $sidebar_first  = render($page['sidebar_first']);
