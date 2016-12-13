1bb208cefb581688354b171f9ad1f8ae117de6fd
gaurav.rajdeo <gaurav.rajdeo@kelltontech.com>
2016-12-13 18:20:39 +0530

    ITG-2389 #comment- changes in hook_mail

--- sites/all/modules/custom/itg_common/templates/itg-personalization-mail.tpl.php
+++ sites/all/modules/custom/itg_common/templates/itg-personalization-mail.tpl.php
@@ -4,15 +4,14 @@
  * Personalization mail template.
  */
 ?> 
-<?php echo 'hi'; ?>
-<!--<html xmlns:v="urn:schemas-microsoft-com:vml">
+
+<html xmlns:v="urn:schemas-microsoft-com:vml">
   <head>
         <title>New Content Published Mail</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
   <body>
-      <h3><?php print '<pre>'; print_r($data, TRUE); ?></h3>
       <h3><?php echo $data; ?></h3>
     </body>
-</html>-->
+</html>
