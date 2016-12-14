18d90b2b43e780af4a83f5492e5324ec2260bfad
user <pavnish.kumar@kelltontech.com>
2016-12-14 20:01:43 +0530

    ITG-2530 #comment issue fix

--- sites/all/modules/custom/itg_widget/templates/section-ordering-data.tpl.php
+++ sites/all/modules/custom/itg_widget/templates/section-ordering-data.tpl.php
@@ -606,15 +606,19 @@ else if ($widget_style == 'movies-celebrities') {
                     <?php
                     if ((!empty($entity->field_story_extra_large_image['und'][0]['uri']) && isset($entity->field_story_extra_large_image['und'][0]['uri']))) {
                         $extra_large_image_url = image_style_url("widget_very_small", $entity->field_story_extra_large_image['und'][0]['uri']);
-                    }
+                    } 
                     ?>
 
                 <?php if (!empty($extra_large_image_url)) { ?>
 
                         <span class="pic  <?php echo $video_class; ?>"> <a href="<?php echo $base_url . '/' . drupal_get_path_alias("node/$entity->nid"); ?>">  <img  src="<?php print $extra_large_image_url ?>" /> </a></span>
 
-        <?php }
+        <?php } else { ?>
+     <span class="pic  <?php echo $video_class; ?>"> <a href="<?php echo $base_url . '/' . drupal_get_path_alias("node/$entity->nid"); ?>">  <img  height="66" width="88" src="<?php print $base_url . "/" . drupal_get_path('theme', 'itg'); ?>/images/default_for_all.png"  /> </a></span>
+
+       <?php }
         ?>
+                        
 
                     <span><a href="<?php echo $base_url . '/' . drupal_get_path_alias("node/$entity->nid"); ?>"><?php echo mb_strimwidth(strip_tags($desc), 0, 100, ".."); ?></span></a>
                 </li>
