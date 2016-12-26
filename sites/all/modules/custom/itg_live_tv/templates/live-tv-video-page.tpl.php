0f5379668fc34aff325e7969061b2cdf05e36aac
Yogesh Kushwaha <ankush.gautam@kelltontech.com>
2016-12-26 16:49:51 +0530

    ITG-2045 #comment work on share on live tv

--- sites/all/modules/custom/itg_live_tv/templates/live-tv-video-page.tpl.php
+++ sites/all/modules/custom/itg_live_tv/templates/live-tv-video-page.tpl.php
@@ -14,8 +14,8 @@ $short_url = shorten_url($actual_link, 'goo.gl');
 </div>
     <div class="social-icon">
         <ul>
-            <li><a class= "def-cur-pointer" title="share on facebook" onclick="fbpop('<?php print $actual_link; ?>', '<?php print $share_title; ?>', '<?php print $share_desc; ?>', '<?php print $image; ?>')"><i class="fa fa-facebook"></i> <span>Share</span></a></li>
-            <li><a class= "def-cur-pointer" title="share on twitter" onclick="twitter_popup('<?php print urlencode($share_title); ?>', '<?php print urlencode($short_url); ?>')"><i class="fa fa-twitter"></i> <span>Tweet</span></a></li>
+            <li><a class="def-cur-pointer" title="share on facebook" onclick="fbpop('<?php print $actual_link; ?>', '<?php print $share_title; ?>', '<?php print $share_desc; ?>', '<?php print $image; ?>')"><i class="fa fa-facebook"></i> <span>Share</span></a></li>
+            <li><a class="def-cur-pointer" title="share on twitter" onclick="twitter_popup('<?php print urlencode($share_title); ?>', '<?php print urlencode($short_url); ?>')"><i class="fa fa-twitter"></i> <span>Tweet</span></a></li>
             <li><a href="#"><i class="fa fa-link"></i> <span>Embed</span></a></li>
             <li><a href="#"><i class="fa fa-lightbulb-o"></i> <span>Light on</span></a></li>            
         </ul>
