1b4f84ad0a2b67010d67be0f64ddb1dfde45af47
user <pavnish.kumar@kelltontech.com>
2017-01-04 18:57:22 +0530

    ITG-2620 #commnet issue fix

--- sites/all/modules/custom/itg_videogallery/js/itg_dailymotion.js
+++ sites/all/modules/custom/itg_videogallery/js/itg_dailymotion.js
@@ -17,7 +17,7 @@
             }, 1000);
 
             $(".ftp-server .asso-filed_single").click(function(e) {
-
+                jQuery('#loader-data img').show().parent().addClass('loader_overlay');
                 var selected_check_boxes_index = 0;
                 $("input:radio").each(function() {
                     if ($(this).is(':checked'))
@@ -28,7 +28,7 @@
                 if (selected_check_boxes_index == 0) {
                     alert("Please select video file.");
                 } else {
-
+  jQuery('#loader-data img').show().parent().addClass('loader_overlay');
                     var getbtnmane = $(this).attr('btn_name');
                     parent.jQuery('[name="' + getbtnmane + '[fid]"]').val(selected_check_boxes_index);
                     parent.jQuery("body").find("input[name='" + getbtnmane + "[filefield_itg_image_repository][button]").trigger('mousedown');
@@ -44,7 +44,7 @@
             $(".ftp-server .asso-filed").click(function(e) {
 
                 // Getting selected videos from checkboxes        
-
+      
                 var selected_check_boxes_values = new Array();
                 var selected_check_boxes_index = 0;
                 $("input:checkbox[class=form-radio]:checked").each(function() {
@@ -59,19 +59,19 @@
                 if (selected_check_boxes_index == 0) {
                     alert("Please select video file.");
                 } else {
-
+                   jQuery('#loader-data img').show().parent().addClass('loader_overlay');
                     //  parent.jQuery("input[name='" + video_field_file + "'").val(vid);
                     parent.jQuery("[name='field_video_upload_add_more']").mousedown();
                     parent.jQuery('#videogallery-node-form').ajaxComplete(function(event, request, settings) {
-                       try {
-                           parent.jQuery.colorbox.close();
-                       } catch(err) {
-                         
-                       }
-                        
-                        
+                        try {
+                            parent.jQuery.colorbox.close();
+                        } catch (err) {
+
+                        }
+
+
                     });
-                   
+
                 }
             });
 
@@ -88,6 +88,7 @@
 
                 if (selected_check_boxes_values.length > 0)
                 {
+                      jQuery('#loader-data img').show().parent().addClass('loader_overlay');
                     //var base_url = Drupal.settings.basePath;
                     var base_url = Drupal.settings.baseUrl.baseUrl;
                     jQuery.ajax({
@@ -257,6 +258,7 @@ jQuery('document').ready(function() {
             parent.jQuery('[name="' + getbtnmane + '[fid]"]').val(selected_check_boxes_index);
             parent.jQuery("body").find("input[name='" + getbtnmane + "[filefield_itg_image_repository][button]").trigger('mousedown');
             $('#videogallery-node-form').ajaxComplete(function(event, request, settings) {
+                
                 parent.jQuery.colorbox.close();
             });
         } else {
