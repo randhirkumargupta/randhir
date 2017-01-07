705a8b8e01b4ecbcd0ddc42636f552d1a841509b
rajveer singh <rajveer.gang@gmail.com>
2017-01-07 20:54:38 +0530

    ITG-1508 #comment js issue fixed

--- sites/all/modules/custom/itg_syndication/js/itg_syndication_rule.js
+++ sites/all/modules/custom/itg_syndication/js/itg_syndication_rule.js
@@ -11,3 +11,17 @@
 
 
 })(jQuery, Drupal, this, this.document);
+
+jQuery (document).ajaxSuccess (function () {
+  for (i = 0; i <= 25; i++) {
+    var field_syndication_frequency = jQuery ('input[name="field_syndication_rule_details[und]['+i+'][field_syndication_frequency][und]"]:checked').val ();
+
+    if (field_syndication_frequency == "Weekly") {
+      jQuery ('[id^=\"edit-field-syndication-rule-details-und-'+i+'-field-syndication-set-day-und\"]').parent ().parent ().show ();
+    }
+
+    if (field_syndication_frequency == "Monthly") {
+      jQuery ('[id^=\"edit-field-syndication-rule-details-und-'+i+'-field-syndication-set-day-month\"]').parent ().parent ().show ();
+    }
+  }
+});
