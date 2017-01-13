07bbe9585527dd04deb525a909daf804e103b094
Yogesh Kushwaha <yogesh.kushwaha@kelltontech.com>
2017-01-13 15:47:34 +0530

    Progress bar class added to badge progress bar.

--- sites/all/modules/custom/itg_personalization/templates/itg-personalizaton-home.tpl.php
+++ sites/all/modules/custom/itg_personalization/templates/itg-personalizaton-home.tpl.php
@@ -49,12 +49,16 @@
         </div>
         <div class="pregress-bar">            
             <span class="current-badge"><small><?php print $data['badge_detail']['earn']; ?></small>Current Level</span>
-            <span class="pregress-bar-active"></span>
+            <span class="pregress-bar-active progress-bar-<?php print $data['badge_detail']['earn']; ?>"></span>
+            <?php if ($data['badge_detail']['earn'] != 5): ?>
             <span class="next-badge"><small><?php print $data['badge_detail']['next']; ?></small>Next Level</span>
+            <?php endif; ?>
         </div>
+        <?php if ($data['badge_detail']['earn'] != 5): ?>
         <div class="points-to-go">
           <?php print '<span>'.$data['badge_detail']['points_to_go'] . '</span> ' . t('Points to go'); ?>
         </div>
+        <?php endif; ?>
       </div>
       <?php endif; ?>
   </div>
