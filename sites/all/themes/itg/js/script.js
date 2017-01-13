2bd0ffbd9c6d430347a92016560cd7f08a197535
sunil <sunil.kumar@kelltontech.com>
2017-01-13 15:01:57 +0530

    ITG-2694 #time 3h #comment UI Development

--- sites/all/themes/itg/js/script.js
+++ sites/all/themes/itg/js/script.js
@@ -492,11 +492,20 @@ jQuery(document).ready(function () {
 
 
 
-jQuery(document).ready(function () {
+    jQuery(document).ready(function () {
     var winWidth;
-    jQuery('.mobile-nav').click(function () {
-        jQuery('.navigation').slideToggle();
+    jQuery('.mobile-nav i.fa').click(function () {
+        if (jQuery(this).hasClass('fa-bars')) {
+            jQuery(this).addClass('fa-times').removeClass('fa-bars');
+            jQuery('.navigation').slideDown();
+
+        } else {
+            jQuery(this).addClass('fa-bars').removeClass('fa-times');
+            jQuery('.navigation').slideUp();
+        }
     });
+    
+    
     jQuery(document).on('click', '.all-menu', function () {
         jQuery('#newlist').slideToggle();
     });
