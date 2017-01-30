364e1532a0a144e6e210156765c9ec6c70ea10b3
Yogesh Kushwaha <yogesh.kushwaha@kelltontech.com>
2017-01-30 20:53:52 +0530

    #ITG-2626 #comment- debugging after unit testing #time 1h

--- sites/all/modules/custom/itg_story/js/itg_story_front.js
+++ sites/all/modules/custom/itg_story/js/itg_story_front.js
@@ -5,17 +5,7 @@
 
 (function($) {
   Drupal.behaviors.itg_story_front = {
-      attach: function(context, settings) {
-        if (Drupal.settings.itg_story_front.previous) {
-          var previous = Drupal.settings.itg_story_front.previous;
-        } else {
-          $(".multiple-photo-disc i.fa-chevron-left").addClass("hide");
-        }
-        if (Drupal.settings.itg_story_front.next) {
-          var next = Drupal.settings.itg_story_front.next;
-        }else{
-            $(".multiple-photo-disc i.fa-chevron-right").addClass("hide");
-        }
+      attach: function(context, settings) {        
         
         $('.multiple-photo-disc').slick({
           infinite: false,
@@ -32,13 +22,25 @@
           asNavFor: '.multiple-photo-disc',        
           focusOnSelect: true,
           centerPadding: '10px',       
-        });        
+        }); 
+        
+        
         var maxValueInArray = $('.multiple-photo-disc .photo-slider').length;         
         $('.multiple-photo-disc').on('afterChange', function(event, slick, currentSlide){               
-            if(currentSlide === maxValueInArray-1){
+            if(currentSlide === maxValueInArray) {
+                if (Drupal.settings.itg_story_front.next) {
+                    var next = Drupal.settings.itg_story_front.next;
+                  } else {
+                    $(".multiple-photo-disc i.fa-chevron-right").css("visibility","hidden").addClass("hide");
+                }
               window.location.href = next;
             }
-            if(currentSlide === 0){
+            if (currentSlide === 0) {
+                if (Drupal.settings.itg_story_front.previous) {
+                    var previous = Drupal.settings.itg_story_front.previous;
+                  } else {
+                    $(".multiple-photo-disc i.fa-chevron-left").css("visibility","hidden").addClass("hide");
+                }
               window.location.href = previous;
             }
         });             
