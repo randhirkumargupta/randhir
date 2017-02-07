1a9280fcec995d177ae1d9d6512f72aa29535fc7
shubham.negi <shubham.negi@kelltontech.com>
2017-02-07 19:25:40 +0530

    ITG-1977 #comment Quiz task

--- sites/all/modules/custom/itg_quiz/js/itg_quiz_front.js
+++ sites/all/modules/custom/itg_quiz/js/itg_quiz_front.js
@@ -22,6 +22,57 @@
           });
         });
       }
+      
+ //  js for quiz(lallan top)     
+      $('.question-container .form-radio').on('click', function() {
+ansid = $(this).attr('id');  
+res = ansid.split("-");
+anslastid = res[2].substr(res[2].length - 1);
+$('input[name='+$(this).attr('name')+']').attr('disabled', 'disabled');
+$(this).parent().parent().parent().parent().find('.answer-container-actual').html('');
+
+         var qnid = $("input[name='nid']").val();
+            if ($(this).is(':checked')) {
+                var ans_val= $(this).val();
+                jQuery.ajax({      
+                    method:"post",
+                    data: {nid:qnid, ans_val:ans_val},
+                    url: Drupal.settings.basePath+"quiz-response",
+                    success: function(data) {
+                      ansvalue = data.split('-');
+                      $('#answer-container-'+anslastid+' .answer-container-actual').html(ansvalue[0]+'<br/>'+ansvalue[1]);
+
+                    }
+               });        
+            }    
+      
+  });
+    $('body').on('click', '#show_answer', function() {
+	   $(this).closest('.answer-container').find('.form-checkbox').attr('disabled', true);
+      var qnid = $("input[name='nid']").val();
+      var selected = [];
+	$('.form-checkboxes input:checked').each(function() {
+	    selected.push($(this).val());
+	});
+      var not_selected = [];
+	$('.form-checkboxes input:checkbox:not(:checked)').each(function() {
+	    not_selected.push($(this).val());
+	});
+     
+      jQuery.ajax({      
+                    method:"post",
+                    //data: "{'nid':'" + qnid+ "', 'value':'" + val+ "'}",
+                    data: {nid:qnid, ans_val:JSON.stringify(selected), not_select:JSON.stringify(not_selected)},
+                    url: Drupal.settings.basePath+"quiz-response-checkbox",
+                    success: function(data) {
+                      
+                      $('.answer-container-actual').html(data);
+
+                    }
+               });
+      return false;
+    });
+      
     }
   };
 })(jQuery, Drupal, this, this.document);
