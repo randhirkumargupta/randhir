69be8106dc31fae6bc97bddd7feb02df2cfae311
user <pavnish.kumar@kelltontech.com>
2017-02-10 16:42:36 +0530

    ITG-2526 #commnet worked on change graph

--- sites/all/modules/custom/itg_reports/templates/itg-reports-career-graph.tpl.php
+++ sites/all/modules/custom/itg_reports/templates/itg-reports-career-graph.tpl.php
@@ -24,81 +24,56 @@
         <div class="black-bg"><?php print $actor[$key]['name']; ?></div>
         <?php //print $actor_pic;  ?>
     </div>  <?php
-    //print drupal_render($value); 
-    $graph_data_all = $value['chart']['career']['#data'];
-    ksort($graph_data_all);
-    $earning = "";
-    $moviename = array();
-    $year = array();
-    foreach ($graph_data_all as $g_key => $graph_data) {
-        $year[] = $g_key;
-        if ($graph_data[1] == "") {
-            $graph_data[1] = '""';
-        }
-        $earning .= $graph_data[1] . ',';
-        if (isset($graph_data[2]) && !empty($graph_data[2])) {
-            $moviename[$g_key] = $graph_data[2];
-        }
-        
-    }
+    
     ?>
-    <script type="text/javascript">
-        jQuery(function() {
-            var xcontent = new Array();
-    <?php foreach ($year as $key => $val) { ?>
-                xcontent.push('<?php echo $val; ?>');
-    <?php } ?>
-            var moviename = {};
-    <?php foreach ($moviename as $key => $val_movie) { ?>
-                moviename[<?php echo $key; ?>] = '<?php echo $val_movie; ?>';
-    <?php } ?>
-            jQuery('#container_<?php echo $key; ?>').highcharts({
-                title: {
-                    text: '',
-                    x: -20 //center
-                },
-                subtitle: {
-                    text: '',
-                    x: -20
-                },
-                xAxis: {
-                    categories: xcontent,
-                    title: {
-                        text: 'Year'
-                    },
-                },
-                yAxis: {
-                    title: {
-                        text: 'Earning'
-                    },
-                    plotLines: [{
-                            value: 0,
-                            width: 1,
-                            color: '#808080'
-                        }]
-                },
-                legend: {
-                    layout: 'vertical',
-                    align: 'right',
-                    verticalAlign: 'middle',
-                    borderWidth: 0
-                },
-                tooltip: {
-                    formatter: function() {
-                        var xcod = this.x;
 
-                        return moviename[xcod]+', <b>' + this.y + ' Cr </b> ,' + this.x ;
-
-                    },
-                },
-                series: [{
-                        name: '',
-                        data: [<?php echo rtrim($earning, ','); ?>]
-                    }]
-            });
-        });
-    </script>
     <div id="container_<?php echo $key; ?>" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
 
 <?php endforeach; ?>
 
+<script>
+    window.onload = function() {
+
+<?php
+foreach ($output as $key => $value) {
+
+    $graph_data_all = $value['chart']['career']['#data'];
+    $earning = "";
+    $drow_data = "";
+    $moviename = array();
+    $year = array();
+    foreach ($graph_data_all as $g_key => $graph_data) {
+
+        if ($graph_data[1] == "") {
+            $graph_data[1] = 'null';
+        }
+        if ($graph_data[2] == "") {
+            $drow_data .= '{ x:' . $graph_data[0] . ', y: ' . $graph_data[1] . ' },';
+        }
+        else {
+            $drow_data .= '{ x:' . $graph_data[0] . ', y: ' . $graph_data[1] . ' , movie_name:"' . $graph_data[2] . '" },';
+        }
+    }
+    ?>
+
+            var chart = new CanvasJS.Chart("container_<?php echo $key; ?>", {
+                title: {
+                    text: ""
+                },
+                axisX: {
+                    interval: 1,
+                },
+                data: [{
+                        type: "line",
+                        toolTipContent: '{movie_name},{y} cr in year {x}',
+                        connectNullData: true,
+                        dataPoints: [
+    <?php echo rtrim($drow_data, ','); ?>
+                        ]
+                    }]
+            });
+            chart.render();
+<?php } ?>
+    };
+
+</script>
