d631d47c8bb2814f223a6938283d3232387f7c9b
arun2016 <arunmishra.coder@gmail.com>
2017-02-24 10:58:51 +0530

    implement template for best college

--- sites/all/modules/custom/itg_bestcolleges/templates/itg-bestcolleges-template.tpl.php
+++ sites/all/modules/custom/itg_bestcolleges/templates/itg-bestcolleges-template.tpl.php
@@ -1,67 +1,85 @@
-<?php
-/**
- * @file : itg-bestcolleges-template.tpl.php
- */
-$comment_value = variable_get('COMMENT_CONFIG');
-$config_name = $comment_value[0]->config_name;
-?>
-<div class="survey-form-main-container" style="margin:10px 0px 10px 0px;">
-  <h1 class="survey-title"><?php //echo 'Quiz: '. $node->title; ?></h1>
-<div class="row">
-  <div class="row" style="border-size:2px; border-style:solid;border-color:black;">
-   <?php
-      if (array_filter(views_get_view_result('best_college_image_slider', 'block_2'))) {
-          print '<h2 class="my-view-block-title">Best college list</h2>';
-          print views_embed_view('best_college_image_slider', 'block_2');
-      }
-     ?>
-     </div>
-   <div class="row" style="border-size:2px; border-style:solid;border-color:black; margin-top: 17px;">
-    <div class="list-grid">
-        <a href="http://staging.indiatodayonline.in/taxonomy/term/878?view_type=list" class="active">
-        <i class="fa fa-list" aria-hidden="true"></i>List</a><span class="pipline"> | </span>
-        <a href="http://staging.indiatodayonline.in/taxonomy/term/878?view_type=grid">
-        <i class="fa fa-th" aria-hidden="true"></i> Grid</a>
-    </div>
-    <?php
-      if (array_filter(views_get_view_result('best_college_image_slider', 'block_1'))) {
-          print views_embed_view('best_college_image_slider', 'block_1');
-      }
 
-    ?>
+
+
+<?php global $base_url;?>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+
+
+<div class="row">
+<div class="col-md-12 col-sm-7 col-xs-12 left-panel arts">
+<!-- Slider Start-->
+
+<h2>INDIA'S BEST ARTS COLLEGES 2016</h2>
+<div class="slider_outer1">
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+
+     <?php
+                if (array_filter(views_get_view_result('best_college_image_slider', 'block_2'))) {
+                    print views_embed_view('best_college_image_slider', 'block_2');
+                }
+
+              ?>
+
+
+
+
+  <div class="clearfix"></div>
     </div>
+
+
+
+  </div>
 </div>
 
+<!-- Slider End-->
 
-  <?php
-  if ($from_story != 'yes') {
-    if (function_exists('taboola_view')) {
-      taboola_view();
-    }
-  }
+<!-- Grid View-->
+<div class="col-sm-12 col-xs-12 view1">
+<div class="title col-md-6 col-sm-6 col-xs-12">Best of The Best 2016</div>
 
-  if ($config_name == 'vukkul') {
-    ?>
-    <div class="vukkul-comment">
-        <div id="vuukle-emote"></div>
-        <div id="vuukle_div"></div>
+        <div class="right_Section pull-right  col-md-6  col-sm-6 col-xs-12 text-right hidden-xs">
+        <strong>view as</strong>
+        <div class="btn-group">
+            <a href="#" id="list" class="btn btn-default btn-sm"><span class="fa fa-th-list"></span> List</a>
+            <a href="#" id="grid" class="btn btn-default btn-sm active_btn"><span class="fa fa-th"></span> Grid</a>
+        </div>
+        </div>
+</div>
 
-        <?php
-        if (function_exists('vukkul_view')) {
-          vukkul_view();
-        }
-        ?>
+<div>
+<div class="clearfix"></div>
+<div class="col-sm-12 remove_padd_right">
+<div id="products" class="row list-group">
+      <div class="clr_chn">
 
-    </div>
-    <?php
-  }
-  if ($config_name == 'other') {
-    ?>
-    <div id="other-comment">
-        <?php
-        $block = module_invoke('itg_ugc_comment', 'block_view', 'ugc_form_comment_block');
-        print render($block['content']);
-        ?>
-    </div>
-  <?php } ?>
+              <?php
+                if (array_filter(views_get_view_result('best_college_image_slider', 'block_1'))) {
+                    print views_embed_view('best_college_image_slider', 'block_1');
+                }
+
+              ?>
+
+
+
+            </div>
+        </div>
+</div>
+</div>
+</div>
+
+<!-- Right Side -->
+
+
+<!-- Grid View End-->
+
 </div>
