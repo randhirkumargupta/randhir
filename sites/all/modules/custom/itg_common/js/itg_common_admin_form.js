ec41051c0c8ede2c9fca3bc998e03e418a62cca3
shravan <shravan.kumar@kelltontech.com>
2017-02-25 19:12:39 +0530

    worked on js issue

---
+++ sites/all/modules/custom/itg_common/js/itg_common_admin_form.js
@@ -0,0 +1,27 @@
+/*
+ * @file itg_common.js
+ * Contains all functionality related to common functionality
+ */
+
+(function ($) {
+  Drupal.behaviors.itg_common_form = {
+    attach: function (context, settings) {
+        
+      var fid = settings.itg_common.settings.formid;
+      var arg_url = settings.itg_common.settings.arg_url;
+      
+      if (arg_url == 'user') {
+        jQuery("#skip-link").hide();
+      }
+      // Add JS to hide extra element of menu forms
+      if (fid == 'menu_edit_item') {
+        jQuery(".form-item-expanded").hide();
+        jQuery(".form-item-parent").hide();
+        jQuery(".form-item-weight").hide();
+      }
+      if (fid == 'custom_html_widgets_node_form') {
+        jQuery(".vertical-tabs").hide();
+      }
+  }
+ };
+})(jQuery, Drupal, this, this.document);
