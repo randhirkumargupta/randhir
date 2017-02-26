b1b0c29bb31a885a7e19dac9ab0f533734619e96
Yogesh Kushwaha <ankushgautam76@gmail.com>
2017-02-26 16:22:39 +0530

    #comment work on front fix

--- sites/all/themes/itg/templates/views-view-custom--video-landing-header--block.tpl.php
+++ sites/all/themes/itg/templates/views-view-custom--video-landing-header--block.tpl.php
@@ -108,7 +108,7 @@ if (function_exists('get_video_in_fieldcollection_by_nid')) {
                               <div class="show-embed-code-div">
                                 <div class="copy-sample-code">
                                   <textarea readonly="true">
-                                  <div id='IndiaToday_gallery' data-type='UAT'></div>
+                                  <div id='IndiaToday_gallery' data-type='STAGE'></div>
                                   <script src='<?php print $base_url;?>/sites/all/themes/itg/js/video_iframeResizer.js'>
                                   </script>
                                   <script> 
@@ -165,7 +165,7 @@ if (function_exists('get_video_in_fieldcollection_by_nid')) {
                               <div class="show-embed-code-div">
                                 <div class="copy-sample-code">
                                   <textarea readonly="true">
-                                  <div id='IndiaToday_gallery' data-type='UAT'></div>
+                                  <div id='IndiaToday_gallery' data-type='STAGE'></div>
                                   <script src='<?php print $base_url;?>/sites/all/themes/itg/js/video_iframeResizer.js'>
                                   </script>
                                   <script> 
