fdfdbab4457a996889f6958a185a23ae903155c4
Yogesh Kushwaha <ankushgautam76@gmail.com>
2017-03-09 15:03:23 +0530

    #comment work on front fixes

--- sites/all/themes/itgadmin/js/script.js
+++ sites/all/themes/itgadmin/js/script.js
@@ -819,7 +819,7 @@ jQuery(document).ready(function() {
     }
     var checkedlist = '';
     for (var i = 0, l = item.length; i < l; i++) {
-        checkedlist += '<li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span><span class="item-value">' + item[i] + '</span><i class="fa fa-times fright" aria-hidden="true"></i></li>';
+        checkedlist += '<li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span><span class="item-value">' + item[i] + '</span><a href="'+Drupal.settings.baseUrl.baseUrl+'/get-related-view/'+item[i]+'" target="_blank"> <i class="fa fa-eye" aria-hidden="true"></i> </a><i class="fa fa-times fright" aria-hidden="true"></i></li>';
     }
     // end of code
 
