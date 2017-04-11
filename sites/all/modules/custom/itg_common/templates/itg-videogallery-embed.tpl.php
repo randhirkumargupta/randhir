3b89a4306ee6de33eaf25ff6224318cbf369bd62
ankushgautm <ankushgautam76@gmail.com>
2017-04-11 20:45:29 +0530

    #comment work on related content issue

--- sites/all/modules/custom/itg_common/templates/itg-videogallery-embed.tpl.php
+++ sites/all/modules/custom/itg_common/templates/itg-videogallery-embed.tpl.php
@@ -19,21 +19,21 @@
               if (module_exists('itg_videogallery')) {
                   $vid = itg_videogallery_get_videoid($row['fid']);
               }
-              if ($video_value->dailymotion_thumb_url != "") {
-                  $newimageds.= '<li><img data-tag="video_' . $video_value->video_id . '" src="' . $video_value->dailymotion_thumb_url . '" height="66" width="88" alt=""></li>';
+              if ($video_value->solr_video_thumb != "") {
+                  $newimageds.= '<li><img data-tag="video_' . $video_value->solr_video_id . '" src="' . $video_value->solr_video_thumb . '" height="66" width="88" alt=""></li>';
               }
               else {
-                  $newimageds.= '<li><img data-tag="video_' . $video_value->video_id . '" src="' . $base_url . '/' . drupal_get_path('theme', 'itg') . '/images/itg_image88x66.jpg" height="66" width="88" alt=""></li>';
+                  $newimageds.= '<li><img data-tag="video_' . $video_value->solr_video_id . '" src="' . $base_url . '/' . drupal_get_path('theme', 'itg') . '/images/itg_image88x66.jpg" height="66" width="88" alt=""></li>';
               }
               $ads_flag = 0;
               if ($video_value->field_include_ads_value == 'yes') {
                   $ads_flag = 1;
               }
               ?>
-            <li><div class="<?php echo $hide_player; ?>" id="video_<?php echo $video_value->video_id; ?>"><div class="iframe-video"><iframe frameborder="0" scrolling="no" src="https://www.dailymotion.com/embed/video/<?php print $video_value->video_id; ?>?autoplay=0&ui-logo=1&mute=1&endscreen-enable=<?php echo $ads_flag; ?>&ui-start-screen-info" allowfullscreen></iframe></div></div>  
+            <li><div class="<?php echo $hide_player; ?>" id="video_<?php echo $video_value->solr_video_id; ?>"><div class="iframe-video"><iframe frameborder="0" scrolling="no" src="https://www.dailymotion.com/embed/video/<?php print $video_value->video_id; ?>?autoplay=0&ui-logo=1&mute=1&endscreen-enable=<?php echo $ads_flag; ?>&ui-start-screen-info" allowfullscreen></iframe></div></div>  
               
             <?php
-            echo '<div class="embed-desc" id="video_dec_' . $video_value->video_id . '" >'.  ucfirst($video_value->field_videogallery_description_value).'</div></li>';
+            echo '<div class="embed-desc" id="video_dec_' . $video_value->solr_video_id . '" >'.  ucfirst($video_value->field_videogallery_description_value).'</div></li>';
         }
         ?>
         </ul>
