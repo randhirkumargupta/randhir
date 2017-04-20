64318771c109797a0390e1267622038f929f7578
rajveer singh <rajveer.gang@gmail.com>
2017-04-20 19:28:51 +0530

    ITG-3154 #comment worked on reports issues

--- sites/all/modules/custom/itg_reports/templates/itg-reports-sections-data-chart.tpl.php
+++ sites/all/modules/custom/itg_reports/templates/itg-reports-sections-data-chart.tpl.php
@@ -21,4 +21,4 @@
     chart.draw(data, options);
   }
 </script>
-<div id="piechart_section" style="width: 900px; height: 600px;"></div>
+<div id="piechart_section" style="width: 800px; height: 400px;"></div>
