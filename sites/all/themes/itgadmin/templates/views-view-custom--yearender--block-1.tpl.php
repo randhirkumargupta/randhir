0d55f58cddf6216726940c3855c57edfa8846925
pavnish <kpavnish@gmail.com>
2017-04-26 17:20:55 +0530

    ITG-2974 #commnet worked on This issue #time 6.5h

--- sites/all/themes/itgadmin/templates/views-view-custom--yearender--block-1.tpl.php
+++ sites/all/themes/itgadmin/templates/views-view-custom--yearender--block-1.tpl.php
@@ -4,7 +4,7 @@ global $base_url;
 <div class="year-slider">    
     <?php foreach($rows as $index => $row): ?>
     <div><?php print $row['field_story_extra_large_image'];?>
-        <h2><?php echo l(mb_strimwidth(strip_tags($row['title']), 0, 105, ".."), $base_url . '/' . drupal_get_path_alias("node/{$row['nid']}")) ?></h2>
+        <h2 title="<?php echo $row['title'];?>"><?php echo l(mb_strimwidth(strip_tags($row['title']), 0, 105, ".."), $base_url . '/' . drupal_get_path_alias("node/{$row['nid']}")) ?></h2>
     </div>
     <?php endforeach; ?>
 </div>
