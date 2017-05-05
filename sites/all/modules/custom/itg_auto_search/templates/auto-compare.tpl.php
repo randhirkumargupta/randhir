e0f957ff6a70a85e4572b2cadd7774194f272d4f
arun2016 <arunmishra.coder@gmail.com>
2017-05-05 14:08:17 +0530

    fix UI issues

--- sites/all/modules/custom/itg_auto_search/templates/auto-compare.tpl.php
+++ sites/all/modules/custom/itg_auto_search/templates/auto-compare.tpl.php
@@ -10,12 +10,12 @@
       $tableClass2 = 'col-md-6';
     }
     ?>
-    <div class="compare-left <?php echo $class; ?>">
+    <div class="compare-left txtcenter borderbott <?php echo $class; ?>">
         <img src="<?php echo $data['first'][0]->image; ?>" alt="<?php echo $data['first'][0]->brand; ?>" title="<?php echo $data['first'][0]->brand; ?>">
     </div>
     <?php if ($data['second'] != null): ?>
-    <div class="compare-left col-md-2"><div class="vs_text"><img src="<?php echo $base_url . '/' . drupal_get_path('module', 'itg_auto_search');?>/images/big-vs.jpg" alt="" title=""></div></div>
-      <div class="compare-right <?php echo $class; ?>">
+    <div class="compare-left vssection"><div class="vs_text"><img src="<?php echo $base_url . '/' . drupal_get_path('module', 'itg_auto_search');?>/images/big-vs.jpg" alt="" title=""></div></div>
+      <div class="compare-right txtcenter borderbott <?php echo $class; ?>">
           <img src="<?php echo $data['second'][0]->image; ?>" alt="<?php echo $data['second'][0]->brand; ?>" title="<?php echo $data['second'][0]->brand; ?>">
       </div>
     <?php endif; ?>
@@ -30,73 +30,75 @@
         <div class="flr rtext"></div>
     </div>
     <?php if ($data['second'] != null): ?>
-      <div class="compare-left col-md-2"><div class="arrow_bottom"><img src="<?php echo $base_url . '/' . drupal_get_path('module', 'itg_auto_search');?>/images/v-img.jpg" alt="" title=""></div></div>
+      <div class="compare-left vssection"><div class="arrow_bottom"><img src="<?php echo $base_url . '/' . drupal_get_path('module', 'itg_auto_search');?>/images/v-img.jpg" alt="" title=""></div></div>
       <div class="compare-right <?php echo $class; ?>">
+          <div class="flr rtext">
           <h3><?php echo $data['second'][0]->brand; ?></h3>
           <div class="rating_icon">
               <img src="<?php echo $base_url . '/' . drupal_get_path('module', 'itg_auto_search');?>/images/rating.jpg" alt="" title="">
           </div>
+         </div>
       </div>
     <?php endif; ?>
     <div class='col-md-12 grybg_background'>
-        <div class="left-table <?php echo $tableClass; ?>">
+        <div class="left-table tableCompareHead <?php echo $tableClass; ?>">
             <table>
-                <tr><td>Brand Name</td></tr>
-                <tr><td>Model</td></tr>
-                <tr><td>Delhi Price</td></tr>
-                <tr><td>Mumbai Price</td></tr>
-                <tr><td>Kolkata Price</td></tr>
-                <tr><td>Banglore price</td></tr>
-                <tr><td>Chennai Price</td></tr>
-                <tr><td>Body Type</td></tr>
-                <tr><td>Engine Displacement (in CC)</td></tr>
-                <tr><td>Cylinder</td></tr>
-                <tr><td>Power</td></tr>
-                <tr><td>Torque</td></tr>
-                <tr><td>Gears</td></tr>
-                <tr><td>Gear Type</td></tr>
-                <tr><td>Fuel Type</td></tr>
-                <tr><td>0-100kmph (Sec)</td></tr>
-                <tr><td>Top Speed</td></tr>
-                <tr><td>Fuel Efficiency (ARAI claimed)</td></tr>
-                <tr><td>Fuel Efficiency (Tested)</td></tr>
-                <tr><td>AC</td></tr>
-                <tr><td>Climate Control</td></tr>
-                <tr><td>Adjustable Steering Rake</td></tr>
-                <tr><td>Adjustable Steering Reach</td></tr>
-                <tr><td>Steering mounted controls</td></tr>
-                <tr><td>Cruise Control</td></tr>
-                <tr><td>Central Lock</td></tr>
-                <tr><td>Remote Lock</td></tr>
-                <tr><td>Keyless entry</td></tr>
-                <tr><td>Power Windows</td></tr>
-                <tr><td>Driver seat adjust</td></tr>
-                <tr><td>Parking sensor</td></tr>
-                <tr><td>Parking camera</td></tr>
-                <tr><td>Split folding rear seats</td></tr>
-                <tr><td>Multi-function trip computer</td></tr>
-                <tr><td>Door mirror</td></tr>
-                <tr><td>MP3 CD Player</td></tr>
-                <tr><td>USB port</td></tr>
-                <tr><td>Blue tooth</td></tr>
-                <tr><td>Fog lamps</td></tr>
-                <tr><td>Rear AC vents</td></tr>
-                <tr><td>Rear defogger</td></tr>
-                <tr><td>Wash wipe</td></tr>
-                <tr><td>Airbags</td></tr>
-                <tr><td>ESP</td></tr>
-                <tr><td>ABS</td></tr>
+                <tr><th>Brand Name</th></tr>
+                <tr><th>Model</th></tr>
+                <tr><th>Delhi Price</th></tr>
+                <tr><th>Mumbai Price</th></tr>
+                <tr><th>Kolkata Price</th></tr>
+                <tr><th>Banglore price</th></tr>
+                <tr><th>Chennai Price</th></tr>
+                <tr><th>Body Type</th></tr>
+                <tr><th>Engine Displacement (in CC)</th></tr>
+                <tr><th>Cylinder</th></tr>
+                <tr><th>Power</th></tr>
+                <tr><th>Torque</th></tr>
+                <tr><th>Gears</th></tr>
+                <tr><th>Gear Type</th></tr>
+                <tr><th>Fuel Type</th></tr>
+                <tr><th>0-100kmph (Sec)</th></tr>
+                <tr><th>Top Speed</th></tr>
+                <tr><th>Fuel Efficiency (ARAI claimed)</th></tr>
+                <tr><th>Fuel Efficiency (Tested)</th></tr>
+                <tr><th>AC</th></tr>
+                <tr><th>Climate Control</th></tr>
+                <tr><th>Adjustable Steering Rake</th></tr>
+                <tr><th>Adjustable Steering Reach</th></tr>
+                <tr><th>Steering mounted controls</th></tr>
+                <tr><th>Cruise Control</th></tr>
+                <tr><th>Central Lock</th></tr>
+                <tr><th>Remote Lock</th></tr>
+                <tr><th>Keyless entry</th></tr>
+                <tr><th>Power Windows</th></tr>
+                <tr><th>Driver seat adjust</th></tr>
+                <tr><th>Parking sensor</th></tr>
+                <tr><th>Parking camera</th></tr>
+                <tr><th>Split folding rear seats</th></tr>
+                <tr><th>Multi-function trip computer</th></tr>
+                <tr><th>Door mirror</th></tr>
+                <tr><th>MP3 CD Player</th></tr>
+                <tr><th>USB port</th></tr>
+                <tr><th>Blue tooth</th></tr>
+                <tr><th>Fog lamps</th></tr>
+                <tr><th>Rear AC vents</th></tr>
+                <tr><th>Rear defogger</th></tr>
+                <tr><th>Wash wipe</th></tr>
+                <tr><th>Airbags</th></tr>
+                <tr><th>ESP</th></tr>
+                <tr><th>ABS</th></tr>
             </table>
         </div>
-        <div class="left-table <?php echo $tableClass2; ?>">
+        <div class="left-table tableComparedata <?php echo $tableClass2; ?>">
             <table>
                 <tr><td><?php echo $data['first'][0]->brand; ?></td></tr>
                 <tr><td><?php echo $data['first'][0]->vehicle; ?></td></tr>
-                <tr><td><?php echo '<span>&#8377;</span> ' . $data['first'][0]->delhi_price; ?></td></tr>
-                <tr><td><?php echo '<span>&#8377;</span> ' . $data['first'][0]->mumbai_price; ?></td></tr>
-                <tr><td><?php echo '<span>&#8377;</span> ' . $data['first'][0]->kolkata_price; ?></td></tr>
-                <tr><td><?php echo '<span>&#8377;</span> ' . $data['first'][0]->bangalore_price; ?></td></tr>
-                <tr><td><?php echo '<span>&#8377;</span> ' . $data['first'][0]->Chennai_price; ?></td></tr>
+                <tr><td><?php echo '<span>&#8377; ' . $data['first'][0]->delhi_price; echo'</span>'?></td></tr>
+                <tr><td><?php echo '<span>&#8377; ' . $data['first'][0]->mumbai_price; echo'</span>' ?></td></tr>
+                <tr><td><?php echo '<span>&#8377; ' . $data['first'][0]->kolkata_price; echo'</span>' ?></td></tr>
+                <tr><td><?php echo '<span>&#8377; ' . $data['first'][0]->bangalore_price; echo'</span>' ?></td></tr>
+                <tr><td><?php echo '<span>&#8377; ' . $data['first'][0]->Chennai_price; echo'</span>' ?></td></tr>
                 <tr><td><?php echo $data['first'][0]->bodytype; ?></td></tr>
                 <tr><td><?php echo $data['first'][0]->EngineDisplacement; ?></td></tr>
                 <tr><td><?php echo $data['first'][0]->cylinders; ?></td></tr>
@@ -139,15 +141,15 @@
         </div>
 
         <?php if ($data['second'] != null): ?>
-          <div class="right-table <?php echo $tableClass2; ?>">
+          <div class="right-table tableComparedata <?php echo $tableClass2; ?>">
               <table>
                   <tr><td><?php echo $data['second'][0]->brand; ?></td></tr>
                   <tr><td><?php echo $data['second'][0]->vehicle; ?></td></tr>
-                  <tr><td><?php echo '<span>&#8377;</span> ' . $data['second'][0]->delhi_price; ?></td></tr>
-                  <tr><td><?php echo '<span>&#8377;</span> ' . $data['second'][0]->mumbai_price; ?></td></tr>
-                  <tr><td><?php echo '<span>&#8377;</span> ' . $data['second'][0]->kolkata_price; ?></td></tr>
-                  <tr><td><?php echo '<span>&#8377;</span> ' . $data['second'][0]->bangalore_price; ?></td></tr>
-                  <tr><td><?php echo '<span>&#8377;</span> ' . $data['second'][0]->Chennai_price; ?></td></tr>
+                  <tr><td><?php echo '<span>&#8377; ' . $data['second'][0]->delhi_price; echo'</span>'?></td></tr>
+                  <tr><td><?php echo '<span>&#8377; ' . $data['second'][0]->mumbai_price; echo'</span>'?></td></tr>
+                  <tr><td><?php echo '<span>&#8377; ' . $data['second'][0]->kolkata_price; echo'</span>'?></td></tr>
+                  <tr><td><?php echo '<span>&#8377; ' . $data['second'][0]->bangalore_price; echo'</span>'?></td></tr>
+                  <tr><td><?php echo '<span>&#8377; ' . $data['second'][0]->Chennai_price; echo'</span>'?></td></tr>
                   <tr><td><?php echo $data['second'][0]->bodytype; ?></td></tr>
                   <tr><td><?php echo $data['second'][0]->EngineDisplacement; ?></td></tr>
                   <tr><td><?php echo $data['second'][0]->cylinders; ?></td></tr>
