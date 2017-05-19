88c69a569d4fc86d805d6994a879e20d88dd187a
rajveer singh <rajveer.gang@gmail.com>
2017-05-19 19:16:33 +0530

    ITG-3582 #comment worked on payment gateway issue

---
+++ sites/all/modules/custom/itg_payment/templates/itg-paytm-cancle-page.tpl.php
@@ -0,0 +1,2 @@
+<?php echo t("<h2>Your Payment was cancel</h2>") ?>
+<?php echo t("Go back to register again <a href='@event_url'>Click</a>" , array('@event_url' => $_SESSION['recent_event_payment_url']));?>
