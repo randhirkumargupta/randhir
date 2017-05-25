ad11bcf431b2c1c93528d3f6e8c992e565d59176
rajveer singh <rajveer.gang@gmail.com>
2017-05-25 20:00:02 +0530

    ITG-3645 #comment issue fixed payment gateway and widgets

--- sites/all/modules/custom/itg_payment/templates/itg-paytm-fail-page.tpl.php
+++ sites/all/modules/custom/itg_payment/templates/itg-paytm-fail-page.tpl.php
@@ -1,6 +1,6 @@
 <?php if(isset($_SESSION['recent_event_payment_url'])) : ?>
 <?php echo t("<h2>Your Payment was failed</h2>") ?>
-<?php echo t("Go back to register again <a href='@event_url'>Click</a>" , array('@event_url' => $_SESSION['recent_event_payment_url']));?>
-<?php else : ?>
+<?php echo t("Go back to register again <a href='@event_url?payment=fail'>Click</a>" , array('@event_url' => $_SESSION['recent_event_payment_url']));?>
+<?php unset($_SESSION['recent_event_payment_url']); else : ?>
 <?php drupal_goto("<front>"); ?>
 <?php endif; ?>
