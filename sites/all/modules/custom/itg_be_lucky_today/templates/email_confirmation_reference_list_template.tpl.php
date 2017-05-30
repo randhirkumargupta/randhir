dc28c3c7e6ee6c4aa621eeac5754ab25389971c2
Your Name <you@example.com>
2017-05-30 10:18:56 +0530

    init be lucky today

---
+++ sites/all/modules/custom/itg_be_lucky_today/templates/email_confirmation_reference_list_template.tpl.php
@@ -0,0 +1,15 @@
+<?php
+global $base_url;
+?>
+<div class="body-center"> <a href="/" title="India Today Group" class="logo">
+        <img src="<?php echo $base_url . '/' . drupal_get_path('module', 'itg_be_lucky_today'); ?>/images/logo.png" alt="India Today Group"></a> <span class="clear"></span>
+    <h1>Be Lucky Today</h1>
+    <h2 class="ytext">Thank You<br>
+        for your Participation. </h2>
+    <br>
+    <ul class="social" style="width:156px;">
+        <li><a href="javascript:void(0);" onclick="return fbs_click()" title="facebook"><img src="<?php echo $base_url . '/' . drupal_get_path('module', 'itg_be_lucky_today'); ?>/images/fb.png" alt="facebook"></a></li>
+        <li><a href="https://twitter.com/intent/tweet?original_referer=<?php echo $base_url ?>/be-lucky-today&amp;source=tweetbutton&amp;text=Grab the chance before it??s gone! Enter the India Today Survey %26 stand a chance to win an iPhone6: bit.ly/Social_sharing %23YourLuckToday" target="_blank" title="twitter"><img src="<?php echo $base_url . '/' . drupal_get_path('module', 'itg_be_lucky_today'); ?>/images/twitter.png" alt="twitter"></a></li>
+        <li><a href="https://plus.google.com/share?url=<?php echo $base_url ?>/be-lucky-today/" target="_blank" title="gplus"><img src="<?php echo $base_url . '/' . drupal_get_path('module', 'itg_be_lucky_today'); ?>/images/gplus.png" alt="gplus"></a></li>
+    </ul>
+</div>
