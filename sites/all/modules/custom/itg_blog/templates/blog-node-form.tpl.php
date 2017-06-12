e8ff6d4dbaff900f4abf401933d5a8e92bb00381
ishwar Chandra Tiwari <ishwartec@2240786.no-reply.drupal.org>
2017-06-12 16:36:51 +0530

    Worked on ITG-3731, ITG-3731

--- sites/all/modules/custom/itg_blog/templates/blog-node-form.tpl.php
+++ sites/all/modules/custom/itg_blog/templates/blog-node-form.tpl.php
@@ -33,12 +33,15 @@
   <?php print drupal_render($form['field_blog_configuration']); ?>
   <?php print drupal_render($form['field_story_comment_question']); ?>
 </div>
- <div id="AkamaiSettings" class="itg-sidebar-form-section">
+    <?php if(isset($form['akamai_timeout']) && !empty($form['akamai_timeout'])) { ?>
+
+<div id="AkamaiSettings" class="itg-sidebar-form-section">
     <h2 class="story-title"><?php print t('Akamai Setting'); ?></h2>
     <div class="itg-form-section">
         <?php print drupal_render($form['akamai_timeout']); ?>
     </div>
 </div>
+    <?php } ?>
 <h2 id="title-metatags" class="story-title"><?php print t('Remarks'); ?></h2>
 <?php print drupal_render_children($form); ?>
 <div><?php print drupal_render($form['actions']); ?></div>
