a42f99652a7151204f5404da81a8cbfa1131c76e
ankushgautm <ankush.gautam@kelltontech.com>
2017-06-27 17:02:01 +0530

    #comment work on amp pages

--- sites/all/themes/amptheme/amptheme/templates/content/node--photogallery.tpl.php
+++ sites/all/themes/amptheme/amptheme/templates/content/node--photogallery.tpl.php
@@ -41,34 +41,37 @@
 <div class="amp-other-gallery">
 <?php
 // get all node id related to current node primary category
-if(function_exists('get_other_gallery_amp')) {
+if (function_exists('get_other_gallery_amp')) {
   global $base_url;
- $primary_category = $node->field_primary_category[LANGUAGE_NONE][0]['value'];
- $entity_arr = get_other_gallery_amp($primary_category, $node->nid, $node->type, 4);
- $other_gallery = '';
- $other_gallery .= '<h2><span>OTHER GALLERY</span></h2>';
- $other_gallery .= '<ul>';
- foreach($entity_arr as $key => $value) {
-   // get image count
-  if(function_exists('get_image_count_entity_id')) { 
-  $img_count = get_image_count_entity_id($value['entity_id'], 'photogallery', 'field_gallery_image');
+  $primary_category = $node->field_primary_category[LANGUAGE_NONE][0]['value'];
+  $entity_arr = get_other_gallery_amp($primary_category, $node->nid, $node->type, 4);
+  if (!empty($entity_arr)) {
+    $other_gallery = '';
+    $other_gallery .= '<h2><span>OTHER GALLERY</span></h2>';
+    $other_gallery .= '<ul>';
+    foreach ($entity_arr as $key => $value) {
+      // get image count
+      if (function_exists('get_image_count_entity_id')) {
+        $img_count = get_image_count_entity_id($value['entity_id'], 'photogallery', 'field_gallery_image');
+      }
+      $entity_id = $value['entity_id'];
+      $title = l($value['title'], $base_url . '/node/' . $value['nid'], array("attributes" => array("title" => $value['title'])));
+      if (!empty($value['field_story_small_image_fid'])) {
+        $file = file_load($value['field_story_small_image_fid']);
+        $small_image = file_create_url($file->uri);
+      }
+      else {
+        $small_image = $base_url . '/' . path_to_theme() . '/images/itg_image170x127.jpg';
+      }
+
+      $path_alias = $baseUrl . '/node/' . $value['nid'];
+
+      $amp_image = '<a href="' . $path_alias . '?amp"><amp-img height="127" width="170" layout="responsive"  alt="' . $value['title'] . '" title="' . $value['title'] . '" src="' . $small_image . '"></amp-img></a>';
+      $other_gallery .= '<li><div class="other-img">' . $amp_image . '<div class="other-count"><i class="fa fa-camera" aria-hidden="true"></i> ' . $img_count . ' images</div></div><div class="other-date">' . date('D, d M, Y', $value['created']) . '</div><div class="other-title">' . $title . '</div></li>';
+    }
+    $other_gallery .= '</ul>';
+    print $other_gallery;
   }
-  $entity_id = $value['entity_id'];
-  $title = l($value['title'], $base_url.'/node/'.$value['nid'], array("attributes" => array("title" => $value['title'])));
-  if(!empty($value['field_story_small_image_fid'])) {
-  $file = file_load($value['field_story_small_image_fid']);
-  $small_image = file_create_url($file->uri);
-  } else {
-  $small_image = $base_url .'/'. path_to_theme().'/images/itg_image170x127.jpg';
-  }
-  
-  $path_alias = $baseUrl . '/node/' .   $value['nid'];
-  
-  $amp_image = '<a href="'.$path_alias.'?amp"><amp-img height="127" width="170" layout="responsive"  alt="'.$value['title'].'" title="'.$value['title'].'" src="' . $small_image . '"></amp-img></a>';
-  $other_gallery .= '<li><div class="other-img">'.$amp_image.'<div class="other-count"><i class="fa fa-camera" aria-hidden="true"></i> '.$img_count.' images</div></div><div class="other-date">'.date('D, d M, Y', $value['created']).'</div><div class="other-title">'.$title.'</div></li>';
- }
- $other_gallery .= '</ul>';
- print $other_gallery;
 }
 ?>  
 </div>
--- sites/all/themes/amptheme/amptheme/templates/content/node--videogallery.tpl.php
+++ sites/all/themes/amptheme/amptheme/templates/content/node--videogallery.tpl.php
@@ -47,31 +47,34 @@
 <div class="amp-other-gallery">
   <?php
 // get all node id related to current node primary category
-  if (function_exists('get_other_gallery_amp')) {
-    $primary_category = $node->field_primary_category[LANGUAGE_NONE][0]['value'];
-    $entity_arr = get_other_gallery_amp($primary_category, $node->nid, $node->type, 4);
+if (function_exists('get_other_gallery_amp')) {
+  $primary_category = $node->field_primary_category[LANGUAGE_NONE][0]['value'];
+  $entity_arr = get_other_gallery_amp($primary_category, $node->nid, $node->type, 4);
+  if (!empty($entity_arr)) {
     $other_video_gallery = '';
     $other_video_gallery .= '<h2><span>OTHER VIDEO</span></h2>';
     $other_video_gallery .= '<ul>';
-    foreach($entity_arr as $key => $value) {
+    foreach ($entity_arr as $key => $value) {
       // get video time
-     if(function_exists('get_amp_video_time')) { 
-     $video_time = get_amp_video_time($value['entity_id'], 'videogallery', 'field_video_duration');
-     }
-     $entity_id = $value['entity_id'];
-     $title = l($value['title'], $base_url.'/node/'.$value['nid'], array("attributes" => array("title" => $value['title'])));
-     if(!empty($value['field_story_small_image_fid'])) {
-     $file = file_load($value['field_story_small_image_fid']);
-     $small_image = file_create_url($file->uri);
-     } else {
-     $small_image = $base_url .'/'. path_to_theme().'/images/itg_image170x127.jpg';
-     }
-     $path_alias = $baseUrl . '/node/' .   $value['nid'];
-     $amp_image = '<a href="'.$path_alias.'?amp"><amp-img height="127" width="170" layout="responsive"  alt="'.$value['title'].'" title="'.$value['title'].'" src="' . $small_image . '"></amp-img></a>';
-     $other_video_gallery .= '<li><div class="other-img">'.$amp_image.'<div class="other-count"><i class="fa fa-play" aria-hidden="true"></i> '.$video_time[0]['field_video_duration_value'].'</div></div><div class="other-date">'.date('D, d M, Y', $value['created']).'</div><div class="other-title">'.$title.'</div></li>';
+      if (function_exists('get_amp_video_time')) {
+        $video_time = get_amp_video_time($value['entity_id'], 'videogallery', 'field_video_duration');
+      }
+      $entity_id = $value['entity_id'];
+      $title = l($value['title'], $base_url . '/node/' . $value['nid'], array("attributes" => array("title" => $value['title'])));
+      if (!empty($value['field_story_small_image_fid'])) {
+        $file = file_load($value['field_story_small_image_fid']);
+        $small_image = file_create_url($file->uri);
+      }
+      else {
+        $small_image = $base_url . '/' . path_to_theme() . '/images/itg_image170x127.jpg';
+      }
+      $path_alias = $baseUrl . '/node/' . $value['nid'];
+      $amp_image = '<a href="' . $path_alias . '?amp"><amp-img height="127" width="170" layout="responsive"  alt="' . $value['title'] . '" title="' . $value['title'] . '" src="' . $small_image . '"></amp-img></a>';
+      $other_video_gallery .= '<li><div class="other-img">' . $amp_image . '<div class="other-count"><i class="fa fa-play" aria-hidden="true"></i> ' . $video_time[0]['field_video_duration_value'] . '</div></div><div class="other-date">' . date('D, d M, Y', $value['created']) . '</div><div class="other-title">' . $title . '</div></li>';
     }
     $other_video_gallery .= '</ul>';
     print $other_video_gallery;
   }
-  ?>  
+}
+?>  
 </div>
