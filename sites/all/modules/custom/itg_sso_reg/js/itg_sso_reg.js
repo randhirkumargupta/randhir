492ffb3ef3a506fdc2928cceeb389a6d77d4b5c4
animeshthakur <animesh.thakur@intoday.com>
2017-06-29 17:12:14 +0530

    chage sso reg js

--- sites/all/modules/custom/itg_sso_reg/js/itg_sso_reg.js
+++ sites/all/modules/custom/itg_sso_reg/js/itg_sso_reg.js
@@ -7,49 +7,39 @@
     Drupal.behaviors.itg_sso_reg = {
         attach: function (context, settings) {
             var base_url = settings.itg_sso_reg.settings.base_url;
-            //var error = settings.itg_sso_reg.settings.custom_error;
-            alert(base_url);exit();
-            
-            if (error) {
-                jQuery.each(error, function (index, item) {
-                    if (index == 'mobile') {
-                        jQuery('.mobile-error').html(item);
-                    }
-                    if (index == 'mail') {
-                        console.log(item);
-                        jQuery('.email-error').html(item);
-                    }
-                    if (index == 'fname') {
-                        jQuery('.fname-msg').html(item);
-                    }
-                    if (index == 'lname') {
-                        jQuery('.lname-msg').html(item);
-                    }
-                    if (index == 'password') {
-                        jQuery('.pass-msg').html(item);
-                    }
-                });
-            }
+            //alert(base_url);exit();
             // ajax for otp
             $('#itg-sso-reg-multistep-form--2 #otpclickme', context).click(function (event) {
 
                 var mobile = $('input[name=step2mobile]').val();
                 var email = $('input[name=step2mail]').val();
 
+
                 var otp = Math.floor(100000 + Math.random() * 900000)
                 otp = otp.toString().substring(0, 4);
                 var post = "&mobile=" + mobile + "&email=" + email + "&otp=" + otp + "&source=register";
 
+//                if (!validateEmail(email) || email == '') {
+//                    alert('please enter valid email');
+//                }
+                   console.log(otp);
+                // if (mobile != '' || (email != '' && validateEmail(email))) {
                 if (mobile != '') {
                     $("#otpclickme").text("Resend OTP");
+
                     $.ajax({
                         'url': base_url + '/otpajaxcallback',
                         'data': post,
                         'type': 'POST',
                         beforeSend: function () {
+
                             $('#ajax-loader').show();
+
+
                         },
-                        'success': function (data) {
+                        'success': function (data)
+                        {
+
                             $('#ajax-loader').hide();
                             $("#otp_success").html("OTP Sent Successfully").show().delay(2000).hide(1000);
                         }
@@ -61,6 +51,7 @@
             $('input[name=mobile]').keyup(function () {
                 this.value = this.value.replace(/[^0-9\.]/g, '');
             });
+
 
         }
 
