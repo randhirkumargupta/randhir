6c071a2265c6a245bbc5e8a6f4542e879c9774b2
yatendra-itgd <yatendra.singh@intoday.com>
2017-07-03 17:36:09 +0530

    Sponsor feedback changes

--- sites/all/modules/custom/itg_widget/templates/most-popular-widget.tpl.php
+++ sites/all/modules/custom/itg_widget/templates/most-popular-widget.tpl.php
@@ -9,7 +9,7 @@
           if(_is_sponsor_story_article($entity['nid'])):?>
             <span class="itg-sponsor-title">SPONSORED</span>
           <?php 
-          endif;
+          endif; ?>
           <?php
           if (function_exists('itg_common_get_smiley_title')) {
             echo l(itg_common_get_smiley_title($entity['nid'], 0, 110), "node/" . $entity['nid'], array("html" => TRUE , 'attributes' => array("title" => $entity['title'])));
