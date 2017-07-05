57948e6e07c2e89264e02580994a3e8bf4984c1d
Your Name <you@example.com>
2017-07-05 14:12:27 +0530

    address validation

--- sites/all/modules/custom/itg_be_lucky_today/js/itg_be_lucky_today.js
+++ sites/all/modules/custom/itg_be_lucky_today/js/itg_be_lucky_today.js
@@ -10,7 +10,7 @@
  */
 jQuery(document).ready(function () {
     var str_pattern = /^[a-zA-Z]+$/;
-    var address_pattern = /^[-\/a-zA-Z]+$/;
+    var address_pattern = /^[-\/a-zA-Z\s]+$/;
     jQuery('.itg_be_lucky_today .next').click(function (event) {
         if (jQuery('.itg_be_lucky_today [name="yourname"]').val().trim().length < 2) {
             alert(Drupal.t('Please enter your name'));
