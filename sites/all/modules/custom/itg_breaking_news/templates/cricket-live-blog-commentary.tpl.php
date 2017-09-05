8cf0c571212cdca476660b6f858d29510806cd19
Vishal Kamal <vishal.kamal@intoday.com>
2017-09-05 17:38:04 +0530

    Cricket Live Blog Feature

--- sites/all/modules/custom/itg_breaking_news/templates/cricket-live-blog-commentary.tpl.php
+++ sites/all/modules/custom/itg_breaking_news/templates/cricket-live-blog-commentary.tpl.php
@@ -1,41 +1,3 @@
-<?php
-/**
- * @file
- * Theme implementation for Cricket live Blog.
- * 
- */
-$innings = array_reverse($data->Match->Innings);
-foreach($innings as $inning){
-	$nodes = array_reverse($inning->Node);
-	foreach($nodes as $commentary){
-		$time = date('H:i T', strtotime($commentary->TimeOfDay));
-		?>
-		<?php if($commentary->Ball == 6):?>
-		<?php 
-			$_batsman = explode('|', $commentary->BatDetails);
-			$batDetails = $_batsman[1] . '('. $_batsman[2] . 'b '. $_batsman[3] . '*4' . $_batsman[4] .'*6)';
-			$_bowler = explode('|', $commentary->BowlDetails);
-			$bowlDetails = $_bowler[1] . '-'. $_bowler[2] . '-'. $_bowler[3] . '-' . $_bowler[4];
-		?>
-			<div class="row">  
-			<div class="batBollDetails">
-				
-					<span class="sectiontime">Score  <?php echo $commentary->Score;?> </span>
-					<br>
-					<span class="sectiontime float-left">Batsman</span><span class="sectiontime float-left">Bowler</span>
-					<span class="sectiontime float-left"><?php echo $commentary->Batsman;?><span class='batsman_detail'><?php echo $batDetails;?></span></span><span class="sectiontime float-left"><?php echo $commentary->Bowler;?><span class="bowler_detail"><?php echo $bowlDetails;?></span></span>
-			</div>
-			</div>
-		<?php endif;?>
-		<div class="row">
-		  <div>
-		   <h2 itemprop="articleBody"><strong><?php echo $time; ?>: </strong><?php echo $commentary->Commentary;?></h2>
-		  </div>
-		</div>
-		<?php
-	}
-}
-?>
 
 <style>
 #cricketblog .bolg-content .row{
@@ -77,3 +39,42 @@ foreach($innings as $inning){
 	float:left;
 }
 </style>
+<?php
+/**
+ * @file
+ * Theme implementation for Cricket live Blog.
+ * 
+ */
+$innings = array_reverse($data->Match->Innings);
+foreach($innings as $inning){
+	$nodes = array_reverse($inning->Node);
+	foreach($nodes as $commentary){
+		$time = date('H:i T', strtotime($commentary->TimeOfDay));
+		?>
+		<?php if($commentary->Ball == 6):?>
+		<?php 
+			$_batsman = explode('|', $commentary->BatDetails);
+			$batDetails = $_batsman[1] . '('. $_batsman[2] . 'b '. $_batsman[3] . '*4' . $_batsman[4] .'*6)';
+			$_bowler = explode('|', $commentary->BowlDetails);
+			$bowlDetails = $_bowler[1] . '-'. $_bowler[2] . '-'. $_bowler[3] . '-' . $_bowler[4];
+		?>
+			<div class="row">  
+			<div class="batBollDetails">
+				
+					<span class="sectiontime">Score  <?php echo $commentary->Score;?> </span>
+					<br>
+					<span class="sectiontime float-left">Batsman</span><span class="sectiontime float-left">Bowler</span>
+					<span class="sectiontime float-left"><?php echo $commentary->Batsman;?><span class='batsman_detail'><?php echo $batDetails;?></span></span><span class="sectiontime float-left"><?php echo $commentary->Bowler;?><span class="bowler_detail"><?php echo $bowlDetails;?></span></span>
+			</div>
+			</div>
+		<?php endif;?>
+		<div class="row">
+		  <div>
+		   <p itemprop="articleBody"><strong><?php echo $time; ?>: </strong><?php echo $commentary->Commentary;?></p>
+		  </div>
+		</div>
+		<?php
+	}
+}
+?>
+
