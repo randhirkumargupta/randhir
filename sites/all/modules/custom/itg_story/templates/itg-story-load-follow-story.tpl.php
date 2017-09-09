4530cfbeb72674311caeb3a02ce317c41a719a40
ishwar Chandra Tiwari <ishwartec@2240786.no-reply.drupal.org>
2017-09-09 14:52:05 +0530

    Changes done

--- sites/all/modules/custom/itg_story/templates/itg-story-load-follow-story.tpl.php
+++ sites/all/modules/custom/itg_story/templates/itg-story-load-follow-story.tpl.php
@@ -37,7 +37,7 @@ $node_author = $content["author"];
 ?>
 <ul>
   <?php if ($user->uid > 0): ?>
-    <li class="mhide"><a title = "Submit Your Story" class="def-cur-pointer" href="<?php print $base_url; ?>/personalization/my-content/"><i class="fa fa-share"></i><span><?php print t('1Submit Your Story'); ?></span></a></li>
+    <li class="mhide"><a title = "Submit Your Story" class="def-cur-pointer" href="<?php print $base_url; ?>/personalization/my-content/"><i class="fa fa-share"></i><span><?php print t('Submit Your Story'); ?></span></a></li>
   <?php else: ?>
     <li class="mhide"><a title = "Submit Your Story" class="def-cur-pointer colorbox-load" href="<?php print $base_url; ?>/node/add/ugc?width=650&height=470&iframe=true&type=<?php print $node->type; ?>"><i class="fa fa-share"></i><span><?php print t('1Submit Your Story'); ?></span></a></li>
   <?php endif; ?>
