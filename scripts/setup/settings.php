0a02bb596251b800b4d28e87b38d211fc14c115b
shravan-sonkar <shravan.kumar@kelltontech.com>
2017-09-14 18:55:39 +0530

    Update settings.php

--- scripts/setup/settings.php
+++ scripts/setup/settings.php
@@ -215,13 +215,13 @@
 $databases['default']['default'] = array(
   'driver' => 'autoslave',
   'master' => 'master', // optional, defaults to 'master'
-  //'slave' => 'autoslave', // optional, defaults to 'autoslave'
-  'slave' => array('slave1','slave2'), // optional, defaults to 'autoslave'
+  'slave' => 'autoslave', // optional, defaults to 'autoslave'
+  //'slave' => array('slave1','slave2'), // optional, defaults to 'autoslave'
 // Always use "master" for tables "semaphore" and "sessions"
   'tables' => array('sessions', 'semaphore', 'watchdog'), // optional, defaults to array('sessions', 'semaphore', 'watchdog')
 );
 // setting for UAT
-/*$databases['default']['master'] = array(
+$databases['default']['master'] = array(
   //'database' => 'indiatoday_migrate',
   'database' => 'indiatoday_migrate',
   'username' => 'itgd_it_write',
@@ -251,10 +251,10 @@ $databases['sso_db']['default'] = array(
       'port' => '',
       'driver' => 'mysql',
       'prefix' => '',
-);*/
+);
 // end setting for UAT
 //for production setting
-$databases['default']['master'] = array(
+/*$databases['default']['master'] = array(
   //'database' => 'indiatoday_migrate',
   'database' => 'indiatoday',
   'username' => 'prod_it_write',
@@ -295,7 +295,7 @@ $databases['sso_db']['default'] = array(
       'port' => '3306',
       'driver' => 'mysql',
       'prefix' => '',
-);
+);*/
 
 //end production setting
 // Use locking that supports force master
