d7ba94978f9ee31722b1988bd3dfc5bbf5599b4a
shravan <shravan.kumar@kelltontech.com>
2017-09-24 15:42:51 +0530

    some issue done

--- sites/all/themes/itgadmin/templates/page.tpl.php
+++ sites/all/themes/itgadmin/templates/page.tpl.php
@@ -67,10 +67,14 @@ if (!in_array('administrator', $user->roles)) {
     <i class="fa fa-bell-o"></i>
     <dfn class="itg-final-bell-count">
       <?php 
+        $octopus_html_slug_notification = '';
         if(function_exists('itg_octopus_get_slug_notification')){
           $itg_octopus_get_slug_notification = '';
           $itg_octopus_get_slug_notification = itg_octopus_get_slug_notification();
           print $itg_octopus_get_slug_notification['count'];
+          if (isset($itg_octopus_get_slug_notification['html'])) {
+            $octopus_html_slug_notification = $itg_octopus_get_slug_notification['html'];
+          }
         }
       ?>
     </dfn>
@@ -89,7 +93,7 @@ if (!in_array('administrator', $user->roles)) {
     print '  Username - '. $user->name.' | Role - '.$role_display.' | '.$myaccount;
    ?>
 </span>                                                  
-<div class="bell-notice itg-final-bell-notice"><?php print $itg_octopus_get_slug_notification['html']; ?></div>
+<div class="bell-notice itg-final-bell-notice"><?php print $octopus_html_slug_notification; ?></div>
 
 </div>
 <?php } ?>
