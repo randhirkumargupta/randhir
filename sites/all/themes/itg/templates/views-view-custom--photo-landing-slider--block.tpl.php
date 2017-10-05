e7decc634894e4d7b7ac62d621278a7ac599ffbe
rajveer singh <rajveer.gang@gmail.com>
2017-10-05 15:09:38 +0530

    ITG-3056 #comment working on menu manager cache setting

--- sites/all/themes/itg/templates/views-view-custom--photo-landing-slider--block.tpl.php
+++ sites/all/themes/itg/templates/views-view-custom--photo-landing-slider--block.tpl.php
@@ -226,11 +226,7 @@ $argum = base64_encode(arg(1));
 </div>
 
 <?php
-    $explode = explode("?", $_SERVER['REQUEST_URI']);
-    $initial_slide = 0;
-    if(count($explode) > 1) {
-        $initial_slide = is_integer(end($explode)) ? end($explode) : 0;
-    }
+    $initial_slide = isset($_GET['photo']) ? $_GET['photo'] : 0;
 ?>
 <script>
     jQuery(document).ready(function (e) {
@@ -294,30 +290,30 @@ $argum = base64_encode(arg(1));
             asNavFor: '.slick-thumbs-slider, .slickslide, .counterslide'
         });
 // Photogallery slider javascript
-//        jQuery(document).ready(function () {
-//            jQuery(".slick-arrow , li.slick-slide").on("click", function () {
-//                var active_slide = jQuery(".slick-active").attr("data-slick-index");
-//                var current_url = window.location.href.split('?')[0];
-//                if (active_slide > 0) {
-//                    window.history.pushState("", "", current_url + "?" + active_slide);
-//                } else {
-//                    // If frist slide then put pull without query string.
-//                    window.history.pushState("", "", current_url);
-//                }
-//            });
-//        });
+        jQuery(document).ready(function () {
+            jQuery(".slick-arrow , li.slick-slide").on("click", function () {
+                var active_slide = jQuery(".slick-active").attr("data-slick-index");
+                var current_url = window.location.href.split('?')[0];
+                if (active_slide > 0) {
+                    window.history.pushState("", "", current_url + "?photo=" + active_slide);
+                } else {
+                    // If frist slide then put pull without query string.
+                    window.history.pushState("", "", current_url);
+                }
+            });
+        });
     });
     // Photogallery slider javascript
     // Handle Thumb for active set class
-//    jQuery(window).load(function() {
-//        var active_slide = jQuery(".slick-active").attr("data-slick-index");
-//        jQuery( '.slick-thumbs li' ).removeClass('slick-current');
-//        jQuery( '.slick-thumbs li' ).each( function(key , item) {
-//            if(key == active_slide) {
-//                jQuery( this).addClass("slick-current");
-//            }
-//        });
-//    });
+    jQuery(window).load(function() {
+        var active_slide = jQuery(".slick-active").attr("data-slick-index");
+        jQuery( '.slick-thumbs li' ).removeClass('slick-current');
+        jQuery( '.slick-thumbs li' ).each( function(key , item) {
+            if(key == active_slide) {
+                jQuery( this).addClass("slick-current");
+            }
+        });
+    });
 </script>
 
 
