72672ce1be034e530fa30342a895421588c029f4
shravan-sonkar <shravan.kumar@kelltontech.com>
2017-10-13 15:43:27 +0530

    Update local.settings.php

--- scripts/setup/local.settings.php
+++ scripts/setup/local.settings.php
@@ -3,10 +3,10 @@
 $conf['mongodb_connections'] = array(
   'default' => array(                             // Connection name/alias
     'host' => '10.6.210.117,10.6.210.214,10.6.210.218',
-    'host' => 'mongodb://10.6.210.19',                      // Omit USER:PASS@ if Mongo isn't configured to use authentication.
+    //'host' => 'mongodb://10.6.210.19',                      // Omit USER:PASS@ if Mongo isn't configured to use authentication.
     //'host' => 'mongodb://10.6.101.66',//prod
     'db' => 'itgcmsmongo',                   // Database name. Make something up, mongodb will automatically create the database.
-    //'connection_options' => array('replicaSet' => 'replicasetname'),
+    'connection_options' => array('replicaSet' => 'replicasetname'),
   ),
 );
 
