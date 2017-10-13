8a89e1629cd6a175176d99bc9893ab85bf0b3863
yatendra-itgd <yatendra.singh@intoday.com>
2017-10-13 13:56:54 +0530

    Section changes in byline popup

--- sites/all/modules/custom/itg_reporter/js/itg_reporter.js
+++ sites/all/modules/custom/itg_reporter/js/itg_reporter.js
@@ -35,18 +35,20 @@
                 $('.form-item-field-constituancy-und-0-value').hide();
                 $('#edit-field-party-name').hide();
             }
-            if (initialhasexist) {
-                // $('#edit-field-story-category').show();
-                $('.form-item-itg-section').show();
-              
-            } else {
-                //$('#edit-field-story-category').hide();
-                $('.form-item-itg-section').hide();
-                $('.form-item-itg-category').hide();
-                $('.form-item-itg-sub-category').hide();
-                $('.form-item-itg-sub-sub-category').hide();
-                $('.form-item-itg-sub-sub-sub-category').hide();
-            }
+            if(initialhasexist !== undefined){
+				if (initialhasexist) {
+					// $('#edit-field-story-category').show();
+					$('.form-item-itg-section').show();
+				  
+				} else {
+					//$('#edit-field-story-category').hide();
+					$('.form-item-itg-section').hide();
+					$('.form-item-itg-category').hide();
+					$('.form-item-itg-sub-category').hide();
+					$('.form-item-itg-sub-sub-category').hide();
+					$('.form-item-itg-sub-sub-sub-category').hide();
+				}
+			}            
 
             if (initialhasexist_reporter) {
                 $('#edit-field-reporter-profile-type').show();
