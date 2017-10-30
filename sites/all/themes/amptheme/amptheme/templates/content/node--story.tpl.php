5989582026fc572206bce0f823b7dd6d0fbcf03e
ankushgautm <ankushgautam@kelltontech.com>
2017-10-30 15:55:13 +0530

    #comment work on sso

--- sites/all/themes/amptheme/amptheme/templates/content/node--story.tpl.php
+++ sites/all/themes/amptheme/amptheme/templates/content/node--story.tpl.php
@@ -304,7 +304,8 @@ if (!empty($content)):
                 <?php if (!empty($node->field_story_rating)): ?>
                   <div class="movie-rating">
                       <div class="grey-star"><amp-img src="<?php print $base_url .'/'. path_to_theme().'/images/rating-grey.png'?>" width="111" height="18"></amp-img></div>
-                      <div class="red-star" style="width: <?php print $node->field_story_rating[LANGUAGE_NONE]['0']['value'] * 22 . "px"; ?>"><amp-img src="<?php print $base_url .'/'. path_to_theme().'/images/rating-red.png'?>" width="111" height="18"></amp-img></div>
+                      <!--<div class="red-star" data-value="<?php //print $node->field_story_rating[LANGUAGE_NONE]['0']['value'];?>" style="width: <?php //print $node->field_story_rating[LANGUAGE_NONE]['0']['value'] * 22 . "px"; ?>"><amp-img src="<?php //print $base_url .'/'. path_to_theme().'/images/rating-red.png'?>" width="111" height="18"></amp-img></div>-->
+                      <div class="red-star" data-value="<?php print $node->field_story_rating[LANGUAGE_NONE]['0']['value']; ?>"><amp-img src="<?php print $base_url .'/'. path_to_theme().'/images/rating-red.png'?>" width="111" height="18"></amp-img></div>
                   </div>                            
                 <?php endif; ?>
                 <div class="movie-detail">
