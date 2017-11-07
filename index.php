3a34960bc8e4ea6e42624818966d7ed4f1d4c231
Mahesh Salaria <mahesh.salaria@kelltontech.com>
2017-11-08 01:04:03 +0530

    Updated index.php

--- index.php
+++ index.php
@@ -17,5 +17,5 @@
 define('DRUPAL_ROOT', getcwd());
 
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
-drupal_bootstrap(DRUPAL_BOOTSTRAP_PAGE_CACHE);
+drupal_bootstrap(DRUPAL_BOOTSTRAP_PAGE_HEADER);
 menu_execute_active_handler();
