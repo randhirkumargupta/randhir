ee297fed0059889ba12ba5ab0ca95c298d5a3ebc
shravan-sonkar <shravan.kumar@kelltontech.com>
2017-11-08 15:43:42 +0530

    Update settings.php

--- scripts/setup/settings.php
+++ scripts/setup/settings.php
@@ -303,8 +303,8 @@ $databases['default']['slave'][] = array (
 
 $databases['default']['default'] = array (
   'driver' => 'autoslave',
-  'master' => array('master'),
-  'slave' => array('slave'),
+  'master' => array('master', 'slave'),
+  'slave' => array('slave', 'master'),
   'replication lag' => 2, // (defaults to $conf['autoslave_assumed_replication_lag'])
   'global replication lag' => TRUE, // Make replication lag mitigation work cross requests for all users. Defaults to TRUE.
   //'invalidation path' => 'sites/default/files', // Path to store invalidation file for flagging unavailable connections. Defaults to empty.
