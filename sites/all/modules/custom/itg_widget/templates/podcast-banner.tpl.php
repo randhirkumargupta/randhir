8e4fdc27477b01f7faf8284e00d7c725ef415a86
Lovish Sharma <lovish.sharma@kelltontech.com>
2017-11-10 19:22:13 +0530

    work on bug

--- sites/all/modules/custom/itg_widget/templates/podcast-banner.tpl.php
+++ sites/all/modules/custom/itg_widget/templates/podcast-banner.tpl.php
@@ -11,86 +11,87 @@ $podcast_audio_element_id = array();
                     <script src="<?php print $base_url . '/' . drupal_get_path('module', 'itg_podcast'); ?>/jwplayer/jwplayer.js"></script>
                     <script>jwplayer.key = "Cbz5fuKQAlYHtZgBSR0G/4GgYFO7YTb0k8Ankg==";</script>
                     <div class="slider-header">
-                        <?php
-                        foreach ($podcast_banner_array as $podcast_id => $podcast_array) {
-                          if (!empty($podcast_banner_array[$podcast_id]['audio']['uri'])) {
-                            $podcast_audio_element_id[] = $podcast_id;
-                          }
-                        }
-                        ?>
+												<?php
+												foreach ($podcast_banner_array as $podcast_id => $podcast_array) {
+													if (!empty($podcast_banner_array[$podcast_id]['audio']['uri'])) {
+														$podcast_audio_element_id[] = $podcast_id;
+													}
+												}
+												?>
                         <!-- Header slider -->
-                        <?php 
-                        $podcast_thumbnail = "";
-                        if (!empty($podcast_banner_array)) :
-                          foreach ($podcast_banner_array as $podcast_id => $podcast_array) {
-                            $podcast_image = $podcast_banner_array[$podcast_id]['image']['uri'];
+												<?php
+												$podcast_thumbnail = "";
+												if (!empty($podcast_banner_array)) :
+													foreach ($podcast_banner_array as $podcast_id => $podcast_array) {
+														$podcast_image = $podcast_banner_array[$podcast_id]['image']['uri'];
 														$podcast_image_alt = $podcast_banner_array[$podcast_id]['image']['alt'];
 														$podcast_image_title = $podcast_banner_array[$podcast_id]['image']['title'];
-                            $description = $podcast_banner_array[$podcast_id]['desc'];
-                            ?>
-                            <div class="podcast-slide">
-                                <div class="pic">
-                                    <?php
-                                    $podcat_uri = image_style_url("widget_small", $podcast_image);
-                                    if (function_exists('url_exists') && url_exists($podcat_uri)) {
-                                      ?>
-                                      <img src="<?php print image_style_url("widget_small", $podcast_image); ?>" alt="<?php print $podcast_image_alt; ?>" title="<?php print $podcast_image_title; ?>" />
-                                      <?php
-                                    }
-                                    else {
-                                      print "<img  src='" . file_create_url(file_default_scheme() . '://../sites/all/themes/itg/images/' . 'itg_image170x127.jpg')."' alt='' title='' />";
-                                    }
-                                    ?>
-                                </div>
-                                <div class="podcast-detail">      
-    <?php if (!empty($podcast_banner_array[$podcast_id]['audio']['uri'])) : ?>
-                                      <div class="podcast-instance" id="podcast-<?php print $podcast_id; ?>">Loading the player...</div>
-                                      <script type="text/javascript">
-                                        var playerInstance = jwplayer("podcast-<?php print $podcast_id; ?>");
-                                        playerInstance.setup({
-                                            file: "<?php print file_create_url($podcast_banner_array[$podcast_id]['audio']['uri']); ?>",
-                                            width: 440,
-                                            height: 40,
-                                            events: {
-                                                onPlay: function () {
-      <?php
-      foreach ($podcast_audio_element_id as $pod_id) {
-        if ($pod_id != $podcast_id) {
-          echo "jwplayer('podcast-$pod_id').pause(true);";
-        }
-      }
-      ?>
-                                                }
-                                            }
-                                        });
-                                      </script>
-                                    <?php endif; ?>
-                                    <?php if (!empty($description)) : ?>
-                                      <p>  <?php print mb_strimwidth($description, 0, 210, "..") ?></p>
-    <?php endif; ?>
-                                </div>
-                            </div>
+														$description = $podcast_banner_array[$podcast_id]['desc'];
+														?>
+														<div class="podcast-slide">
+																<div class="pic">
+																		<?php
+																		$podcat_uri = image_style_url("widget_small", $podcast_image);
+																		if (function_exists('url_exists') && url_exists($podcat_uri)) {
+																			?>
+																			<img src="<?php print image_style_url("widget_small", $podcast_image); ?>" alt="<?php print $podcast_image_alt; ?>" title="<?php print $podcast_image_title; ?>" />
+																			<?php
+																		}
+																		else {
+																			print "<img  src='" . file_create_url(file_default_scheme() . '://../sites/all/themes/itg/images/' . 'itg_image170x127.jpg') . "' alt='' title='' />";
+																		}
+																		?>
+																</div>
+																<div class="podcast-detail">      
+																		<?php if (!empty($podcast_banner_array[$podcast_id]['audio']['uri'])) : ?>
+																			<div class="podcast-instance" id="podcast-<?php print $podcast_id; ?>">Loading the player...</div>
+																			<script type="text/javascript">
+			                                  var playerInstance = jwplayer("podcast-<?php print $podcast_id; ?>");
+			                                  playerInstance.setup({
+			                                      file: "<?php print file_create_url($podcast_banner_array[$podcast_id]['audio']['uri']); ?>",
+			                                      width: 440,
+			                                      height: 40,
+			                                      events: {
+			                                          onPlay: function () {
+			<?php
+			foreach ($podcast_audio_element_id as $pod_id) {
+				if ($pod_id != $podcast_id) {
+					echo "jwplayer('podcast-$pod_id').pause(true);";
+				}
+			}
+			?>
+			                                          }
+			                                      }
+			                                  });
+																			</script>
+																		<?php endif; ?>
+																		<?php if (!empty($description)) : ?>
+																			<p>  <?php print mb_strimwidth($description, 0, 210, "..") ?></p>
+																		<?php endif; ?>
+																</div>
+														</div>
 
-                            <?php
+														<?php
 														$flag = 0;
-                            $extra_large_image_url = image_style_url("widget_very_small", $podcast_image);
-                            if (function_exists('url_exists') && url_exists($extra_large_image_url)) {
-                              $extra_large_final_image = $extra_large_image_url;
+														$extra_large_image_url = image_style_url("widget_very_small", $podcast_image);
+														if (function_exists('url_exists') && url_exists($extra_large_image_url)) {
+															$extra_large_final_image = $extra_large_image_url;
 															$flag = 1;
-                            }
-                            else {
-                              $extra_large_final_image = file_create_url(file_default_scheme() . '://../sites/all/themes/itg/images/' . 'itg_image88x66.jpg');
-                            }
-														if($flag == 1){
-															$very_small_image = '<img class="podcast-thumb-img" src="' . $extra_large_final_image . '" alt="'.$podcast_image_alt.'" title="'.$podcast_image_title.'" />';
-														}else{
+														}
+														else {
+															$extra_large_final_image = file_create_url(file_default_scheme() . '://../sites/all/themes/itg/images/' . 'itg_image88x66.jpg');
+														}
+														if ($flag == 1) {
+															$very_small_image = '<img class="podcast-thumb-img" src="' . $extra_large_final_image . '" alt="' . $podcast_image_alt . '" title="' . $podcast_image_title . '" />';
+														}
+														else {
 															$very_small_image = '<img class="podcast-thumb-img" src="' . $extra_large_final_image . '" alt="" title="" />';
 														}
-                            $podcast_thumbnail .= "<div class='podcast-nav-item'> $very_small_image </div>";
-                          }
-                        endif;
-                        $podcast_thumbnail .= "";
-                        ?>
+														$podcast_thumbnail .= "<div class='podcast-nav-item'> $very_small_image </div>";
+													}
+												endif;
+												$podcast_thumbnail .= "";
+												?>
                     </div>
                 </div>
                 <div class="podcast-header-bottom">
@@ -102,11 +103,11 @@ $podcast_audio_element_id = array();
         </div>
         <div class="col-md-4">
             <div class="itg-adds">
-                <?php
-                $block = block_load('itg_ads', ADS_RHS1);
-                $render_array = _block_get_renderable_array(_block_render_blocks(array($block)));
-                print render($render_array);
-                ?>
+								<?php
+								$block = block_load('itg_ads', ADS_RHS1);
+								$render_array = _block_get_renderable_array(_block_render_blocks(array($block)));
+								print render($render_array);
+								?>
             </div>
         </div>
     </div>
