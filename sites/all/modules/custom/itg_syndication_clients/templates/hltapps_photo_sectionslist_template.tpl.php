15e563d5eeed7f9eeec8681a2de9f8658d96d378
Vishal Kamal <vishal.kamal@intoday.com>
2017-11-10 17:05:39 +0530

    Syndication clients hltapps

--- sites/all/modules/custom/itg_syndication_clients/templates/hltapps_photo_sectionslist_template.tpl.php
+++ sites/all/modules/custom/itg_syndication_clients/templates/hltapps_photo_sectionslist_template.tpl.php
@@ -2,39 +2,39 @@
 <id>1</id>
 <itemcount>7</itemcount>
 <item>
-<title>
-<![CDATA[ india ]]>
-</title>
-<url>india.xml</url>
+    <title>
+        <![CDATA[ india ]]>
+    </title>
+    <url>india.xml</url>
 </item>
 <item>
-<title>
-<![CDATA[ world ]]>
-</title>
-<url>world.xml</url>
+    <title>
+        <![CDATA[ world ]]>
+    </title>
+    <url>world.xml</url>
 </item>
 <item>
-<title>
-<![CDATA[ sports ]]>
-</title>
-<url>sports.xml</url>
+    <title>
+        <![CDATA[ sports ]]>
+    </title>
+    <url>sports.xml</url>
 </item>
 <item>
-<title>
-<![CDATA[ entertainment ]]>
-</title>
-<url>entertainment.xml</url>
+    <title>
+        <![CDATA[ entertainment ]]>
+    </title>
+    <url>entertainment.xml</url>
 </item>
 <item>
-<title>
-<![CDATA[ business ]]>
-</title>
-<url>business.xml</url>
+    <title>
+        <![CDATA[ business ]]>
+    </title>
+    <url>business.xml</url>
 </item>
 <item>
-<title>
-<![CDATA[ election ]]>
-</title>
-<url>election.xml</url>
+    <title>
+        <![CDATA[ election ]]>
+    </title>
+    <url>election.xml</url>
 </item>
 </root>
--- sites/all/modules/custom/itg_syndication_clients/templates/hltapps_video_sectionslist_template.tpl.php
+++ sites/all/modules/custom/itg_syndication_clients/templates/hltapps_video_sectionslist_template.tpl.php
@@ -2,39 +2,39 @@
 <id>1</id>
 <itemcount>6</itemcount>
 <item>
-<title>
-<![CDATA[ india ]]>
-</title>
-<url>india.xml</url>
+    <title>
+        <![CDATA[ india ]]>
+    </title>
+    <url>india.xml</url>
 </item>
 <item>
-<title>
-<![CDATA[ world ]]>
-</title>
-<url>world.xml</url>
+    <title>
+        <![CDATA[ world ]]>
+    </title>
+    <url>world.xml</url>
 </item>
 <item>
-<title>
-<![CDATA[ so sorry ]]>
-</title>
-<url>so-sorry.xml</url>
+    <title>
+        <![CDATA[ so sorry ]]>
+    </title>
+    <url>so-sorry.xml</url>
 </item>
 <item>
-<title>
-<![CDATA[ entertainment ]]>
-</title>
-<url>entertainment.xml</url>
+    <title>
+        <![CDATA[ entertainment ]]>
+    </title>
+    <url>entertainment.xml</url>
 </item>
 <item>
-<title>
-<![CDATA[ business ]]>
-</title>
-<url>business.xml</url>
+    <title>
+        <![CDATA[ business ]]>
+    </title>
+    <url>business.xml</url>
 </item>
 <item>
-<title>
-<![CDATA[ election ]]>
-</title>
-<url>election.xml</url>
+    <title>
+        <![CDATA[ election ]]>
+    </title>
+    <url>election.xml</url>
 </item>
 </root>
