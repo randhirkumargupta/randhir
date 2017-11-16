b6e110d9e56d707fb148cd1aa6ef303e63018ed6
shravan-sonkar <shravan.kumar@kelltontech.com>
2017-11-16 19:56:18 +0530

    Update local.settings.php

--- scripts/setup/local.settings.php
+++ scripts/setup/local.settings.php
@@ -29,33 +29,13 @@ ini_set('session.cookie_lifetime', 2000000);
 #$conf['queue_default_class'] = 'MongoDBQueue';
 
 # Memcache specific settings
-/*$conf['cache_backends'][] = 'sites/all/modules/memcache/memcache.inc';
-$conf['cache_default_class'] = 'MemCacheDrupal';
-$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
-$conf['cache_class_cache_update'] = 'DrupalDatabaseCache';
-$conf['memcache_stampede_protection'] = TRUE;*/
 
-//$conf['memcache_storage_debug'] = TRUE;
-//$conf['memcache_extension'] = 'Memcache';
-
-//$conf['memcache_storage_key_prefix'] = 'itgmem';
-//$conf['session_inc'] = 'sites/all/modules/contrib/memcache_storage/includes/session.inc';
-
-// setting for UAT
-//$conf['memcache_servers'] = array(
-//  'itgd-drupal-memc-dev.yissgx.cfg.aps1.cache.amazonaws.com:11211' => 'default',
-//);
-
-// setting for production
- /*$conf['memcache_servers'] = array(
-  'itgd-cms-mem-prod-1.yissgx.cfg.aps1.cache.amazonaws.com:11211' => 'default',
-);*/
-
+//uat:itgd-drupal-memc-dev.yissgx.cfg.aps1.cache.amazonaws.com:11211
+//prod:itgd-cms-mem-prod-1.yissgx.cfg.aps1.cache.amazonaws.com:11211
 $conf = array(
 'cache_backends' => array('sites/all/modules/memcache/memcache.inc'),
 'cache_default_class' => 'MemCacheDrupal',
 'cache_class_cache_form' => 'DrupalDatabaseCache',
-//  'session_inc' => './sites/all/modules/memcache/memcache-session.inc',
   'memcache_servers' => array(
    'itgd-cms-mem-prod-1.yissgx.cfg.aps1.cache.amazonaws.com:11211' => 'default',   
   ),
@@ -63,8 +43,7 @@ $conf = array(
     'cache' => 'default',
     'cache_filter' => 'default',
     'cache_menu' => 'default',
-    'cache_page' => 'default',
-   // 'session' => 'session',
+    'cache_page' => 'default',   
     'users' => 'default',
     'cache_pathdst' => 'default',
     'cache_pathsrc' => 'default',
@@ -75,9 +54,11 @@ $conf['path_inc'] = 'sites/all/modules/contrib/pathcache/path.inc';
 
 
 #MongoDB 
+//prodreplica and uatreplica
 $conf['mongodb_connections'] = array(
   'default' => array(
-    'host' => 'mongodb://prod_write:Pr0d_wr1te654@mongodb1,mongodb2,mongodb3/itgcmsmongo',
+    'host' => 'mongodb://prod_write:Pr0d_wr1te654@mongodb1,mongodb2,mongodb3/itgcmsmongo',//prod
+    //'host' => 'mongodb://itoday_write:1t0day_wr1te111@mongodb1,mongodb2,mongodb3/itgcmsmongo',// uat
     'db' => 'itgcmsmongo',
     'connection_options' => array('replicaSet' => 'prodreplica'),
   ),
