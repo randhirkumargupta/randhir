83ba6569f8fd90698efff2b17068f228429eb5aa
Vishal Kamal <vishal.kamal@intoday.com>
2017-12-16 17:24:53 +0530

    changes

--- sites/all/modules/custom/itg_layout_manager/templates/layout-state-election-map.tpl.php
+++ sites/all/modules/custom/itg_layout_manager/templates/layout-state-election-map.tpl.php
@@ -2,6 +2,7 @@
 <div class="state-election-map-wrapper">
 <div class="full_state_graph">
 	<div class="breadMap"><div class="breadMapLink"><?php echo $state_name;?> Detailed Result</div><style>
+.breadMap .breadMapLink {font-size: 29px;color: #000;}
 .kikerNewsRightSec { width:100%}
 .kikerNewsRightSec b { margin:5px 0; display:inline-block}
 .kikerNewsRightSec ul {list-style-type: disc;color: #c00;margin-left: 20px;}
