d99b6acb10add37371ea1c687113a6f39ec7946a
yatendra-itgd <yatendra.singh@intoday.com>
2017-12-21 12:22:39 +0530

    AMP Seach changes

--- sites/all/themes/amptheme/amptheme/templates/layout/html.tpl.php
+++ sites/all/themes/amptheme/amptheme/templates/layout/html.tpl.php
@@ -54,7 +54,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <script async src="https://cdn.ampproject.org/v0.js"></script>
         <script async custom-element="amp-carousel" src="https://cdn.ampproject.org/v0/amp-carousel-0.1.js"></script>
-		<script async custom-element="amp-bind" src="https://cdn.ampproject.org/v0/amp-bind-0.1.js"></script>
+        <script async custom-element="amp-bind" src="https://cdn.ampproject.org/v0/amp-bind-0.1.js"></script>
         <script async custom-element="amp-dailymotion" src="https://cdn.ampproject.org/v0/amp-dailymotion-0.1.js"></script>
         <script async custom-element="amp-jwplayer" src="https://cdn.ampproject.org/v0/amp-jwplayer-0.1.js"></script>
         <script async custom-element="amp-accordion" src="https://cdn.ampproject.org/v0/amp-accordion-0.1.js"></script>
