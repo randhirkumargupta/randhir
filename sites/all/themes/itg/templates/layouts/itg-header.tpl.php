058cb478a5cefffdd7ad67a4cc8eac8c7e347253
arun2016 <mishrasoft2009@gmail.com>
2017-12-21 20:04:28 +0530

    remove home tab

--- sites/all/themes/itg/templates/layouts/itg-header.tpl.php
+++ sites/all/themes/itg/templates/layouts/itg-header.tpl.php
@@ -86,15 +86,7 @@ $uri = base64_encode($scheme . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
                 // Contion to check fucntion isset.
                 $load_parent = (null != arg(2)) ? itg_common_taxonomy_get_parents(arg(2)) : array();
                 $default_image = file_create_url(file_default_scheme() . '://../sites/all/themes/itg/images/default_for_all_48_32.jpeg');
-                ?>
-                <li class="nav-items ripple-effect hidden-desktop">
-                    <a href="<?php print $base_url ?>" class="second-level-child">`
-                        <span class='menu-icons hidden-desktop'><img class='itg-user-icon-home' src="<?php print $default_image ?>" alt='' /></span>
-<?php print t('Home'); ?>
-                    </a>
-                </li>
-
-                <?php
+              
                 $menu_manager = !empty($data['menu_manager']) ? $data['menu_manager'] : '';
                 // Contion to check fucntion isset.
                 $load_parent = (null != arg(2)) ? itg_common_taxonomy_get_parents(arg(2)) : array();
