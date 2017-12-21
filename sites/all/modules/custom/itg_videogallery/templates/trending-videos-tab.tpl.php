bd95323bde25e12110923f69938106bef69353fa
yatendra-itgd <yatendra.singh@intoday.com>
2017-12-21 17:36:33 +0530

    trending video iamge changes

--- sites/all/modules/custom/itg_videogallery/templates/trending-videos-tab.tpl.php
+++ sites/all/modules/custom/itg_videogallery/templates/trending-videos-tab.tpl.php
@@ -4,8 +4,8 @@
         <li class="<?php print $entity['type'] ?> trending-videos-list">
             <?php if (!empty($entity['si_file_uri']) && file_exists($entity['si_file_uri'])) { ?>            
               <a class="pic" href="<?php print $base_url . '/' . drupal_get_path_alias("node/" . $entity['nid']); ?>">
-                  <?php $extra_large_image_url = image_style_url("widget_small", $entity['si_file_uri']); ?>
-                  <img src="<?php print $extra_large_image_url; ?>" alt="<?php echo $entity['field_story_small_image_alt'] ?>" title="<?php echo $entity['field_story_small_image_title'] ?>" />
+                  <?php //$extra_large_image_url = image_style_url("widget_small", $entity['si_file_uri']); ?>
+                  <img src="<?php print file_create_url($entity['si_file_uri']); ?>" alt="<?php echo $entity['field_story_small_image_alt'] ?>" title="<?php echo $entity['field_story_small_image_title'] ?>" />
               </a>
               <?php
             }
