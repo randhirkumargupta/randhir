7494aa5880e47d3c1036a3171719d1c31630fca6
Mahesh Salaria <mahesh.salaria@kelltontech.com>
2018-01-04 05:48:43 +0530

    Fixed base_url

--- scripts/setup/settings.php
+++ scripts/setup/settings.php
@@ -720,6 +720,6 @@ if (file_exists($local_settings)) {
 $conf['image_suppress_itok_output'] = TRUE;
 $conf['image_allow_insecure_derivatives'] = TRUE;
 $conf['block_cache_bypass_node_grants'] = TRUE;
-$base_url = 'https://www.indiatoday.in';
+$base_url = 'https://'.$_SERVER['SERVER_NAME'];
 //$conf['cache_default_class'] = 'ConsistentCache';
 //$conf['consistent_cache_default_safe'] = FALSE;
