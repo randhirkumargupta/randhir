b692fa185b5b752fae9bbf0dca169e06708f447d
Mahesh Salaria <mahesh.salaria@kelltontech.com>
2018-01-05 14:27:20 +0530

    Update settings.php

--- scripts/setup/settings.php
+++ scripts/setup/settings.php
@@ -331,7 +331,8 @@ $databases['sso_db']['default'] = array(
 //end production setting
 
 // Use locking that supports force master
-$conf['lock_inc'] = 'sites/all/modules/contrib/autoslave/lock.inc';
+// $conf['lock_inc'] = 'sites/all/modules/contrib/autoslave/lock.inc';
+$conf['lock_inc'] = 'sites/all/modules/contrib/autoslave/memcache-lock.inc';
 
 //$conf['cache_default_class'] = 'AutoslaveCache';
 //$conf['autoslave_cache_default_class'] = 'ConsistentCache';
