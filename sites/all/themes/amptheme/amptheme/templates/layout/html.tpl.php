8ed93111628fa41cff5be92fd24c5705ccb8f815
yatendra-itgd <yatendra.singh@intoday.com>
2018-01-05 17:30:19 +0530

    AMP Ad spacing

--- sites/all/themes/amptheme/amptheme/templates/layout/html.tpl.php
+++ sites/all/themes/amptheme/amptheme/templates/layout/html.tpl.php
@@ -1445,6 +1445,7 @@ ul.header-menu li {
     border-bottom: 1px solid #ccc;
     width: 270px;
 }
+.story_ad_block.custom-amp-ad{margin-top:15px;}
 button.searchbut{float: right; width: 27px; height: 27px; margin-right: 5px;} button.searchbut .fa-search { top: 1px; right: 0px;}
 @media only screen and (max-width: 767px) {
     ul.pron-cons-img {
