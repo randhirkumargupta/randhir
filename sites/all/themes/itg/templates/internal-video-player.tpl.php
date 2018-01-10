8e6aa6b03799db5e0408ad0ede9f5f07e463e72b
Shashank Singh <shashank.singh@intoday.com>
2018-01-11 02:31:17 +0530

    update code

--- sites/all/themes/itg/templates/internal-video-player.tpl.php
+++ sites/all/themes/itg/templates/internal-video-player.tpl.php
@@ -76,7 +76,6 @@ $player_content = itg_videogallery_make_parm_for_jwpalyer($video_all_data, $used
   loadplayerjw();
   <?php
   $arg = arg();
-  p($arg);
   if(($arg[0] == 'video' && $arg[2] == 'embed')) { ?>
    //ga('create', '<?php //echo $data_video["ga_code"]; ?>', 'auto');
    ga('create', 'UA-20047041-23', 'auto');
