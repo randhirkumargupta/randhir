5b535d9330e48bf9079b2c39029d65213c7685b7
arun2016 <mishrasoft2009@gmail.com>
2018-01-11 17:20:14 +0530

    image slider

--- sites/all/modules/custom/itg_bestcolleges/templates/bestcollege-image-slider.tpl.php
+++ sites/all/modules/custom/itg_bestcolleges/templates/bestcollege-image-slider.tpl.php
@@ -1,32 +1,31 @@
-<div class="col-sm-12 related-story-list">
-    <div class="row">
-    <div class="clearfix">
-    <?php 
-    if(is_array($data) && count($data) > 0) {
-
-     foreach ($data as $key => $value){ 
-       $file_uri = $value['uri'];
-         ?>
-         <div class="midstoryleft">
-             <a href="<?php echo $base_url . '/' . drupal_get_path_alias("node/" . $value['nid']); ?>" target="_blank">
+<div class="slider_besrCollage slider_outer">
+    <?php foreach($data as $index => $row) { 
+         $file_uri = $row['uri'];
+      ?>
+    <div>
+      <a href="<?php echo $base_url . '/' . drupal_get_path_alias("node/" . $row['nid']); ?>" target="_blank">
        <?php if (!empty($file_uri)) : ?>
                <img src="<?php print $file_uri; ?>" alt="" title="" width="125" align="left" height="93" />
        <?php else : ?>
                  <img src="<?php print  file_create_url(file_default_scheme() . '://../sites/all/themes/itg/images/' . 'itg_image483x271.jpg'); ?>" width="125" align="left" height="93" />
        <?php endif; ?>
-             </a>
-           <div class="midstorydetail">
-               <div class="midstorytitle"><a href="<?php echo $base_url . '/' . drupal_get_path_alias("node/" . $value['nid']); ?>" target="_blank"><?php print $value['title']; ?></a>
-               </div>
-               <div class="midstoryintro"><?php print $value['title']; ?></div>
-           </div>
-         </div>
-    <?php
+      </a>
 
-     }
-    }  
-    ?>
-    </div>
+                <div class="carousel-caption">
+                <?php echo l(mb_strimwidth(strip_tags($row['title']), 0, 105, ".."), $base_url . '/' . drupal_get_path_alias("node/{$row['nid']}")) ?>
+                  
+                </div>
         </div>
-
+  <?php } ?>
 </div>
+
+<script>
+jQuery(document).ready(function () {
+    jQuery('.slider_outer').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: false
+    });
+});
+</script>
