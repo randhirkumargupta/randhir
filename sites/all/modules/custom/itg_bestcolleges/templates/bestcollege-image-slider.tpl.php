a4d535433f1d54fa98fe148e4882dc17fdacd550
arun2016 <mishrasoft2009@gmail.com>
2018-01-11 19:02:56 +0530

    image slide fix

--- sites/all/modules/custom/itg_bestcolleges/templates/bestcollege-image-slider.tpl.php
+++ sites/all/modules/custom/itg_bestcolleges/templates/bestcollege-image-slider.tpl.php
@@ -1,11 +1,12 @@
 <div class="slider_besrCollage slider_outer">
     <?php foreach($data as $index => $row) { 
-         $file_uri = $row['uri'];
+         $file = file_load($row['field_story_extra_large_image_fid']);
+         $image = file_create_url($file->uri);
       ?>
     <div>
       <a href="<?php echo $base_url . '/' . drupal_get_path_alias("node/" . $row['nid']); ?>" target="_blank">
-       <?php if (!empty($file_uri)) : ?>
-               <img src="<?php print $file_uri; ?>" alt="" title="" width="125" align="left" height="93" />
+       <?php if (!empty($image)) : ?>
+               <img src="<?php print $image; ?>" alt="" title="" width="125" align="left" height="93" />
        <?php else : ?>
                  <img src="<?php print  file_create_url(file_default_scheme() . '://../sites/all/themes/itg/images/' . 'itg_image483x271.jpg'); ?>" width="125" align="left" height="93" />
        <?php endif; ?>
