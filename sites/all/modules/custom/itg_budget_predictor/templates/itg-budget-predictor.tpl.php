97d9d14cc521cca8c18aea5a4762dbc7d9c25761
Arun Mishra <arunmishra.coder@gmail.com>
2018-01-23 18:50:32 +0530

    add class in template

--- sites/all/modules/custom/itg_budget_predictor/templates/itg-budget-predictor.tpl.php
+++ sites/all/modules/custom/itg_budget_predictor/templates/itg-budget-predictor.tpl.php
@@ -91,10 +91,7 @@ $uri = base64_encode($actual_link);
                 ?>
                 <?php if(empty($user_id)) { ?>
                       <div class="cheaper-deaper-action">
-						  <a href="<?php print PARENT_SSO; ?>/saml_login/other/<?php print $uri; ?>" class="user-icon sso-click"><?php print t('Submit'); ?></a>
-						  
-						  <!-- <button class="btn-submit" type="button" onclick="Go (550, 500, 50, 'indiatoday', '', '<?php //print PARENT_SSO; ?>', '<?php //print $sso_arg_url; ?>')"><?php //print t('Submit'); ?></button> -->
-						  
+						  <a href="<?php print PARENT_SSO; ?>/saml_login/other/<?php print $uri; ?>" class="user-icon sso-click btn-submit"><?php print t('Submit'); ?></a>
 						  </div>
                 <?php } else { ?>
                       <div class="cheaper-deaper-action"><button class="btn-submit" type="button" onclick="captureCurrentDiv(<?php print $section_id; ?>)"><?php print t('Submit'); ?></button></div>
