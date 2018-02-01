9d5164ba1b0f0db19eebfaa1d1f58636f00b5988
yatendra-itgd <yatendra.singh@intoday.com>
2018-02-01 10:28:08 +0530

    Scorecard changes for PRE UAT

--- sites/all/modules/custom/itg_widget/templates/score-card-iframe.tpl.php
+++ sites/all/modules/custom/itg_widget/templates/score-card-iframe.tpl.php
@@ -12,7 +12,7 @@ if(isset($data->score_story)){
             </div>
             <div class="col-lg-6 col-ms-6 mhide scorecard-stripcontent">
                 <ul>
-                    <li><a href="<?php print $base_url . '/' . drupal_get_path_alias("node/" . $data->score_story); ?>" title="">Live Cricket Score, 3rd Test Day 2: India aim to bounce back after dismal batting display</a></li>
+                    <li><a href="<?php print $base_url . '/' . drupal_get_path_alias("node/" . $data->score_story); ?>" title=""><?php print $node_title; ?></a></li>
                     <li><a href="<?php print $base_url . '/' . drupal_get_path_alias("node/" . $data->score_photo); ?>" title=""><i class="fa fa-camera"></i> Photo</a></li>
                     <li><a href="<?php print $base_url . '/' . drupal_get_path_alias("node/" . $data->score_video); ?>" title=""><i class="fa fa-play-circle"></i> Video</a></li>
                 </ul>
