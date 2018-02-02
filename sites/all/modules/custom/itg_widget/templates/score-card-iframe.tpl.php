977fc8daadb6a748940a9ce67b461bf9553daf82
yatendra-itgd <yatendra.singh@intoday.com>
2018-02-02 11:41:31 +0530

    Score card changes

--- sites/all/modules/custom/itg_widget/templates/score-card-iframe.tpl.php
+++ sites/all/modules/custom/itg_widget/templates/score-card-iframe.tpl.php
@@ -12,9 +12,15 @@ if(isset($data->score_story)){
             </div>
             <div class="col-lg-6 col-ms-6 mhide scorecard-stripcontent">
                 <ul>
+				  <?php if(isset($data->score_story)) : ?>
                     <li><a href="<?php print $base_url . '/' . drupal_get_path_alias("node/" . $data->score_story); ?>" title=""><?php print $node_title; ?></a></li>
+                  <?php endif; ?>
+                  <?php if(isset($data->score_photo)) : ?>
                     <li><a href="<?php print $base_url . '/' . drupal_get_path_alias("node/" . $data->score_photo); ?>" title=""><i class="fa fa-camera"></i> Photo</a></li>
+                  <?php endif; ?>
+                  <?php if(isset($data->score_video)) : ?>
                     <li><a href="<?php print $base_url . '/' . drupal_get_path_alias("node/" . $data->score_video); ?>" title=""><i class="fa fa-play-circle"></i> Video</a></li>
+                  <?php endif; ?>
                 </ul>
             </div>
         </div>
