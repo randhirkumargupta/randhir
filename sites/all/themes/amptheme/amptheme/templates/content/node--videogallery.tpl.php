162e57d3cb0f16fd9319bc097e3a4c4b7c7d61ef
yatendra-itgd <yatendra.singh@intoday.com>
2018-02-12 20:15:29 +0530

    PRE UAT Changes for AMP and Duplicate video title

--- sites/all/themes/amptheme/amptheme/templates/content/node--videogallery.tpl.php
+++ sites/all/themes/amptheme/amptheme/templates/content/node--videogallery.tpl.php
@@ -22,11 +22,11 @@
       else {
         $description = $node->field_video_kicker[LANGUAGE_NONE][0]['value'];
       }
-      if(function_exists('itg_custom_amp_body_filter')) {
-	      $description = itg_custom_amp_body_filter($description);
-	    }
-                        
-      foreach ($videoids as $keys => $video_value) {
+      if (function_exists('itg_custom_amp_body_filter')) {
+        $description = itg_custom_amp_body_filter($description);
+      }
+
+    foreach ($videoids as $keys => $video_value) {
         if (function_exists('get_amp_video_time')) {
         $video_time = get_amp_video_time($node->nid, 'videogallery', 'field_video_duration');
         } 
@@ -96,10 +96,10 @@
                   } else {
                 $description = $video_kicker[0]->field_video_kicker_value;
               }
-              if(function_exists('itg_custom_amp_body_filter')) {
+              if (function_exists('itg_custom_amp_body_filter')) {
                 $description = itg_custom_amp_body_filter($description);
               }
-              ?>
+  ?>
         <amp-carousel width="300"
                   height="280"
                   layout="responsive"
