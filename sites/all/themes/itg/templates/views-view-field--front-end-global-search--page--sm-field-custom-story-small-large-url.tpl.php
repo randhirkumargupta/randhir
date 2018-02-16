4a6f09d1414b8747d8e4730090bd02a1d56564bc
Arun Mishra <arunmishra.coder@gmail.com>
2018-02-16 20:36:09 +0530

    image html style update from search page

--- sites/all/themes/itg/templates/views-view-field--front-end-global-search--page--sm-field-custom-story-small-large-url.tpl.php
+++ sites/all/themes/itg/templates/views-view-field--front-end-global-search--page--sm-field-custom-story-small-large-url.tpl.php
@@ -30,7 +30,7 @@ if(function_exists('itg_get_related_story_content')) {
 }
 if(!empty($related_data->sm_field_magazine_small_url[0]) && $related_data->bundle == 'magazine' && getimagesize($related_data->sm_field_magazine_small_url[0]) !== false) {
 ?>
-<a href="<?php print $front_url; ?>" title="<?php print $related_data->label; ?>" target="_blank"><img alt="<?php print $related_data->label; ?>" style="width: 170px; height: 127px" src="<?php print $related_data->sm_field_magazine_small_url[0]; ?>" title="<?php print $related_data->label; ?>"/></a>
+<a href="<?php print $front_url; ?>" title="<?php print $related_data->label; ?>" target="_blank"><img alt="<?php print $related_data->label; ?>" style="width: 170px; height: 96px" src="<?php print $related_data->sm_field_magazine_small_url[0]; ?>" title="<?php print $related_data->label; ?>"/></a>
 <?php
 } else if(!empty($related_data->sm_field_custom_story_small_large_url[0]) && getimagesize($related_data->sm_field_custom_story_small_large_url[0]) !== false) {
  ?> 
