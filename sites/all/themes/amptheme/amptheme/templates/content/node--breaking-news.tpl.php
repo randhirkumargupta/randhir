2bd650c56adcd8006f8db98c051bc792dcf8c733
yatendra-itgd <yatendra.singh@intoday.com>
2018-02-16 12:06:13 +0530

    Live TV AMP Issue

--- sites/all/themes/amptheme/amptheme/templates/content/node--breaking-news.tpl.php
+++ sites/all/themes/amptheme/amptheme/templates/content/node--breaking-news.tpl.php
@@ -169,7 +169,7 @@ if ($node->field_type['und']['0']['value'] == 'Live Blog') {
                     else {
 						$live_url = str_replace('<iframe', '<amp-iframe', $live_url);
 					    $live_url = str_replace('</iframe>', '</amp-iframe>', $live_url);
-					    $live_url = str_replace('style="z-index:4"', '', $live_url);
+					    $live_url = str_replace('style="z-index:4"', 'sandbox="allow-scripts allow-same-origin"', $live_url);
                         print $live_url;
                     }
                   }
