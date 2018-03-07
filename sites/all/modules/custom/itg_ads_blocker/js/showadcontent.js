5a6236e2cfff9db0fea62a4e27282d7b775b1720
Arun Mishra <arunmishra.coder@gmail.com>
2018-03-07 12:07:48 +0530

    adsblocker issue fix for video

--- sites/all/modules/custom/itg_ads_blocker/js/showadcontent.js
+++ sites/all/modules/custom/itg_ads_blocker/js/showadcontent.js
@@ -35,6 +35,8 @@ function openPopup() {
         {
            jQuery(".ad-blocker").html(result);
            if(jQuery('body').hasClass('node-type-videogallery')){
+               jwplayer('videoplayer').remove();
+               jwplayer('videoplayer_0').remove();
                jQuery('.node-type-videogallery').find('#content').prepend('<div class="ad-blocker">' + result + '</div>');
            }
            if(jQuery('body').hasClass('node-type-photogallery')){
