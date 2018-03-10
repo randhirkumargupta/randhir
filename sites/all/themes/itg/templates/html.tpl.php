b15bcb24910de9f987a0cca33daa44cca27a900d
Shashank Singh <shashank.singh@intoday.com>
2018-03-10 16:03:45 +0530

    update code of html

--- sites/all/themes/itg/templates/html.tpl.php
+++ sites/all/themes/itg/templates/html.tpl.php
@@ -131,18 +131,36 @@
       }
     ?>
     <script type="text/javascript">
-    (function(b,r,a,n,c,h,,s,d,k){if(!b[n]||!b[n]._q){for(;s<.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src="https://cdn.branch.io/branch-latest.min.js";k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments])}},{_q:[],_v:1},"addListener applyCode banner closeBanner creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setBranchViewData setIdentity track validateCode".split(" "), 0);
-    branch.init('key_live_djuJxtD2ZARYUWRnZZp9WnegBtjqJrld',
-    {'no_journeys':false}
-
+    (function(b, r, a, n, c, h, , s, d, k) {
+    if (!b[n] || !b[n]._q) {
+        for (; s < .length;) c(h, _[s++]);
+        d = r.createElement(a);
+        d.async = 1;
+        d.src = "https://cdn.branch.io/branch-latest.min.js";
+        k = r.getElementsByTagName(a)[0];
+        k.parentNode.insertBefore(d, k);
+        b[n] = h
+    }
+    })(window, document, "script", "branch", function(b, r) {
+    b[r] = function() {
+        b._q.push([r, arguments])
+    }
+    }, {
+    _q: [],
+    _v: 1
+    }, "addListener applyCode banner closeBanner creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setBranchViewData setIdentity track validateCode".split(" "), 0);
+    branch.init('key_live_djuJxtD2ZARYUWRnZZp9WnegBtjqJrld', {
+      'no_journeys': false
+    } 
     );
     var linkData = {
-      data: {
-      '$canonical_identifier':"<?php print $content_id; ?>",
-      '$data_type': "<?php print $content_type; ?>",
-    }};
-    branch.setBranchViewData(linkData);
-    );
+    data: {
+        '$canonical_identifier': "<?php print $content_id; ?>",
+        '$data_type': "<?php print $content_type; ?>",
+    }
+    };
+branch.setBranchViewData(linkData);
+);
 	</script>
   <!-- Branch IO code end -->
 </head>
