d88ef2b486fa101b0842b469cf5b15cb60be6e9e
Arun Mishra <arunmishra.coder@gmail.com>
2018-03-10 20:18:36 +0530

    message print for ipad

--- sites/all/modules/custom/itg_flag/js/itg_flag.js
+++ sites/all/modules/custom/itg_flag/js/itg_flag.js
@@ -152,6 +152,7 @@ jQuery(document).ready(function () {
                 if (obj.error == 'error') {
 
                     jQuery("#voted_" + obj.nd_id).html('You have already voted').show(0).delay(2000).hide(1000);
+                    jQuery("#voted_mobile_" + obj.nd_id).html('You have already voted').show(0).delay(2000).hide(1000);
                 }
                 jQuery('#like_count,#dislike_count').prop('disabled', false);
                 jQuery('.btn-heart-like').addClass( "heart-dislike" );
