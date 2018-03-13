8b63fc16ea2989e1c1b7a51b33ed6c0ef8066f8d
Sandeep <sandeep.bharti@aajtak.com>
2018-03-13 21:50:38 +0530

    HIGHLIGHTS section AMP

--- sites/all/themes/amptheme/amptheme/templates/layout/html.tpl.php
+++ sites/all/themes/amptheme/amptheme/templates/layout/html.tpl.php
@@ -49,10 +49,8 @@
         <?php print $head; ?>
         <?php include $amptheme_path_file . '/templates/amp-css/amp-boilerplate-styles-min.inc' ?>
         <?php //include $ampsubtheme_path_file . '/css/amp-custom-styles.css' ?>
-
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Merriweather:400,700" rel="stylesheet">
-
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <script async src="https://cdn.ampproject.org/v0.js"></script>
         <script async custom-element="amp-carousel" src="https://cdn.ampproject.org/v0/amp-carousel-0.1.js"></script>
@@ -323,6 +321,11 @@ img {
     font-weight: 600;
     text-transform: capitalize
 }
+
+.story-section .briefcase { margin:15px 0;}
+.story-section .briefcase h4{font-size: 18px;font-size: 1.125rem;text-transform: uppercase; margin-bottom: 10px; color: #bb0a0a;}
+.story-section .briefcase ul li {position: relative; font-size: 15px; margin-bottom: 10px; padding-left: 15px;line-height: 22px;}
+.story-section .briefcase ul li:before { position: absolute; top: 6px; left: 0px; content: ''; border-radius: 100%; background: #a00606; height: 5px; width: 5px;}
 .scroll-x {
     overflow-x: auto
 }
