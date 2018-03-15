30eab25ee7d7a20beb4a8abdfb1be0318f79dcfb
yatendra-itgd <yatendra.singh@intoday.com>
2018-03-15 14:08:26 +0530

    PTI Changes

--- sites/all/themes/itg/templates/views-view-custom--category-wise-content-list--section-wise-content-listing.tpl.php
+++ sites/all/themes/itg/templates/views-view-custom--category-wise-content-list--section-wise-content-listing.tpl.php
@@ -75,7 +75,7 @@ global $base_url;
     <?php endif; ?>
     <div class="detail"><h3 class="<?php echo $pipelineclass; ?>" title="<?php echo strip_tags($row['title']);?>"><?php
       if (function_exists('itg_common_get_smiley_title')) {
-        print l(stripInvalidXml(itg_common_get_smiley_title($row['nid'], 0, 100)), "node/" . $row['nid'], array("html" => TRUE));
+        print l(itg_common_get_smiley_title($row['nid'], 0, 100), "node/" . $row['nid'], array("html" => TRUE));
         echo $pipelinetext;
         
       }
