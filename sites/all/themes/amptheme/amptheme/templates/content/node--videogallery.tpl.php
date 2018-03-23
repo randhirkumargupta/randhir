ee3ce3bb37c627972eca68c50a5d2812655fc043
yatendra-itgd <yatendra.singh@intoday.com>
2018-03-23 17:11:00 +0530

    AMP Validation Fixes

--- sites/all/themes/amptheme/amptheme/templates/content/node--videogallery.tpl.php
+++ sites/all/themes/amptheme/amptheme/templates/content/node--videogallery.tpl.php
@@ -39,6 +39,7 @@ if(!empty($node->field_story_reporter)){
       if (function_exists('itg_custom_amp_body_filter')) {
         $description = itg_custom_amp_body_filter($description);
       }
+      $description = preg_replace('#<script(.*?)>(.*?)</script>#is', '', $description);
 
     foreach ($videoids as $keys => $video_value) {
         if (function_exists('get_amp_video_time')) {
@@ -113,6 +114,7 @@ if(!empty($node->field_story_reporter)){
               if (function_exists('itg_custom_amp_body_filter')) {
                 $description = itg_custom_amp_body_filter($description);
               }
+		      $description = preg_replace('#<script(.*?)>(.*?)</script>#is', '', $description);
   ?>
         <amp-carousel width="300"
                   height="280"
