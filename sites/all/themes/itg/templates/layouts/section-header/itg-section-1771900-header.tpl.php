53a583012d4203641d98d04743b023fa127cbea2
Sandeep <sandeep.bharti@aajtak.com>
2018-03-28 17:40:44 +0530

    panchayat css upadte and header socila icon link

--- sites/all/themes/itg/templates/layouts/section-header/itg-section-1771900-header.tpl.php
+++ sites/all/themes/itg/templates/layouts/section-header/itg-section-1771900-header.tpl.php
@@ -8,7 +8,7 @@ drupal_add_css(drupal_get_path('theme', 'itg') . '/css/section-header.css');
  <div class="container">
     <div id="conclave-header">
         <div class="topBanner">
-            <div class="conclave-logo"><a href="#" title="Karnataka Panchayat 2018"><img src="https://akm-img-a-in.tosshub.com/indiatoday/section-header/images/karnataka-panchayat-logo.png" alt="Karnataka Panchayat 2018"></a></div>
+            <div class="conclave-logo"><a href="/karnataka-panchayat-2018/2018/" title="Karnataka Panchayat 2018"><img src="https://akm-img-a-in.tosshub.com/indiatoday/section-header/images/karnataka-panchayat-logo.png" alt="Karnataka Panchayat 2018"></a></div>
             <div class="conclave-event-details">
                 <div class="center-content">
                     <h3>THE ELECTION SHOWDOWN</h3>                    
@@ -27,7 +27,7 @@ drupal_add_css(drupal_get_path('theme', 'itg') . '/css/section-header.css');
 		<div id="block-menu-menu-event-menu" class="container event-header-menu-container">
 			  <div class="row ">
 				  <div class="col-md-12 col-sm-12" style="color:#fff">
-					  <div class="wrap-mobile-social"> <a class="mobile-nav nochange" href="javascript:void(0)"><i class="fa fa-bars"></i></a><div class="social-share"> <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a> <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a><div class="search-icon-parent"> <a href="javascript:void(0)" class="search-icon-default" title=""><i class="fa fa-search"></i></a> <a href="javascript:void(0)" class="search-icon-search" title="" style="display: none;"><i class="fa fa-search"></i></a><div class="globle-search"> <input class="search-text" placeholder="Type here" type="text" value=""></div></div></div></div>
+					  <div class="wrap-mobile-social"> <a class="mobile-nav nochange" href="javascript:void(0)"><i class="fa fa-bars"></i></a><div class="social-share"> <a href="https://www.facebook.com/IndiaToday/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a> <a href="https://twitter.com/indiatoday" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a><div class="search-icon-parent"> <a href="javascript:void(0)" class="search-icon-default" title=""><i class="fa fa-search"></i></a> <a href="javascript:void(0)" class="search-icon-search" title="" style="display: none;"><i class="fa fa-search"></i></a><div class="globle-search"> <input class="search-text" placeholder="Type here" type="text" value=""></div></div></div></div>
 					  <ul class="menu">
 						  <?php foreach ($data['data']['menu_manager']['menu'] as $menu): ?>
 							<?php print $menu ?>
