c01a65df8fd8824edb2a2ad4850b84c122609bb3
yatendra-itgd <yatendra.singh@intoday.com>
2018-04-03 17:29:34 +0530

    Remove Byline From Video Page

--- sites/all/themes/amptheme/amptheme/templates/content/node--videogallery.tpl.php
+++ sites/all/themes/amptheme/amptheme/templates/content/node--videogallery.tpl.php
@@ -1,10 +1,10 @@
 <?php
-$byline_title = '';
+/*$byline_title = '';
 if(!empty($node->field_story_reporter)){
 	$target_nid = $node->field_story_reporter[LANGUAGE_NONE][0]['target_id'];	
 	$byline_title = itg_common_get_node_title($target_nid);
 	$byline_title = trim($byline_title);
-}
+}*/
 ?>
 <div class="black-box">
   <div class="photo-title"><h1><?php print $node->title; ?></h1></div>
@@ -80,7 +80,7 @@ if(!empty($node->field_story_reporter)){
             <?php
             }
             ?>
-                    <div class="video-caption"><span><?php date('F j, Y', strtotime($node->field_itg_content_publish_date[LANGUAGE_NONE][0]['value'])); ?></span><p><?php print $video_value->field_video_title_value;?></p></div>
+                    <div class="video-caption"><span><?php print date('F j, Y', strtotime($node->field_itg_content_publish_date[LANGUAGE_NONE][0]['value'])); ?></span><p><?php print $video_value->field_video_title_value;?></p></div>
                     <div class="video_dec_amp video_dec_<?php print $video_value->video_id; ?>" ><?php print ucfirst($description); ?></div>
                     
                 </div>
@@ -137,7 +137,7 @@ if(!empty($node->field_story_reporter)){
                        controls>
                        <source type="video/webm" src="<?php print $video_id;?>">
                       </amp-video>
-                    <div class="video-caption"><span><?php date('F j, Y', strtotime($node->field_itg_content_publish_date[LANGUAGE_NONE][0]['value'])); ?></span><p><?php print $video_value->field_video_title_value;?></p></div>
+                    <div class="video-caption"><span><?php print date('F j, Y', strtotime($node->field_itg_content_publish_date[LANGUAGE_NONE][0]['value'])); ?></span><p><?php print $video_value->field_video_title_value;?></p></div>
                     <div class="video_dec_amp video_dec_<?php print $video_value->video_id; ?>" ><?php print ucfirst($description); ?></div>
                     
                 </div>
