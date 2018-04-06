5b920b6be2d67044ae274f5f29e78578636736fb
Vishal Kamal <vishal.kamal@intoday.com>
2018-04-06 19:04:17 +0530

    Not found new page

--- sites/all/modules/custom/itg_seo/templates/itg-seo-not-found-200.tpl.php
+++ sites/all/modules/custom/itg_seo/templates/itg-seo-not-found-200.tpl.php
@@ -8,9 +8,10 @@ global $base_url;
 ?>
 <div class="page-error">
   <div class="row">
-    <div class="col-md-12">
+		<div class="col-md-4">&nbsp;</div>
+    <div class="col-md-4">
       <p class="go-back">
-          <?php print t('The Content has expired and can no longer be used.'); ?>
+          <?php print t('The Content has expired.'); ?></br> <?php print t('Open the'); ?> <a href="<?php print url(''); ?>"><?php print t('India Today'); ?></a> <?php print t('homepage for me'); ?>
       </p>
       <span class="error-or">OR</span>
       <h3><?php print t('Check out Latest Headlines'); ?></h3>
@@ -21,5 +22,6 @@ global $base_url;
           
       </ul>
     </div>
+    <div class="col-md-4">&nbsp;</div>
   </div>
 </div>
