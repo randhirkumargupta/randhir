752f6bfeaf3628e790916621ec22ee5c32b8c34c
Sandeep <sandeep.bharti@aajtak.com>
2018-04-17 13:18:04 +0530

    drag js

--- sites/all/themes/itg/templates/layouts/home/page--front.tpl.php
+++ sites/all/themes/itg/templates/layouts/home/page--front.tpl.php
@@ -1029,6 +1029,8 @@ if ($theme != 'itgadmin') {
     </div>
 </div>
 <script type="text/javascript" src="https://code.jquery.com/ui/1.8.18/jquery-ui.min.js"></script>
+<script type="text/javascript">!function(a){function f(a,b){if(!(a.originalEvent.touches.length>1)){a.preventDefault();var c=a.originalEvent.changedTouches[0],d=document.createEvent("MouseEvents");d.initMouseEvent(b,!0,!0,window,1,c.screenX,c.screenY,c.clientX,c.clientY,!1,!1,!1,!1,0,null),a.target.dispatchEvent(d)}}if(a.support.touch="ontouchend"in document,a.support.touch){var e,b=a.ui.mouse.prototype,c=b._mouseInit,d=b._mouseDestroy;b._touchStart=function(a){var b=this;!e&&b._mouseCapture(a.originalEvent.changedTouches[0])&&(e=!0,b._touchMoved=!1,f(a,"mouseover"),f(a,"mousemove"),f(a,"mousedown"))},b._touchMove=function(a){e&&(this._touchMoved=!0,f(a,"mousemove"))},b._touchEnd=function(a){e&&(f(a,"mouseup"),f(a,"mouseout"),this._touchMoved||f(a,"click"),e=!1)},b._mouseInit=function(){var b=this;b.element.bind({touchstart:a.proxy(b,"_touchStart"),touchmove:a.proxy(b,"_touchMove"),touchend:a.proxy(b,"_touchEnd")}),c.call(b)},b._mouseDestroy=function(){var b=this;b.element.unbind({touchstart:a.proxy(b,"_touchStart"),touchmove:a.proxy(b,"_touchMove"),touchend:a.proxy(b,"_touchEnd")}),d.call(b)}}}(jQuery);
+</script>
 <style type="text/css"> 
     #twister{ position: fixed;right: 0px;bottom: -8px;z-index: 99999; cursor:move; width:190px; height: 180px;}
     span.crosscloseif{z-index: 99; background: #f1f1f1; color: #000;font-size: 20px;font-weight: bold;padding: 5px 10px;position: absolute;top: 18px;
