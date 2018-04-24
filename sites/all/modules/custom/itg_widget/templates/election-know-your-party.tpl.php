5fe3cecef71fba66b88627769f6d8d1288d3f43c
Vishal Kamal <vishal.kamal@intoday.com>
2018-04-24 16:22:54 +0530

    Election template

--- sites/all/modules/custom/itg_widget/templates/election-know-your-party.tpl.php
+++ sites/all/modules/custom/itg_widget/templates/election-know-your-party.tpl.php
@@ -3,7 +3,11 @@
   <div class="partylist-body">
   <ul>
     <?php foreach($data as $value){?>
-    <li><span class="party-logo"><img src="<?php print file_create_url($value->uri);?>" alt="<?php print $value->field_cm_display_title_value;?>"></span><span class="party-details"><h6><?php print $value->field_cm_display_title_value;?></h6><p><?php print $value->field_story_kicker_text_value;?></p></span></li>
+      <?php if(!empty($value->field_story_external_url_value)){ ?>
+        <li><a href="<?php print $value->field_story_external_url_value;?>"><span class="party-logo"><img src="<?php print file_create_url($value->uri);?>" alt="<?php print $value->field_cm_display_title_value;?>"></span><span class="party-details"><h6><?php print $value->field_cm_display_title_value;?></h6><p><?php print $value->field_story_kicker_text_value;?></p></span></a></li>
+        <?php } else{?>
+        <li><span class="party-logo"><img src="<?php print file_create_url($value->uri);?>" alt="<?php print $value->field_cm_display_title_value;?>"></span><span class="party-details"><h6><?php print $value->field_cm_display_title_value;?></h6><p><?php print $value->field_story_kicker_text_value;?></p></span></li>
+      <?php }?>
     <?php }?>
   </ul>
 </div> 
