3def6fd0196c4b3555910f464c636b3fbe8597c7
Arun Mishra <arunmishra.coder@gmail.com>
2018-05-02 15:23:49 +0530

    #comment: issue fix #id 557

--- sites/all/themes/itg/templates/node--breaking-news-custom.tpl.php
+++ sites/all/themes/itg/templates/node--breaking-news-custom.tpl.php
@@ -196,6 +196,7 @@ $modify_date = date('Y-m-d H:i:s', $node->changed);
         $breaking_output .= '<div class="breaking-section"><a name="'.$breaking_item->bid.'"></a>';
         $blog_title = $breaking_item->blog_title;
         $html = !empty($blog_title) ? $blog_title : preg_replace('#<script(.*?)>(.*?)</script>#is', '', $breaking_item->blog_description);
+        $blog_desc = preg_replace('#<script(.*?)>(.*?)</script>#is', '', $breaking_item->blog_description);
         $fb_title = $string = preg_replace('/\s+/', ' ', itg_common_only_text_string($html));
         $pub_time = date("H:i", strtotime($breaking_item->blog_publish_time));
         $pub_display_time = date("H:i A", strtotime($breaking_item->blog_publish_time));
@@ -204,7 +205,7 @@ $modify_date = date('Y-m-d H:i:s', $node->changed);
         
           $breaking_output .= '<div class="dwrap" timevalue="' . $pub_time2 . '" tcount="' . count($custom_content) . '"><div class="dateauthor"><div class="breaking-date">' . $pub_display_time . ' IST</div><div class="breaking-author"> Posted by ' . $user_name . '</div></div>';
           $breaking_output .= '<div class="blog-multi-title">'. $blog_title .'</div>';
-          $breaking_output .= '<div class="blog-multi-desc">'. $html .'</div>';
+          $breaking_output .= '<div class="blog-multi-desc">'. $blog_desc .'</div>';
           $breaking_output .= '<div class="breaking-social-share">' . $redirection_url . '</div><div class="social-share-new"><ul><li><a title="share on facebook" onclick=\'fbpop("' . $share_page_link . '" , "' . urlencode($fb_title) . '" , "' . urlencode($share_desc) . '" , "' . $share_image . '")\' class="facebook def-cur-pointer"><i class="fa fa-facebook"></i></a></li><li><a title="share on twitter" rel="' . $node->nid . '" data-tag="' . $node->type . '" data-activity="twitter_share" data-status="1" onclick=\'twitter_popup("' . urlencode($fb_title) . '" , "' . urlencode($short_url) . '")\' class="user-activity twitter def-cur-pointer"><i class="fa fa-twitter"></i></a></li><li><a title="share on google+" rel="' . $node->nid . '" data-tag="' . $node->type . '" data-activity="google_share" data-status="1" onclick=\'return googleplusbtn("' . $share_page_link . '" )\' class="user-activity google def-cur-pointer"><i class="fa fa-google-plus"></i></a></li></ul></div>';
           $breaking_output .= '</div></div>';
           if($i == 3) {
