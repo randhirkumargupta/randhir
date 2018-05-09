9ee121a44f752a48c3df00d1381c5ef2e7c01279
Sandeep <sandeep.bharti@aajtak.com>
2018-05-10 01:47:42 +0530

    election update

--- sites/all/themes/itg/templates/page--electionconstituency.tpl.php
+++ sites/all/themes/itg/templates/page--electionconstituency.tpl.php
@@ -18,9 +18,9 @@ drupal_add_js(drupal_get_path('module', 'itg_widget') . '/js/itg_election_refres
           </div>      
       </div>    
     <?php endif; ?>
-    <div class="row">
+    <div class="row electiontop-header mb-20">
         <div class="col-md-8 col-sm-8 col-sx-8 custom-page-title"> <h1><?php print drupal_get_title();?></h1> </div>
-        <div class="col-md-4 col-sm-4 col-sx-4 election-back-button"><span class="back-button"><a href="<?php print FRONT_URL . '/' . drupal_get_path_alias('taxonomy/term/' . $_GET['section']);?>">Back</a></span> </div>
+        <div class="col-md-4 col-sm-4 col-sx-4 election-back-button"><span class="back-button"><a href="<?php print FRONT_URL . '/' . drupal_get_path_alias('taxonomy/term/' . $_GET['section']);?>"><i class="fa fa-long-arrow-left" aria-hidden="true"></i> Back</a></span> </div>
     </div>
     <div class="row">
         <div class="col-md-8 col-sm-12 col-sx-12 election-graph left-side">             
