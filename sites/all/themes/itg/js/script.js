d49ac336fae82263fb8eb8540bb7201b6d4725b1
Arun Mishra <arunmishra.coder@gmail.com>
2018-05-11 18:42:14 +0530

    ga code issue fix

--- sites/all/themes/itg/js/script.js
+++ sites/all/themes/itg/js/script.js
@@ -1145,7 +1145,7 @@ function load_migrate_video_in_slider(getvideoimage, ajaxpath, getvideonid, getv
 
 // Adding js for google analytocs for home page photo carousel
 jQuery(window).load(function () {
-    if (window.location.pathname == '/') {
+    if (window.location.pathname == '/' || window.location.pathname == '/elections/karnataka-election-2018') {
         // adding onclick attribute for ga code for photo carousel for next button
         jQuery('div.flexslider li.flex-nav-next a').attr('onclick', "ga('send', 'event', 'homephotocarouselNext', 'click','1', 1, {'nonInteraction': 1});return true;");
         jQuery('div.flexslider li.flex-nav-next a').attr("href", "#homephotocarouselnext");
