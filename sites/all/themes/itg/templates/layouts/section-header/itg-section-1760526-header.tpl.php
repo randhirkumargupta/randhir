463801cd22cf0b653c51ab32ca661db82935e19d
Sandeep <sandeep.bharti@aajtak.com>
2018-05-18 01:16:15 +0530

    bhopal youthsummit 2017

--- sites/all/themes/itg/templates/layouts/section-header/itg-section-1760526-header.tpl.php
+++ sites/all/themes/itg/templates/layouts/section-header/itg-section-1760526-header.tpl.php
@@ -54,7 +54,7 @@ drupal_add_css(drupal_get_path('theme', 'itg') . '/css/section-header.css');
     
   </div>
 </div>
-    <div class="itg-logo-container">
+    <div class="itg-logo-container clearfix">
 	<?php
 	if ($data['is_event']) {
 	  ?>
