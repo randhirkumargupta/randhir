998e806794e1d9de77736a0ab5eb1ec9a952557b
Arun Mishra <arunmishra.coder@gmail.com>
2018-05-21 16:12:34 +0530

    fifa top story

--- sites/all/modules/custom/itg_widget/templates/fifa-top-stories-manual-widget-data.tpl.php
+++ sites/all/modules/custom/itg_widget/templates/fifa-top-stories-manual-widget-data.tpl.php
@@ -7,7 +7,7 @@
 <div class="row">
     <div class="col-md-8">
         <div class="auto-block-1">
-            <div class="featured-news">
+            <div class="fifa-top-story">
                 <?php
                 global $base_url;
                 foreach ($data as $index => $row) {
@@ -19,9 +19,8 @@
                   if (strtolower($row['type']) == 'videogallery') {
                     $video_class = 'video-icon';
                   }
-                  if ($index == 0) {
                     ?>
-                    <div class="featured-post featured-post-first <?php echo $video_class; ?>">
+                    <div class="fifa-post fifa-post-first <?php echo $video_class; ?>">
                         <?php if ($row['uri'] != "") { ?>
                           <a  href="<?php echo $base_url . '/' . drupal_get_path_alias("node/" . $row['nid']); ?>">
                               <img src="<?php print image_style_url("magazine_top_story_483x271", $row['uri']); ?>" alt="<?php echo $row['field_story_extra_large_image_alt']; ?>" title="<?php echo $row['field_story_extra_large_image_title']; ?>" />
@@ -43,52 +42,9 @@
                             ?>
                         </h2>           
                     </div>
-                  <?php } ?>
             <?php } ?>
             </div>
         </div>    
     </div>
-    <!-- -->
-    <div class="col-md-4">
-        <div class="auto-block-2">
-            <div class="movies-featured-news">
-                <?php
-                foreach ($data as $index => $row) {
-                  $video_class = "";
-                  if (strtolower($row['type']) == 'videogallery') {
-                    $video_class = 'video-icon';
-                  }
-                  if ($index > 0 && $index <= 2) {
-                    ?>				
-                    <div class="movies-featured-post <?php echo $video_class; ?>">
-                        <?php if ($row['mi_file_uri'] != "") { ?>
-                          <a  href="<?php echo $base_url . '/' . drupal_get_path_alias("node/" . $row['nid']); ?>">
-                              <img src="<?php print image_style_url("home_page_feature_small", $row['mi_file_uri']); ?>" alt="<?php echo $row['field_story_medium_image_alt']; ?>" title="<?php echo $row['field_story_medium_image_title']; ?>" />
-                          </a>
-                        <?php
-                        }
-                        else {
-                          print "<img  src='" . $base_url . "/" . drupal_get_path('theme', 'itg') . "/images/itg_image237x133.jpg' />";
-                        }
-                        ?>
-                        <h3 <?php echo $data_tb_region_item;?> title="<?php echo strip_tags($desc); ?>">
-                            <?php
-                            if (function_exists('itg_common_get_smiley_title')) {
-                              echo l(itg_common_get_smiley_title($row['node_obj'], 0, 80), "node/" . $row['nid'], array("html" => TRUE));
-                            }
-                            else {
-                              echo l(mb_strimwidth(strip_tags($desc), 0, 85, ".."), "node/" . $row['nid']);
-                            }
-                            ?>
-                        </h3>
-                    </div>
-                  <?php
-                  }
-                }
-                ?>
-            </div>
-        </div>
-    </div>
-    <!-- -->
 </div>
 
