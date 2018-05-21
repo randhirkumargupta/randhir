29629264401572986a9043bb67d78985fdccbc99
Sandeep <sandeep.bharti@aajtak.com>
2018-05-21 22:17:50 +0530

    section header tpl

--- sites/all/themes/itg/templates/layouts/section-header/itg-section-1208866-header.tpl.php
+++ sites/all/themes/itg/templates/layouts/section-header/itg-section-1208866-header.tpl.php
@@ -61,7 +61,11 @@ drupal_add_css(drupal_get_path('theme', 'itg') . '/css/section-header.css');
 		<div id="block-menu-menu-event-menu" class="container event-header-menu-container">
 			  <div class="row ">
 				  <div class="col-md-12 col-sm-12" style="color:#fff">
-					  <div class="wrap-mobile-social"> <a class="mobile-nav nochange" href="javascript:void(0)"><i class="fa fa-bars"></i></a><div class="social-share"> <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a> <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a><div class="search-icon-parent"> <a href="javascript:void(0)" class="search-icon-default" title=""><i class="fa fa-search"></i></a> <a href="javascript:void(0)" class="search-icon-search" title="" style="display: none;"><i class="fa fa-search"></i></a><div class="globle-search"> <input class="search-text" placeholder="Type here" type="text" value=""></div></div></div></div>
+					  <div class="wrap-mobile-social"> <a class="mobile-nav nochange" href="javascript:void(0)"><i class="fa fa-bars"></i></a><div class="social-share">
+              <a href="https://www.facebook.com/MindRocks" title="Mind Rocks 2017" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+             <a href="https://twitter.com/mindrocks/" title="Mind Rocks 2017" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+             <a href="https://plus.google.com/103095347742615965425/about" title="Mind Rocks 2017" target="_blank"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+              <div class="search-icon-parent"> <a href="javascript:void(0)" class="search-icon-default" title=""><i class="fa fa-search"></i></a> <a href="javascript:void(0)" class="search-icon-search" title="" style="display: none;"><i class="fa fa-search"></i></a><div class="globle-search"> <input class="search-text" placeholder="Type here" type="text" value=""></div></div></div></div>
 					  <ul class="menu">
 						  <?php foreach ($data['data']['menu_manager']['menu'] as $menu): ?>
 							<?php print $menu ?>
