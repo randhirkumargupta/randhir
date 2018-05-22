eeba127f4af23259918a4edb4a84965b67857e30
raviprakashjha <ravi.jha@aajtak.com>
2018-05-22 17:34:55 +0530

    css changes on fifa tpl

--- sites/all/modules/custom/itg_widget/templates/fifa-section-manual-widget-data.tpl.php
+++ sites/all/modules/custom/itg_widget/templates/fifa-section-manual-widget-data.tpl.php
@@ -5,11 +5,24 @@
   }
 ?>
 <style type="text/css">
-#page{background: url(http://smedia2.intoday.in/indiatoday/images/fifa-background-bg.jpg) no-repeat center top;}
-#itg-block-1{background-color: #f0f0f0;}
-.sport-home-layout-page .itg-layout-615 .auto-block-1{background: none; border:0; }
+.fifa-wcup{background: url(https://smedia2.intoday.in/indiatoday/images/fifa-background-bg.jpg) no-repeat center 277px;}
+.fifa-wcup #itg-block-1{background-color: #f0f0f0;}
+.fifa-wcup .sport-home-layout-page .itg-layout-615 .droppable{height: auto;}
+.fifa-wcup .sport-home-layout-page .itg-layout-615 .auto-block-1{background: none; border:0; }
+.fifa-wcup .featured-post{position: relative;}
+.fifa-wcup .featured-post h3{position: absolute; bottom: 17px; left: 17px;}
+ 
+.fifa-wcup .movies-featured-post {background-color: #313131;padding: 15px 5px;}
+.fifa-wcup .movies-featured-post img{width: 88px;height: 50px;float: left;margin-right: 8px;}
+.fifa-wcup .movies-featured-post h3{font-size: 13px;line-height: 16px;color: #fff;display: block;}
+.fifa-wcup .movies-featured-post h3 a{color: #fff;}
+.fifa-wcup .movies-featured-post:hover{background-color: #a00606;}
 
-
+.fifa-wcup .headingtext{display: inline-block; width: 100%;}
+.fifa-wcup .headingtext .round01{width: 12px; height: 12px; background-color: #a10506; display: inline-block;}
+.fifa-wcup .headingtext .round02{width: 10px; height: 10px; background-color: #a10506; display: inline-block; margin: 0 3px;}
+.fifa-wcup .headingtext .round03{width: 7px; height: 7px; background-color: #a10506; display: inline-block;}
+.fifa-wcup .headingtext h2{font-size: 15px; line-height: 20px; color: #000; text-transform: uppercase;}
 
 </style>
 <div class="row">
@@ -30,6 +43,9 @@
                   if ($index == 0) {
                     ?>
                     <div class="featured-post featured-post-first <?php echo $video_class; ?>">
+                      
+                      <h2>Football follows cricket: FIFA proposes mini - World cup every 2 years, just like world</h2>
+
                         <?php if ($row['uri'] != "") { ?>
                           <a  href="<?php echo $base_url . '/' . drupal_get_path_alias("node/" . $row['nid']); ?>">
                               <img src="<?php print image_style_url("magazine_top_story_483x271", $row['uri']); ?>" alt="<?php echo $row['field_story_extra_large_image_alt']; ?>" title="<?php echo $row['field_story_extra_large_image_title']; ?>" />
