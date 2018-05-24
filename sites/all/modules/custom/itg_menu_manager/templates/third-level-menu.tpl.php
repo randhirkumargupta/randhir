6c49fb4702cef94caff22ce23ab059f8337269da
Arun Mishra <arunmishra.coder@gmail.com>
2018-05-25 00:38:31 +0530

    debug

--- sites/all/modules/custom/itg_menu_manager/templates/third-level-menu.tpl.php
+++ sites/all/modules/custom/itg_menu_manager/templates/third-level-menu.tpl.php
@@ -48,12 +48,11 @@ if($_GET['test'] == 'abc') {
 if (isset($cat_id) && is_numeric($cat_id)) {
   $section_tids = array_reverse(taxonomy_get_parents_all($cat_id));
   $section_tid = $section_tids[0]->tid;
-  if($cat_id != $section_tid){
-    $cat_id = $section_tid;
-  }
   if($cat_id == '1771916'){
 	  $cat_id = '1771916';
-  }	   
+  }	elseif($cat_id != $section_tid){
+    $cat_id = $section_tid;
+  }   
 }
 $section_banner_data = taxonomy_term_load($cat_id);
 	
