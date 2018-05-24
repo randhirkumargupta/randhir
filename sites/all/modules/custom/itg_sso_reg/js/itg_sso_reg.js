91724ee18a302cd904f5f99b034aa826e363cf7b
Vishal <vishal.kamal@aajtak.com>
2018-05-25 00:37:10 +0530

    Country check

--- sites/all/modules/custom/itg_sso_reg/js/itg_sso_reg.js
+++ sites/all/modules/custom/itg_sso_reg/js/itg_sso_reg.js
@@ -27,6 +27,9 @@
                     if (index == 'password') {
                         jQuery('.pass-msg').html(item);
                     }
+                    if (index == 'forget_me') {
+                        jQuery('.forget_me-error').html(item);
+                    }
                 });
             }
             // ajax for otp
@@ -60,8 +63,65 @@
             $('input[name=mobile]').keyup(function () {
                 this.value = this.value.replace(/[^0-9\.]/g, '');
             });
+            
+            // validation
+            var selected_country = jQuery('select[name="page_country"]').val();
+            console.log(selected_country, 'asd');
+			var flag = 0;
+			if (Drupal.settings.countries_json[selected_country] != undefined && Drupal.settings.countries_json[selected_country] == 0)  {
+				jQuery('.social-share').show();
+				jQuery(':input[type="submit"]').prop('disabled', false);
+				jQuery('.country_restriction_msg').hide();
+			}else{
+				jQuery('.social-share').hide();
+				jQuery(':input[type="submit"]').prop('disabled', true);
+				jQuery('.country_restriction_msg').show();
+			}
+			
+			jQuery('select[name="page_country"]').on('change', function() {
+				var selected_country1 = this.value;
+				var flag1 = 0;
+				if (Drupal.settings.countries_json[selected_country1] != undefined && Drupal.settings.countries_json[selected_country1] == 0)  {
+					jQuery('.social-share').show();
+					jQuery(':input[type="submit"]').prop('disabled', false);
+					jQuery('.country_restriction_msg').hide();
+				}else{
+					jQuery('.social-share').hide();
+					jQuery(':input[type="submit"]').prop('disabled', true);
+					jQuery('.country_restriction_msg').show();
+				}
+			})
+            // validation
 
         }
 
     };
 })(jQuery, Drupal, this, this.document);
+
+//~ jQuery(document).ready(function(){
+	//~ var selected_country = jQuery("#edit-page-country").val();
+	//~ var flag = 0;
+	//~ if (Drupal.settings.countries_json[selected_country] != undefined && Drupal.settings.countries_json[selected_country] == 0)  {
+		//~ jQuery('.social-share').show();
+		//~ jQuery(':input[type="submit"]').prop('disabled', false);
+		//~ jQuery('.country_restriction_msg').hide();
+	//~ }else{
+		//~ jQuery('.social-share').hide();
+		//~ jQuery(':input[type="submit"]').prop('disabled', true);
+		//~ jQuery('.country_restriction_msg').show();
+	//~ }
+	
+	//~ jQuery("#edit-page-country").on('change', function() {
+		//~ var selected_country1 = this.value;
+		//~ var flag1 = 0;
+		//~ if (Drupal.settings.countries_json[selected_country1] != undefined && Drupal.settings.countries_json[selected_country1] == 0)  {
+			//~ jQuery('.social-share').show();
+			//~ jQuery(':input[type="submit"]').prop('disabled', false);
+			//~ jQuery('.country_restriction_msg').hide();
+		//~ }else{
+			//~ jQuery('.social-share').hide();
+			//~ jQuery(':input[type="submit"]').prop('disabled', true);
+			//~ jQuery('.country_restriction_msg').show();
+		//~ }
+	//~ })
+//~ });
