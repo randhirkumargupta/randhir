f19ce01adbd0e847a5fd419d1109adc94523be84
Sandeep <sandeep.bharti@aajtak.com>
2018-05-28 23:03:23 +0530

    newswiz 2018 RHS

--- staticpages/newswiz/2018/index.php
+++ staticpages/newswiz/2018/index.php
@@ -90,7 +90,8 @@ document.write(page_request.responseText)}
 
 
 <style>    
-.video_episode_snippets .video_inner p {font-size: 15px !important;line-height: 18px;padding: 5px;}.video_info,.item {background: none !important;}.outer-video-area {margin-bottom: 56px;background-color: #fff;}.video_episode_snippets .video_inner {height: 278px; margin-bottom:15px;}.video_episode_snippets .video_inner img {height: 232px;}.timeline-outer .slider-year {background: none !important;overflow: hidden}.timeline-slider ul li {background-color: #fff;margin-bottom: 56px;height: 282px;}li .video_episode_snippets .video_inner p {padding-left: 5px !important;background-color: #fff;}.videoshow-section {background: none !important;height: 1635px;overflow: hidden;}.videoshow-section ul {position: relative;margin: 0;padding: 0;}.videoshow-section ul li {color: #fff;width: 320px;float: left;height: 282px;background-color: #fff;margin-bottom: 56px;}.pad26 {padding-bottom: 26px;}.headtxt {font-size: 19px;margin-bottom: 5px;}.headtxt a {color: #007ed2 !important;text-decoration: underline}ul.headtxt_t{ padding-left: 20px;line-height: 28px;font-size: 20px; text-transform:uppercase;}ul.headtxt_t li{margin-bottom:6px}
+.video_episode_snippets .video_inner p {font-size: 15px !important;line-height: 18px;padding: 5px;}.video_info,.item {background: none !important;}.outer-video-area {margin-bottom: 56px;background-color: #fff;}.video_episode_snippets .video_inner {height: 278px; margin-bottom:15px;}.video_episode_snippets .video_inner img {height: 232px;}.timeline-outer .slider-year {background: none !important;overflow: hidden}.timeline-slider ul li {background-color: #fff;} 
+li .video_episode_snippets .video_inner p {padding-left: 5px !important;background-color: #fff;}.videoshow-section {background: none !important;height: 1635px;overflow: hidden;}.videoshow-section ul {position: relative;margin: 0;padding: 0;}.videoshow-section ul li {color: #fff;width: 320px;float: left;height: 282px;background-color: #fff;margin-bottom: 56px;}.pad26 {padding-bottom: 26px;}.headtxt {font-size: 19px;margin-bottom: 5px;}.headtxt a {color: #007ed2 !important;text-decoration: underline}ul.headtxt_t{ padding-left: 20px;line-height: 28px;font-size: 20px; text-transform:uppercase;}ul.headtxt_t li{margin-bottom:6px}
    .support-team .support-slider ul li span{width:100%} 
    .support-team .sup_like_all .share_like ul li{display:inline-block;line-height: 34px;width:auto;padding-bottom: 0px;float:none;}   
    .support-team .sup_like_all.sup_like_all1{height:152px;}
@@ -413,35 +414,35 @@ $(".navLink ul li ul.flashLinkMenu").toggleClass("flashToggle");
   $('.slider-year ul').css('width', fwidth);
   var counters = 1;
   
-  $('#nexttag').click(function(){
-                     
-    if(counters < photolen-4)   
+  $('#next').click(function(){
+
+    if(counters < photolen)
     {
         $('.slider-year ul').animate({
-          top : '-='+twidth
+          left : '-=320'
         });
          counters += 1;
-        
-        $('#pretag').css('opacity', '1');
+
+        $('#pre').css('opacity', '1');
     }
     else{
-      $('#nexttag').css('opacity', '1');
-      
+      $('#next').css('opacity', '1');
+
     }
   });
-  
-  
-  $('#pretag').click(function(){
+
+
+  $('#pre').click(function(){
     if(counters == 1)
     {
-        $('#pretag').css('opacity', '1');
+        $('#pre').css('opacity', '1');
     }
     else{
       $('.slider-year ul').animate({
-          top : '+='+twidth
-        });   
+          left : '+=320'
+        });
       counters -= 1;
-      $('#nexttag').css('opacity', '1');
+      $('#next').css('opacity', '1');
     }
   });
 
