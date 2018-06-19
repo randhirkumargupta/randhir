b0988b7e89c77f43295a1d020a5b06773bbaa2cc
Arun Mishra <arunmishra.coder@gmail.com>
2018-06-19 17:48:03 +0530

    debug

--- sites/all/themes/itg/templates/html--headless.tpl.php
+++ sites/all/themes/itg/templates/html--headless.tpl.php
@@ -1,3 +1,5 @@
 <?php
-$node_obj = menu_get_object();
-print $node_obj->body[LANGUAGE_NONE][0]['value'];
+// $node_obj = menu_get_object();
+//print $node_obj->body[LANGUAGE_NONE][0]['value'];
+
+print render($content);
