0b796bf15b9ca0e9f3712f8c300b22dac3888f63
Arun Mishra <arunmishra.coder@gmail.com>
2018-06-19 19:23:11 +0530

    debug

--- sites/all/themes/itg/templates/html--headless.tpl.php
+++ sites/all/themes/itg/templates/html--headless.tpl.php
@@ -1,6 +1,3 @@
 <?php
 $node_obj = menu_get_object();
-//print $node_obj->body[LANGUAGE_NONE][0]['value'];
-pr($node_obj->body);
-print render($node_obj->body);
-
+print str_replace("&#13;", "", $node_obj->body[LANGUAGE_NONE][0]['value']);
