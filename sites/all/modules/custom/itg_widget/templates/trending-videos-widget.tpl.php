921dbfc6085d6dc1ceb2539b7fc897ecc032d4ce
Sandeep <sandeep.bharti@aajtak.com>
2018-06-28 18:39:11 +0530

    tranding videos css

--- sites/all/modules/custom/itg_widget/templates/trending-videos-widget.tpl.php
+++ sites/all/modules/custom/itg_widget/templates/trending-videos-widget.tpl.php
@@ -5,58 +5,6 @@
 	$data_tb_region_item = 'data-tb-region-item';  
   }
 ?>
-<style type="text/css">
-.home-trending-video .itg-widget-child.tab-data{border-left: 1px solid #ddd;border-right: 1px solid #ddd;border-bottom: 1px solid #ddd;}
-.home-trending-video #trending-videos .trending-videos{border:0px;}
-.home-trending-video #trending-videos{ padding:10px; position: relative;}
-.home-trending-video #trending-videos ul li{padding:0px; margin-right:0px!important}
-.home-trending-video #trending-videos .trending-videos li span.videoicon i:before{ color:#323232;font-size:20px; }
-.home-trending-video #trending-videos .trending-videos li span.videoicon{color:#323232; font-size:14px; line-height:20px}
-.home-trending-video #trending-videos .trending-videos.slides li p{ padding:5px 0 7px; }
-.home-trending-video #trending-videos .trending-videos{overflow-x: hidden;}
-.home-trending-video #trending-videos .trending-videos li+li{ border-top:0px;}
-.home-trending-video #trending-videos .trending-videos .trending-videos-list .pic{ float: none; margin-right:0px; }
-.home-trending-video #trending-videos .trending-videos li a.pic:after{ background:transparent; }
-.home-trending-video #trending-videos .trending-videos li span.pic.video-icon{position: relative; height: 24px;display: block;float: none;line-height: 23px; margin-top: 10px;}
-.home-trending-video #trending-videos .trending-videos li span.pic.video-icon:after{ font: normal normal normal 14px/1 FontAwesome; content:"\f01d"; font-size: 23px; color:#323232; bottom: 0;left: 0;background: transparent;top: 2px;}
-.videoicon.desktophide{ display:none; }
-
-.home-trending-video #trending-videos ul.trending-videos{border:0; padding-top:0; width:650%;}
-.home-trending-video #trending-videos ol.flex-control-paging{display:none;}
-.home-trending-video #trending-videos .flex-direction-nav a{opacity:1!important;}
-.home-trending-video #trending-videos .flex-nav-prev,.home-trending-video #trending-videos .flex-nav-next{display: inline-block; background-color:rgba(0,0,0,0.5); width: 40px; height: 64px; border-radius: 0px 35px 35px 0; position:absolute;}
-.home-trending-video #trending-videos .flex-nav-prev{left:0px; top:80px;}
-.home-trending-video #trending-videos .flex-nav-next{right:20px; top:80px;border-radius: 35px 0px 0px 35px;}
-.home-trending-video #trending-videos .flex-direction-nav .flex-nav-next .flex-next{padding-left:14px;}
-.home-trending-video #trending-videos .flex-direction-nav{position: absolute;top: 0;width: 100%;}
-.home-trending-video #trending-videos .flex-direction-nav .flex-next{right:0; position:static;line-height: 64px;}
-.home-trending-video #trending-videos .flex-direction-nav .flex-prev{left:0; position:static;line-height: 64px;}
-.home-trending-video #trending-videos .flex-direction-nav a{display:inline;}
-.home-trending-video #trending-videos .flex-direction-nav a:before{color:#fff;}
-.home-trending-video #trending-videos .trending-videos-flex{position:relative;}
-.home-trending-video #trending-videos ul li{padding:0px; margin-right:0px!important}
-@media only screen and (min-width: 768px) and (max-width: 800px) {.home-trending-video h3.desktop-hide{display:none;}}
-@media only screen and (min-width: 320px) and (max-width: 767px) {
- .home-trending-video .tab-buttons{ display: none; }
- .home-trending-video h3{text-transform: uppercase; padding: 0 0 0 10px; margin:0px; color: #000;font-size: 16px;font-weight: 600;margin-bottom:0px; height: auto;font-family: "OpenSans-Bold";}
- .home-trending-video .itg-widget-child.tab-data{ display: block!important }
-.home-trending-video .videoicon.mobilehide{ display:none; }
-.home-trending-video .videoicon.desktophide{ display:inline-block; }
-.home-trending-video #trending-videos .flex-direction-nav a{opacity:0!important;}
-.home-trending-video #trending-videos .flex-nav-prev,.home-trending-video #trending-videos .flex-nav-next{display: none;}
-.home-trending-video #trending-videos ul.trending-videos li{box-shadow: 0 0px 4px rgba(0,0,0,0.24), 0 0px 4px rgba(0,0,0,0.24); -webkit-box-shadow: 0 0px 4px rgba(0,0,0,0.12), 0 0px 4px rgba(0,0,0,0.24); border:1px solid rgba(0,0,0,0.12);}
-.home-trending-video #trending-videos ul li{padding:10px 0px 0; margin-right:15px!important;border-radius: 5px; margin-bottom:2px; margin-top:2px; }
-.home-trending-video #trending-videos ul li p{ padding: 15px 10px; font-size: 15px; font-family: "OpenSans-regular"; line-height:20px;}
-.home-trending-video #trending-videos .trending-videos li span.videoicon i:before {color: #fff; font-size:18px;}
-.home-trending-video #trending-videos .trending-videos li span.videoicon{padding:3px 5px 1px; background:rgba(0,0,0,1); color:#fff; font-size:14px; line-height:20px; position: absolute; bottom:0;}
-.home-trending-video #trending-videos .trending-videos.slides li p{padding:7px 10px}
-.home-trending-video .itg-widget-child.tab-data{border:0px;}
-.home-trending-video #trending-videos{ padding:5px 10px; margin-bottom:15px; }
-.home-trending-video #trending-videos .trending-videos .trending-videos-list{min-height:250px;}
-.home-trending-video #trending-videos .top_takes_videos_widget h3{ margin-top:20px;}
-}
-
-</style>
 <h3 class="desktop-hide">Trending Videos</h3>
 <div class="trending-videos-flex" id="trending-videos">
   <ul class="trending-videos slides">
