3180f0ca105aa3144e6664cbb300199fca444e7a
Sandeep <sandeep.bharti@aajtak.com>
2018-07-28 01:21:24 +0530

    css update

--- sites/all/modules/custom/itg_story/templates/itg-story-load-briefly.tpl.php
+++ sites/all/modules/custom/itg_story/templates/itg-story-load-briefly.tpl.php
@@ -1,6 +1,10 @@
 <!-- <button id="opener"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 width="24px" height="24px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
 <g display="none"><image display="inline" overflow="visible" width="32" height="24" id="_x38_tQb2G_1_" xlink:href="341E23CB5CF74B60.jpg"  transform="matrix(1 0 0 1 -4 -2)"></image></g><path fill="#FFFFFF" d="M23,13c0,0.55-0.45,1-1,1H2c-0.55,0-1-0.45-1-1V2c0-0.55,0.45-1,1-1h20c0.55,0,1,0.45,1,1V13z"/><path fill="#FFFFFF" d="M19,17c0,0.55-0.45,1-1,1H5c-0.55,0-1-0.45-1-1l0,0c0-0.55,0.45-1,1-1h13C18.55,16,19,16.45,19,17L19,17z"/><path fill="#FFFFFF" d="M17,21c0,0.55-0.45,1-1,1H7c-0.55,0-1-0.45-1-1l0,0c0-0.55,0.45-1,1-1h9C16.55,20,17,20.45,17,21L17,21z"/></svg></button> -->
+<style type="text/css">
+	#block-itg-layout-manager-header-block .menu-login .social-nav ul li.briefly-icon-prant svg{ position: relative; top:7px; margin: 0 10px; cursor: pointer;}
+	#block-itg-layout-manager-header-block .menu-login .social-nav ul li.briefly-icon-prant:hover svg .hoversvg{fill:#ffc106}
+</style>
 <?php
   if($_GET['test'] == 'abc') {
     p($data);
